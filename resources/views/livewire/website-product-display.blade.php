f47023f7827b922733b2a787d6e33b6caaf7de52
spsDev <dev@oit.com.np>
2022-03-10 21:16:59 +0545

    FB OG share update

--- resources/views/livewire/website-product-display.blade.php
+++ resources/views/livewire/website-product-display.blade.php
@@ -1,25 +1,5 @@
 <div>
 
-  <div class="py-2 mb-3 border">
-    <h2>
-      Share
-    </h2>
-    @if (false)
-    <button class="btn btn-success" wire:click="">
-      Facebook
-    </button>
-    @endif
-    <script>
-    document.getElementById('shareBtn').onclick = function() {
-      FB.ui({
-        display: 'popup',
-        method: 'share',
-        href: "{{ Request::url() }}",
-      }, function(response){});
-    }
-    </script>
-  </div>
-
   <div class="row">
     <div class="col-md-6 py-3">
       <h2 class="ml-2 mb-3">
@@ -55,6 +35,19 @@
                 </span>
               </td>
             </tr>
+            <tr>
+              <th>
+                Share
+              </th>
+              <td class="">
+                <a href="https://www.facebook.com/sharer/sharer.php?u={{ url()->current() }}" target="_blank">
+                  <button class="btn">
+                    <i class="text-primary fab fa-facebook fa-2x mr-3"></i>
+                  </button>
+                </a>
+              </td>
+            </tr>
+
           </tbody>
         </table>
       </div>
