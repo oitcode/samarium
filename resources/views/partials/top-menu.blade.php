3cb6f1ab45cfd518b569c4118be5bae94464e51e
spsDev <dev@oit.com.np>
2022-03-10 11:22:33 +0545

    misc

--- resources/views/partials/top-menu.blade.php
+++ resources/views/partials/top-menu.blade.php
@@ -8,7 +8,7 @@
     @foreach ($productCategories as $productCategory)
         <div class="col-md-2 border p-0">
           <a href="{{ route('website-product-category-product-list', [$productCategory->product_category_id, $productCategory->name]) }}">
-            <button class="btn btn-danger w-100" style="font-size: 1.1rem;">
+            <button class="btn btn-danger w-100 h-100" style="font-size: 1.1rem;">
               {{ $productCategory->name }}
             </button>
           </a>
