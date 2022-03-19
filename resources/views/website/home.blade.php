fcb416b6ee24f3a2f7579d04e5a179ffd4d83a7e
spsDev <dev@oit.com.np>
2022-03-19 09:28:08 +0545

    FB OG Meta tags updated

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -1,5 +1,13 @@
 @extends ('website.base')
 
+@section ('fbOgMetaTags')
+<meta property="og:url"                content="{{ Request::url() }}" />
+<meta property="og:type"               content="article" />
+<meta property="og:title"              content="Kimchi Ramen | Authentic korean food in Kathmandu." />
+<meta property="og:description"        content=" Order Online Kathmandu. Discount available" />
+<meta property="og:image"              content="{{ asset('storage/' . $company->logo_image_path) }}" />
+@endsection
+
 @section ('content')
   @livewire ('website-home-component')
 @endsection
