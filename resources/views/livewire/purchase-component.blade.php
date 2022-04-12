549e2fada814457d1064e4e1fff4f8445014f3aa
spsDev <dev@oit.com.np>
2022-04-12 11:48:31 +0545

    misc

--- resources/views/livewire/purchase-component.blade.php
+++ resources/views/livewire/purchase-component.blade.php
@@ -2,22 +2,22 @@
 
   @if (true || ! $modes['create'] && ! $modes['display'])
   <div class="mb-3">
-    <button class="btn btn-success-rm m-0 border shadow-sm" style="height: 100px; width: 225px; font-size: 1.5rem;" wire:click="enterMode('create')">
+    <button class="btn btn-success-rm m-0 border shadow-sm" style="height: 100px; width: 200px; font-size: 1.3rem;" wire:click="enterMode('create')">
       <i class="fas fa-plus-circle mr-3"></i>
       New
     </button>
 
-    <button class="btn btn-success-rm m-0 border shadow-sm" style="height: 100px; width: 225px; font-size: 1.5rem;" wire:click="enterMode('list')">
+    <button class="btn btn-success-rm m-0 border shadow-sm" style="height: 100px; width: 200px; font-size: 1.3rem;" wire:click="enterMode('list')">
       <i class="fas fa-list mr-3"></i>
       List
     </button>
 
-    <button class="btn btn-success-rm m-0 border shadow-sm" style="height: 100px; width: 225px; font-size: 1.5rem;" wire:click="enterMode('create')">
+    <button class="btn btn-success-rm m-0 border shadow-sm" style="height: 100px; width: 200px; font-size: 1.3rem;" wire:click="enterMode('create')">
       <i class="fas fa-chart-line mr-3"></i>
       Report
     </button>
 
-    <button class="btn btn-success-rm m-0 border shadow-sm" style="height: 100px; width: 225px; font-size: 1.5rem;" wire:click="enterMode('create')">
+    <button class="btn btn-success-rm m-0 border shadow-sm" style="height: 100px; width: 200px; font-size: 1.3rem;" wire:click="enterMode('create')">
       <i class="fas fa-search mr-3"></i>
       Search
     </button>
