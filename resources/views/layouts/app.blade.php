9b66b2b7615eff0bcb892d090fb76ce77132dd92
spsDev <dev@oit.com.np>
2022-06-21 13:44:16 +0545

    misc

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -446,7 +446,7 @@
                 <div class="dropdown-divider mb-0"></div>
                 <a class="dropdown-item" href="">
                   <i class="fas fa-history text-secondary mr-2"></i>
-                  v0.1.3
+                  v0.1.4
                 </a>
                 <div class="dropdown-divider mb-0"></div>
                 <a class="dropdown-item mb-0" href="{{ route('logout') }}"
