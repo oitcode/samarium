11da7c9cbdbe2eb71a7fa7f7ea3ca79c2cfdc82a
spsDev <dev@oit.com.np>
2022-06-22 12:16:29 +0545

    misc

--- resources/views/livewire/takeaway-component.blade.php
+++ resources/views/livewire/takeaway-component.blade.php
@@ -1,30 +1,31 @@
 <div class="p-3 p-md-0">
   @if (! $modes['create'] && ! $modes['display'])
   <div class="mb-3">
-    <button class="btn border shadow-sm m-0 badge-pill mr-3" style="height: 75px; width: 150px; font-size: 1.3rem;" wire:click="enterMode('create')">
+
+    {{-- Show in bigger screens --}}
+    <button class="btn border shadow-sm m-0 badge-pill mr-3 d-none d-md-inline-block"
+        style="height: 75px; width: 150px; font-size: 1.3rem;" wire:click="enterMode('create')">
       <i class="fas fa-plus-circle mr-3"></i>
       New
     </button>
-    <button class="btn
-        m-0 border shadow-sm badge-pill mr-3"
+    <button class="btn border shadow-sm m-0 badge-pill mr-3 d-none d-md-inline-block"
         style="height: 75px; width: 150px; font-size: 1.3rem;" wire:click="enterMode('list')">
       <i class="fas fa-list mr-3"></i>
       List
     </button>
 
-    @if (false)
-    <button class="btn btn-success-rm m-0 border shadow-sm badge-pill mr-3"
-        style="height: 75px; width: 150px; font-size: 1.3rem;" wire:click="">
-      <i class="fas fa-chart-line mr-3"></i>
-      Report
+    {{-- Show in smaller screens --}}
+    <button class="btn m-0 border shadow-sm badge-pill mr-3 mb-3 d-md-none"
+        style="font-size: 1.3rem;"
+        wire:click="enterMode('create')">
+      <i class="fas fa-plus-circle mr-3"></i>
+      New
     </button>
-
-    <button class="btn btn-success-rm m-0 border shadow-sm badge-pill mr-3"
-        style="height: 75px; width: 150px; font-size: 1.3rem;" wire:click="">
-      <i class="fas fa-search mr-3"></i>
-      Search
+    <button class="btn m-0 border shadow-sm badge-pill mr-3 mb-3 d-md-none"
+        style="font-size: 1.3rem;" wire:click="enterMode('list')">
+      <i class="fas fa-list mr-3"></i>
+      List
     </button>
-    @endif
 
     <button class="btn btn-success m-0 float-right d-none d-md-block"
         style="height: 100px; width: 225px; font-size: 1.5rem;">
