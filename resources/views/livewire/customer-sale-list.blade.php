b67b79db6b1d105b86d3d6072bbd17614133e5de
spsDev <dev@oit.com.np>
2022-06-26 14:41:09 +0545

    misc

--- resources/views/livewire/customer-sale-list.blade.php
+++ resources/views/livewire/customer-sale-list.blade.php
@@ -1,6 +1,8 @@
 <div>
   @if ($saleInvoices != null && count($saleInvoices) > 0)
-    <div class="table-responsive">
+
+    {{-- Show in bigger screens --}}
+    <div class="table-responsive d-none d-md-block">
       <table class="table table-sm table-bordered table-hover">
         <thead>
           <tr class="text-secondary" style="font-size: 1.1rem;">
@@ -51,6 +53,66 @@
                     {{ $saleInvoice->payment_status }}
                   </span>
                 @endif
+              </td>
+              <td>
+                <div class="dropdown">
+                  <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-cog text-secondary"></i>
+                  </button>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <button class="dropdown-item" wire:click="$emit('displaySaleInvoice', {{ $saleInvoice }})">
+                      <i class="fas fa-folder text-primary mr-2"></i>
+                      View bill
+                    </button>
+                    <button class="dropdown-item" wire:click="$emit('receiveSaleInvoicePayment', {{ $saleInvoice->sale_invoice_id }})">
+                      <i class="fas fa-money-check-alt text-primary mr-2"></i>
+                      Receive payment
+                    </button>
+                    <button class="dropdown-item" wire:click="">
+                      <i class="fas fa-print text-primary mr-2"></i>
+                      Print
+                    </button>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+
+    {{-- Show in smaller screens --}}
+    <div class="table-responsive d-md-none border">
+      <table class="table table-sm table-hover">
+        <tbody>
+          @foreach ($saleInvoices as $saleInvoice)
+            <tr>
+              <td>
+                {{ $saleInvoice->sale_invoice_id }}
+                <div>
+                  {{ $saleInvoice->sale_invoice_date }}
+                </div>
+              </td>
+              <td>
+                <div class="font-weight-bold" style="font-size: 1rem;">
+                  Rs
+                  {{ $saleInvoice->getTotalAmount() }}
+                </div>
+                <div>
+                  @if (strtolower($saleInvoice->payment_status) === 'pending')
+                    <span class="badge badge-danger badge-pill">
+                      Pending
+                    </span>
+                  @elseif (strtolower($saleInvoice->payment_status) === 'partially_paid')
+                    <span class="badge badge-warning badge-pill">
+                      Partially Paid
+                    </span>
+                  @elseif (strtolower($saleInvoice->payment_status) === 'paid')
+                    <span class="badge badge-success badge-pill">
+                      {{ $saleInvoice->payment_status }}
+                    </span>
+                  @endif
+                </div>
               </td>
               <td>
                 <div class="dropdown">
