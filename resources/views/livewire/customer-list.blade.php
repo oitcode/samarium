c7a3126b6b03298664bbdcd4784bd2b135939d51
spsDev <dev@oit.com.np>
2022-06-27 12:23:29 +0545

    misc

--- resources/views/livewire/customer-list.blade.php
+++ resources/views/livewire/customer-list.blade.php
@@ -99,7 +99,8 @@
   {{-- Customer table --}}
   @if ($customers != null && count($customers) > 0)
 
-    <div class="table-responsive border">
+    {{-- Show in bigger screens --}}
+    <div class="table-responsive border d-none d-md-block">
       <table class="table table-hover mb-0">
         <thead>
           <tr class="bg-success text-white" style="font-size: 1rem;">
@@ -144,6 +145,68 @@
               </td>
               <td class="text-secondary-rm" style="font-size: 1rem;">
                 {{ $customer->phone }}
+              </td>
+              <td>
+                @if ($customer->getBalance() > 0)
+                  <span class="text-muted mr-1" style="font-size: 0.9rem;">
+                    Rs
+                  </span>
+                  <span class="text-danger-rm font-weight-bold">
+                    @php echo number_format( $customer->getBalance() ); @endphp
+                  </span>
+                @else
+                  Rs
+                  @php echo number_format( $customer->getBalance() ); @endphp
+                @endif
+              </td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+
+    {{-- Show in smaller screens --}}
+    <div class="table-responsive border d-md-none">
+      <table class="table table-hover mb-0">
+        <thead>
+          <tr class="bg-success text-white" style="font-size: 1rem;">
+            <th></th>
+            <th>Customer</th>
+            <th>Balance</th>
+          </tr>
+        </thead>
+
+        <tbody class="bg-white">
+          @foreach ($customers as $customer)
+            <tr class="border">
+              <td style="width: 5vw;">
+
+                @if (false)
+                <button class="btn btn-success-rm border border-primary rounded-circle text-primary"
+                    wire:click="$emit('displayCustomer', {{ $customer->customer_id }})">
+                  <i class="fas fa-folder"></i>
+                </button>
+                @endif
+
+                <div class="dropdown">
+                  <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-cog text-secondary"></i>
+                  </button>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <button class="dropdown-item" wire:click="$emit('displayCustomer', {{ $customer->customer_id }})">
+                      <i class="fas fa-file text-primary mr-2"></i>
+                      View
+                    </button>
+                  </div>
+                </div>
+              </td>
+              <td>
+                <span style="font-size: calc(1rem + 0.1vw);">
+                  {{ ucwords($customer->name) }}
+                </span>
+                <div class="text-secondary" style="0.9rem;">
+                  {{ $customer->phone }}
+                </div>
               </td>
               <td>
                 @if ($customer->getBalance() > 0)
