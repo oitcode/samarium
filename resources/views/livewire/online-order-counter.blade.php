768a7fcd4531af0028825c558fad4729ebd2e75a
spsDev <dev@oit.com.np>
2022-07-23 20:03:12 +0545

    misc

--- resources/views/livewire/online-order-counter.blade.php
+++ resources/views/livewire/online-order-counter.blade.php
@@ -2,7 +2,14 @@
     <span style="font-size: 0.8rem;">
       New Orders:
     </span>
-    <span wire:poll.30000ms class="badge badge-pill badge-danger">
+    
+    <span wire:poll.30000ms class="badge badge-pill
+        @if ($newOrderCount > 0)
+          badge-danger
+        @else
+          badge-light border
+        @endif
+        ">
       {{ $newOrderCount }}
     </span>
 </div>
