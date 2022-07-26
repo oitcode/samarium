83311cd84f2c62baa7e892e48cf235c3d745e083
spsDev <dev@oit.com.np>
2022-07-26 11:11:34 +0545

    Updated login page

---
+++ resources/views/partials/login-left.blade.php
@@ -0,0 +1,16 @@
+<div>
+  <div class="d-flex">
+    <i class="fas fa-dice-d6 mr-5 h1"></i>
+    <div>
+      <h1>
+        Kimchi Ramen
+      </h1>
+      @if (true)
+      <h2 class="h5">
+        EDB
+      </h2>
+      @endif
+
+    </div>
+  </div>
+</div>
