7592af89335af3ebf219748f9d6184dddd8eff3a
spsDev <dev@oit.com.np>
2022-08-03 13:40:39 +0545

    Refactor; Progress

--- resources/views/livewire/customer-component.blade.php
+++ resources/views/livewire/customer-component.blade.php
@@ -72,22 +72,6 @@
       </span>
     </button>
 
-    @if (false)
-    <div class="d-inline-block float-right">
-      <div class="dropdown">
-        <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-cog text-secondary"></i>
-        </button>
-        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton" style="font-size: 1rem;">
-          <button class="dropdown-item py-2" wire:click="">
-            <i class="fas fa-plus-circle text-primary mr-2"></i>
-            Todo
-          </button>
-        </div>
-      </div>
-    </div>
-    @endif
-
     <div class="clearfix">
     </div>
   </div>
@@ -116,38 +100,18 @@
   @else
     <div class="row" style="margin: auto;">
       <div class="col-md-2 shadow-sm p-0 mr-5 mb-4">
-        <div class="card">
-          <div class="card-body p-0 bg-white text-secondary">
-            <div class="d-flex w-100">
-              <div class="p-3">
-                <h2 class="mb-4" style="font-size: 1.3rem;">Customers</h2>
-                <h2>{{ $totalCustomers }}</h2>
-              </div>
-              <div class="d-flex justify-content-center w-50">
-                <div class="d-flex flex-column justify-content-center">
-                  <i class="fas fa-users fa-2x"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        @include ('partials.flash-card-simple', [
+            'fcTitle' => 'Customers',
+            'fcData' => $totalCustomers,
+            'fcIconFaClass' => 'fas fa-users',
+        ])
       </div>
       <div class="col-md-2 shadow-sm p-0 mr-5 mb-4">
-        <div class="card">
-          <div class="card-body p-0 bg-white text-secondary">
-            <div class="d-flex w-100">
-              <div class="p-3">
-                <h2 class="mb-4" style="font-size: 1.3rem;">Debtors</h2>
-                <h2>{{ $totalDebtors }}</h2>
-              </div>
-              <div class="d-flex justify-content-center w-50">
-                <div class="d-flex flex-column justify-content-center">
-                  <i class="fas fa-exclamation-circle fa-2x"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        @include ('partials.flash-card-simple', [
+            'fcTitle' => 'Debtors',
+            'fcData' => $totalDebtors,
+            'fcIconFaClass' => 'fas fa-exclamation-circle',
+        ])
       </div>
     </div>
   @endif
---
+++ resources/views/partials/flash-card-simple.blade.php
@@ -0,0 +1,17 @@
+<div class="card">
+  <div class="card-body p-0 bg-white text-secondary">
+    <div class="d-flex w-100">
+      <div class="p-3">
+        <h2 class="mb-4" style="font-size: 1.3rem;">
+          {{ $fcTitle }}
+        </h2>
+        <h2>{{ $fcData }}</h2>
+      </div>
+      <div class="d-flex justify-content-center w-50">
+        <div class="d-flex flex-column justify-content-center">
+          <i class="{{ $fcIconFaClass }} fa-2x"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
