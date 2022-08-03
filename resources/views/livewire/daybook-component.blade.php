d704efaf985396df9996484bcc239e46a46f32d7
spsDev <dev@oit.com.np>
2022-08-03 11:29:27 +0545

    Refactor; Daybook; Started

--- resources/views/livewire/daybook-component.blade.php
+++ resources/views/livewire/daybook-component.blade.php
@@ -4,7 +4,6 @@
     {{-- Show in bigger screens --}}
     @if (! $modes['displaySaleInvoice'])
     <div class="bg-info-rm mb-4 d-none d-md-block p-0">
-      @if (true)
       <div class="float-left d-flex bg-warning-rm">
         <button class="btn btn-success-rm mr-4 p-0 bg-white badge-pill-rm" wire:click="setPreviousDay">
           <i class="fas fa-arrow-alt-circle-left fa-2x mr-3-rm
@@ -28,7 +27,6 @@
           </button>
         </div>
       </div>
-      @endif
 
       <button wire:loading class="btn btn-danger-rm" style="font-size: 1.5rem;">
         <div class="spinner-border text-info mr-3" role="status">
@@ -36,7 +34,6 @@
         </div>
       </button>
 
-      @if (true)
       @if (! $modes['displaySaleInvoice'])
       <div class="shadow-sm-rm float-right" style="">
         <div class="card bg-white text-dark" style="">
@@ -53,7 +50,6 @@
         </div>
       </div>
       @endif
-      @endif
 
       @if (! $modes['displaySaleInvoice'])
         <div class="float-left mt-3-rm px-2 pt-2 ml-5">
@@ -68,21 +64,14 @@
     @endif
 
 
-
     {{-- Show in smaller screens --}}
     <div class="bg-info-rm mb-4 d-md-none">
       <button class="btn btn-success-rm m-0" style="font-size: 1.5rem;" wire:click="setPreviousDay">
         <i class="fas fa-arrow-left mr-3"></i>
-        @if (false)
-        Previous
-        @endif
       </button>
 
       <button class="btn btn-danger-rm m-0" style="font-size: 1.5rem;" wire:click="setNextDay">
         <i class="fas fa-arrow-right mr-3"></i>
-        @if (false)
-        Next
-        @endif
       </button>
 
       <button wire:loading class="btn btn-danger-rm" style="font-size: 1.5rem;">
@@ -121,7 +110,51 @@
     </div>
 
 
-  @if (! $modes['displaySaleInvoice'])
+  @if ($modes['displaySaleInvoice'])
+    @if (true)
+      {{-- Toolbar --}}
+      <div class="d-flex justify-content-between mb-4 border p-1 bg-white-rm text-white-rm shadow-sm" style="background-color: #fff;">
+        <div>
+          <a href="{{ route('dashboard-print-sale-invoice', $displayingSaleInvoice->sale_invoice_id) }}"
+              target="_blank"
+              class="btn text-secondary">
+            <i class="fas fa-print fa-2x-rm"></i>
+            <br />
+            Print
+          </a>
+          <button class="btn text-secondary">
+            <i class="fas fa-file-pdf-o fa-2x-rm"></i>
+            <br />
+            PDF
+          </button>
+          <button wire:loading class="btn btn-danger-rm" style="font-size: 1.5rem;">
+            <div class="spinner-border text-info mr-3" role="status">
+              <span class="sr-only">Loading...</span>
+            </div>
+          </button>
+          @if (false)
+          <button class="btn text-secondary">
+            <i class="fas fa-file-excel-o fa-2x-rm"></i>
+            <br />
+            Excel
+          </button>
+          @endif
+        </div>
+        @if (false)
+        <div class="">
+          <button class="btn text-dark" wire:click="">
+            <i class="fas fa-times-circle fa-2x"></i>
+            <br />
+            Close
+          </button>
+        </div>
+        @endif
+      </div>
+      @livewire ('daybook-sale-invoice-display', ['saleInvoice' => $displayingSaleInvoice,])
+    @else
+      @livewire ('core-sale-invoice-display', ['saleInvoice' => $displayingSaleInvoice,])
+    @endif
+  @else
     <div class="row">
       <div class="col-md-12">
         @if ( true {{--$saleInvoices != null && count($saleInvoices) > 0--}})
@@ -161,11 +194,6 @@
                           </span>
                         </td>
                         <td class="d-none d-md-table-cell">
-                          @if (false)
-                          <div>
-                            {{ $saleInvoice->sale_invoice_date }}
-                          </div>
-                          @endif
                           <div>
                             {{ $saleInvoice->created_at->format('H:i A') }}
                           </div>
@@ -342,13 +370,13 @@
 
             {{-- Pending Amount --}}
             <div class="">
-                  <h2 class="text-muted mb-3 font-weight-bold h6">
-                    Pending
-                  </h2>
-                  <h3 class="text-danger font-weight-bold h5">
-                    Rs
-                    @php echo number_format( $netPendingAmount ); @endphp
-                  </h3>
+              <h2 class="text-muted mb-3 font-weight-bold h6">
+                Pending
+              </h2>
+              <h3 class="text-danger font-weight-bold h5">
+                Rs
+                @php echo number_format( $netPendingAmount ); @endphp
+              </h3>
             </div>
 
           </div>
@@ -401,49 +429,6 @@
         @endif
       </div>
     </div>
-  @else
-    @if (true)
-      {{-- Toolbar --}}
-      <div class="d-flex justify-content-between mb-4 border p-1 bg-white-rm text-white-rm shadow-sm" style="background-color: #fff;">
-        <div>
-          <a href="{{ route('dashboard-print-sale-invoice', $displayingSaleInvoice->sale_invoice_id) }}"
-              target="_blank"
-              class="btn text-secondary">
-            <i class="fas fa-print fa-2x-rm"></i>
-            <br />
-            Print
-          </a>
-          <button class="btn text-secondary">
-            <i class="fas fa-file-pdf-o fa-2x-rm"></i>
-            <br />
-            PDF
-          </button>
-          <button wire:loading class="btn btn-danger-rm" style="font-size: 1.5rem;">
-            <div class="spinner-border text-info mr-3" role="status">
-              <span class="sr-only">Loading...</span>
-            </div>
-          </button>
-          @if (false)
-          <button class="btn text-secondary">
-            <i class="fas fa-file-excel-o fa-2x-rm"></i>
-            <br />
-            Excel
-          </button>
-          @endif
-        </div>
-        @if (false)
-        <div class="">
-          <button class="btn text-dark" wire:click="">
-            <i class="fas fa-times-circle fa-2x"></i>
-            <br />
-            Close
-          </button>
-        </div>
-        @endif
-      </div>
-      @livewire ('daybook-sale-invoice-display', ['saleInvoice' => $displayingSaleInvoice,])
-    @else
-      @livewire ('core-sale-invoice-display', ['saleInvoice' => $displayingSaleInvoice,])
-    @endif
   @endif
+
 </div>
