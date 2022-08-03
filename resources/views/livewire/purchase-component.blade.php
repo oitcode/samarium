067c561c259fbde96a69b771437f7640e32ea8a5
spsDev <dev@oit.com.np>
2022-08-03 16:09:08 +0545

    Refactor; Progress

--- resources/views/livewire/purchase-component.blade.php
+++ resources/views/livewire/purchase-component.blade.php
@@ -23,26 +23,11 @@
       List
     </button>
 
-    @if (false)
-    <button class="btn btn-success-rm m-0 border shadow-sm badge-pill mr-3"
-        style="height: 75px; width: 150px; font-size: 1.3rem;" wire:click="">
-      <i class="fas fa-chart-line mr-3"></i>
-      Report
-    </button>
-
-    <button class="btn btn-success-rm m-0 border shadow-sm badge-pill mr-3"
-        style="height: 75px; width: 150px; font-size: 1.3rem;" wire:click="">
-      <i class="fas fa-search mr-3"></i>
-      Search
-    </button>
-    @endif
-
     <button wire:loading class="btn m-0"
         style="height: 100px; width: 225px; font-size: 1.5rem;">
       <span class="spinner-border text-info mr-3" role="status">
       </span>
     </button>
-
 
     <div class="clearfix">
     </div>
@@ -76,12 +61,12 @@
       </span>
     </button>
 
-
     <div class="clearfix">
     </div>
   </div>
   @endif
 
+  {{-- Use the required component as per mode --}}
   @if ($modes['create'])
     @livewire ('purchase-create')
   @elseif ($modes['list'])
@@ -93,9 +78,6 @@
           'purchase' => $displayingPurchase,
       ])
     @else
-      @if (false)
-      @livewire ('purchase-display', ['purchase' => $displayingPurchase,])
-      @endif
       @livewire ('core-purchase-display', ['purchase' => $displayingPurchase,])
     @endif
   @endif
