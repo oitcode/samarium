08dc69c89ea3be6b60de6699c49a8cda2c7282a3
spsDev <dev@oit.com.np>
2022-08-04 09:07:19 +0545

    Refactor; Bg fix

--- app/Http/Livewire/SaleInvoiceWork.php
+++ app/Http/Livewire/SaleInvoiceWork.php
@@ -83,6 +83,7 @@ class SaleInvoiceWork extends Component
 
         $this->deletingSaleInvoiceItem = null;
         $this->exitMode('confirmRemoveSaleInvoiceItem');
+        $this->emit('makePaymentPleaseUpdate');
         $this->render();
     }
 
