ffb106228b3336c709aa6052f80cf3a7e10d515c
spsDev <dev@oit.com.np>
2022-08-05 11:57:04 +0545

    Tool bar button; modifies

--- resources/views/partials/tool-bar-button-pill.blade.php
+++ resources/views/partials/tool-bar-button-pill.blade.php
@@ -4,9 +4,17 @@
         @isset ($modes[$btnCheckMode])
           @if ($modes[$btnCheckMode])
             btn-success
+          @else
+            bg-white
           @endif
+        @else
+          bg-white
         @endisset
+      @else
+        bg-white
       @endisset
+    @else
+      bg-white
     @endisset
     border shadow-sm m-0 badge-pill mr-3"
     style="font-size: 1.1rem;" wire:click="{{ $btnClickMethod }}">
