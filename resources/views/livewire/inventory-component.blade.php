22f01c20a3a8aca10cac59ce5d6f9a7b43a151a6
spsDev <dev@oit.com.np>
2022-08-08 11:30:46 +0545

    misc

--- resources/views/livewire/inventory-component.blade.php
+++ resources/views/livewire/inventory-component.blade.php
@@ -43,6 +43,8 @@
                 <td>
                   @if ($product->isUsedToday())
                     <i class="fas fa-check-circle text-success"></i>
+                  @else
+                    <i class="fas fa-times-circle text-danger"></i>
                   @endif
                 </td>
                 <td>
