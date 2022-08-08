581792b5647288f344a9a905aa3f51cfe82fefbf
spsDev <dev@oit.com.np>
2022-08-08 12:57:16 +0545

    v0.2.8

--- resources/views/partials/app-top-menu-user-dropdown.blade.php
+++ resources/views/partials/app-top-menu-user-dropdown.blade.php
@@ -28,7 +28,7 @@
       <div class="dropdown-divider mb-0"></div>
       <a class="dropdown-item" href="">
         <i class="fas fa-history text-secondary mr-2"></i>
-        v0.2.7
+        v0.2.8
       </a>
       <div class="dropdown-divider mb-0"></div>
       <a class="dropdown-item mb-0" href="{{ route('logout') }}"
