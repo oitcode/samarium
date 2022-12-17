d24d38792a65d91c75b30a822e7aa4b09b2f5689
spsDev <dev@oit.com.np>
2022-12-17 18:47:59 +0545

    Added few page meta description tags

--- resources/views/partials/ecs/footer.blade.php
+++ resources/views/partials/ecs/footer.blade.php
@@ -1,7 +1,10 @@
 <div>
   <div class="row">
   
-    <div class="col-md-4" style="font-size: 1.5rem;">
+    <div class="col-md-4" style="font-size: 1.3rem;">
+      <div class="mb-3" style="font-size: 1.3rem; font-weight: bold;">
+        Address
+      </div>
       <div class="mb-2">
         <i class="fas fa-map-marker-alt mr-3"></i>
         {{ $company->name }}
@@ -14,70 +17,147 @@
         <i class="fas fa-envelope mr-3"></i>
         {{ $company->email }}
       </div>
-      <div class="mb-1" style="font-size: 1rem;">
+      <div class="mb-4" style="font-size: 1rem;">
         <i class="fas fa-map-marker-alt mr-3"></i>
         {{ $company->address }}
       </div>
-    </div>
-  
-    <div class="col-md-4" style="font-size: 1.3rem;">
-      <div class="mb-2">
-        Social media
-      </div>
       <div>
-        @if ($company->fb_link)
-          <a href="{{ $company->fb_link }}" target="_blank">
-            <div class="float-left text-white" style="">
+        @if (true)
+          <a href="" target="_blank">
+            <div class="float-left text-info" style="">
               <i class="fab fa-facebook mr-3 fa-2x"></i>
             </div>
           </a>
         @endif
-        @if ($company->twitter_link)
-          <a href="{{ $company->twitter_link }}" target="_blank">
+        @if (false)
+          <a href="" target="_blank">
             <div class="float-left text-info" style="">
               <i class="fab fa-twitter mr-3 fa-2x"></i>
             </div>
           </a>
         @endif
-        @if ($company->insta_link)
-          <a href="{{ $company->insta_link }}" target="_blank">
-            <div class="float-left text-white" style="">
-              <i class="fab fa-instagram mr-3 fa-2x"></i>
+        @if (true)
+          <a href="" target="_blank">
+            <div class="float-left text-info" style="">
+              <i class="fab fa-linkedin mr-3 fa-2x"></i>
             </div>
           </a>
         @endif
-        @if ($company->youtube_link)
-          <a href="{{ $company->youtube_link }}" target="_blank">
+        @if (false)
+          <a href="" target="_blank">
             <div class="float-left text-danger" style="">
               <i class="fab fa-youtube mr-3 fa-2x"></i>
             </div>
           </a>
         @endif
-        @if ($company->tiktok_link)
-          <a href="{{ $company->tiktok_link }}" target="_blank">
-            <div class="float-left text-white" style="">
+        @if (false)
+          <a href="" target="_blank">
+            <div class="float-left text-danger" style="">
               <i class="fab fa-tiktok mr-3 fa-2x"></i>
             </div>
           </a>
         @endif
-        <div class="clearfix">
-        </div>
+      </div>
+    </div>
+  
+    <div class="col-md-3">
+      <div class="mb-3" style="font-size: 1.3rem; font-weight: bold;">
+        Services
+      </div>
+      <div class="mb-2">
+        <a href=""
+            class="text-white text-decoration-none">
+          <i class="fas fa-angle-right mr-2"></i>
+          Geotechnical investigation
+        </a>
+      </div>
+      <div class="mb-2">
+        <a href=""
+            class="text-white text-decoration-none">
+          <i class="fas fa-angle-right mr-2"></i>
+          Design works
+        </a>
+      </div>
+      <div class="mb-2">
+        <a href=""
+            class="text-white text-decoration-none">
+          <i class="fas fa-angle-right mr-2"></i>
+          Protection works design
+        </a>
+      </div>
+      <div class="mb-2">
+        <a href=""
+            class="text-white text-decoration-none">
+          <i class="fas fa-angle-right mr-2"></i>
+          Slope stability analysis
+        </a>
+      </div>
+      <div class="mb-2">
+        <a href=""
+            class="text-white text-decoration-none">
+          <i class="fas fa-angle-right mr-2"></i>
+          Topographical survey
+        </a>
+      </div>
+      <div class="mb-2">
+        <a href=""
+            class="text-white text-decoration-none">
+          <i class="fas fa-angle-right mr-2"></i>
+          Construction supervision
+        </a>
       </div>
     </div>
 
-    <div class="col-md-4" style="font-size: 1rem;">
-      <div class="">
-        <i class="fas fa-award mr-3 fa-2x"></i>
-        Educational service since 2014
+    <div class="col-md-3">
+      <div class="mb-3" style="font-size: 1.3rem; font-weight: bold;">
+        Quick Links
+      </div>
+      <div class="mb-2">
+        <a href="" class="text-white text-decoration-none">
+          <i class="fas fa-angle-right mr-2"></i>
+          Home
+        </a>
+      </div>
+      <div class="mb-2">
+        <a href="" class="text-white text-decoration-none">
+          <i class="fas fa-angle-right mr-2"></i>
+          About us
+        </a>
+      </div>
+      <div class="mb-2">
+        <a href="" class="text-white text-decoration-none">
+          <i class="fas fa-angle-right mr-2"></i>
+          Services
+        </a>
+      </div>
+      <div class="mb-2">
+        <a href="" class="text-white text-decoration-none">
+          <i class="fas fa-angle-right mr-2"></i>
+          Portfolio
+        </a>
+      </div>
+      <div class="mb-2">
+        <a href="" class="text-white text-decoration-none">
+          <i class="fas fa-angle-right mr-2"></i>
+          Contact us
+        </a>
       </div>
     </div>
+
+    @if (true)
+    <div class="col-md-2" style="font-size: 1rem;">
+      <div class="mb-3" style="font-size: 1.3rem; font-weight: bold;">
+        Map
+      </div>
+    </div>
+    @endif
   </div>
+
+  <hr />
+
   <div class="text-center-rm mt-4">
-    &copy; 2022
-    BIA
-    &nbsp;&nbsp;
-    |
-    &nbsp;&nbsp;
-    Website developed by <a href="https://oit.com.np" target="_blank" class="text-white">OIT</a>
+    &copy; 2022 | {{ $company->name }} | All rights reserved
+        
+        
   </div>
 </div>
