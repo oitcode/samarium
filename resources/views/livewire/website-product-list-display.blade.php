173412b8fd4d2964c30ff397b132134c0e14225c
spsDev <dev@oit.com.np>
2022-12-18 15:25:49 +0545

    Misc update online store FE updates

--- resources/views/livewire/website-product-list-display.blade.php
+++ resources/views/livewire/website-product-list-display.blade.php
@@ -7,12 +7,27 @@
     <div class="card h-100 shadow">
 
       <div class="d-flex flex-column justify-content-between h-100 bg-success-rm">
-        <img class="img-fluid w-100" src="{{ asset('storage/' . $product->image_path) }}" alt="Card image cap" style="height: 100px;">
+        <div class="p-0">
+          <img class="img-fluid" src="{{ asset('storage/' . $product->image_path) }}" alt="Card image cap" style="{{--max-height: 100px; max-width:
+          100px;--}}">
+        </div>
         <div class="d-flex flex-column justify-content-between flex-grow-1 overflow-auto">
           <div class="p-2">
-            <h2 class="h3 mt-3 mb-2" style="color: #004; font-family: Arial; font-weight: bold;">
-              {{ $product->name }}
-            </h2>
+            <div class="mb-1">
+              <i class="far fa-star" style="color: orange;"></i>
+              <i class="far fa-star" style="color: orange;"></i>
+              <i class="far fa-star" style="color: orange;"></i>
+              <i class="far fa-star" style="color: orange;"></i>
+              <i class="far fa-star" style="color: orange;"></i>
+              <span class="mx-1 text-muted">
+                (0) reviews
+              </span>
+            </div>
+            <a href="{{ route('website-product-view', [$product->product_id, $product->name]) }}">
+              <h2 class="h3 mt-2 mb-2" style="color: #004; font-family: Arial; font-weight: bold;">
+                {{ $product->name }}
+              </h2>
+            </a>
             <div class="h5 text-secondary text-left">
               Rs.
               @php echo number_format( $product->selling_price ); @endphp
