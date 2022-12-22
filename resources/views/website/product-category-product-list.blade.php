090e8901ee00e504667486139ccbc1c0a6e0ab14
spsDev <dev@oit.com.np>
2022-12-22 11:47:45 +0545

    mimsc

--- resources/views/website/product-category-product-list.blade.php
+++ resources/views/website/product-category-product-list.blade.php
@@ -1,5 +1,12 @@
 @extends ('website.base')
 
+@section ('pageTitleTag')
+  <title>
+    {{ $productCategory->name }}
+    {{ env ('CMP_NAME', 'Foobiz') }}
+  </title>
+@endsection
+
 @section ('content')
   <div class="container p-3">
     @livewire ('website-product-category-product-list', ['productCategory' => $productCategory,])
