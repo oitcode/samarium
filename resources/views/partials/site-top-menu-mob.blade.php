0ca649337dd3fca7e7cd3c4ba35a31f2900990e2
Dev <dev@oit.com.np>
2023-02-01 20:49:48 +0545

    Misc cms update

--- resources/views/partials/site-top-menu-mob.blade.php
+++ resources/views/partials/site-top-menu-mob.blade.php
@@ -13,11 +13,30 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       @foreach ($cmsNavMenu->cmsNavMenuItems()->orderBy('order', 'asc')->get() as $cmsNavMenuItem)
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('website-webpage-' . $cmsNavMenuItem->webpage->permalink) }}">
-            {{ $cmsNavMenuItem->name }}
-          </a>
-        </li>
+        @if ($cmsNavMenuItem->type == 'item')
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('website-webpage-' . $cmsNavMenuItem->webpage->permalink) }}">
+              {{ $cmsNavMenuItem->name }}
+            </a>
+          </li>
+        @else
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-secondary" href="#"
+                id="navbarDropdown-{{ $cmsNavMenuItem->name }}"
+                role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              {{ $cmsNavMenuItem->name }}
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown-{{ $cmsNavMenuItem->name }}">
+              @if ($cmsNavMenuItem->cmsNavMenuDropdownItems)
+                @foreach ($cmsNavMenuItem->cmsNavMenuDropdownItems as $cmsNavMenuDropdownItem)
+                  <a class="dropdown-item" href="{{ route('website-webpage-' . $cmsNavMenuDropdownItem->webpage->permalink) }}">
+                    {{ $cmsNavMenuDropdownItem->name }}
+                  </a>
+                @endforeach
+              @endif
+            </div>
+          </li>
+        @endif
       @endforeach
     </ul>
   </div>
