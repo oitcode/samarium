07ebd11fa257bd0503e7d2b8759d7c15262ead02
Dev <dev@oit.com.np>
2023-02-02 11:27:02 +0545

    Misc update; cms site front page

--- resources/views/livewire/ecs/menu-wp.blade.php
+++ resources/views/livewire/ecs/menu-wp.blade.php
@@ -44,8 +44,8 @@
   </div>
 
   {{-- SMALLER SCREEN --}}
-  <div class="d-md-none bg-white">
-    <div class="container-fluid">
+  <div class="d-md-none">
+    <div class="container-fluid p-0">
       @if (true)
       @include ('partials.site-top-menu-mob')
       @endif
@@ -53,45 +53,3 @@
   </div>
 
 </div>
-
-@if (false)
-<div class="container-fluid {{ env('SITE_ECS_THEME_BS_CLASS') }}">
-<div class="container">
-  <nav class="navbar navbar-expand-lg navbar-dark" style="font-size: 1.2rem;">
-    <button class="navbar-toggler border border-white" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-  
-          @foreach ($cmsNavMenu->cmsNavMenuItems()->orderBy('order', 'asc')->get() as $cmsNavMenuItem)
-            @if ($cmsNavMenuItem->type == 'item')
-              <li class="nav-item text-white mr-3 pr-3">
-                <a class="nav-link text-white" href="{{ route('website-webpage-' . $cmsNavMenuItem->webpage->permalink) }}">
-                  {{ $cmsNavMenuItem->name }}
-                </a>
-              </li>
-            @else
-              <li class="nav-item dropdown mr-3">
-                <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown-{{ $loop->iteration }}" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  {{ $cmsNavMenuItem->name }}
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown-{{ $loop->iteration }}">
-                  @if ($cmsNavMenuItem->cmsNavMenuDropdownItems)
-                    @foreach ($cmsNavMenuItem->cmsNavMenuDropdownItems as $cmsNavMenuDropdownItem)
-                      <a class="dropdown-item" href="{{ route('website-webpage-' . $cmsNavMenuDropdownItem->webpage->permalink) }}">
-                        {{ $cmsNavMenuDropdownItem->name }}
-                      </a>
-                    @endforeach
-                  @endif
-                </div>
-              </li>
-            @endif
-          @endforeach
-      </ul>
-    </div>
-  </nav>
-</div>
-</div>
-@endif
