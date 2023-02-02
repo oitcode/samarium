a0273c7ab657f4a0490805c25b6e731dedb95bf9
Dev <dev@oit.com.np>
2023-02-02 16:20:37 +0545

    Misc update; cms site front page

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -14,8 +14,8 @@
 @section ('fbOgMetaTags')
 <meta property="og:url"                content="{{ Request::url() }}" />
 <meta property="og:type"               content="article" />
-<meta property="og:title"              content="Kimchi Ramen | Authentic korean food in Kathmandu." />
-<meta property="og:description"        content=" Order Online Kathmandu. Discount available" />
+<meta property="og:title"              content="{{ $company->name }}" />
+<meta property="og:description"        content="{{ $company->tagline }}" />
 <meta property="og:image"              content="{{ asset('storage/' . $company->logo_image_path) }}" />
 @endsection
 
