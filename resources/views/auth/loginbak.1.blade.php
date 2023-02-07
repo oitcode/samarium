0a19fb0a8c5175e59541731fb72892d4ef28c6e2
Dev <dev@oit.com.np>
2023-02-07 10:43:50 +0545

    Login page update

--- resources/views/auth/loginbak.1.blade.php
+++ resources/views/auth/loginbak.1.blade.php
@@ -35,100 +35,8 @@
 
 <body style="height: 100% !important;">
 
-  <div class="d-flex justify-content-center h-100">
-    <div class="d-flex flex-column justify-content-center">
-        <div class="row-rm border shadow" style="">
-          <div class="col-md-4-rm d-flex flex-column justify-content-center bg-white p-3-rm px-5-rm p-0 bg-warning">
-            <div class="d-flex bg-danger-rm p-3 px-5">
-              <div class="mr-2 p-0">
-                <i class="fas fa-check-circle fa-2x text-primary"></i>
-              </div>
-              <div class="p-0">
-                @if (false)
-                <h1 class="h2 text-primary p-0 m-0">
-                  WebCMS
-                </h1>
-                <div class="h4 text-primary">
-                  Content management
-                </div>
-                @endif
-              </div>
-            </div>
-          </div>
-          <div class="col-md-8-rm px-5 py-3">
-            <h2>
-              Login
-            </h2>
-            <div class="text-secondary mb-4">
-              Welcome back. Please login to your account.
-            </div>
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
-
-                <div class="form-group">
-                  <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                      name="email" value="{{ old('email') }}"
-                      required
-                      autocomplete="email"
-                      placeholder="Email"
-                      autofocus>
-
-                  @error('email')
-                      <span class="invalid-feedback" role="alert">
-                          <strong>{{ $message }}</strong>
-                      </span>
-                  @enderror
-                </div>
-
-                <div class="form-group">
-                    @if (false)
-                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-                    @endif
-
-                    <input id="password" type="password"
-                        class="form-control @error('password') is-invalid @enderror"
-                        name="password"
-                        required
-                        placeholder="Password"
-                        autocomplete="current-password">
-
-                    @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
-
-                <div class="form-group">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                        <label class="form-check-label" for="remember">
-                            {{ __('Remember Me') }}
-                        </label>
-                    </div>
-                </div>
-
-                <div class="form-group mb-0">
-                    <button type="submit" class="btn btn-success">
-                        {{ __('Login') }}
-                    </button>
-
-                    @if (Route::has('password.request'))
-                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                            {{ __('Forgot Your Password?') }}
-                        </a>
-                    @endif
-                </div>
-            </form>
-          </div>
-        </div>
-    </div>
-  </div>
 
 
-
-  @if (false)
   <div class="row bg-warning-rm" style="height: 100% !important;">
     <div class="col-md-6">
       <div class="h-100 bg-info text-white-rm" style="color: #050; font-family: monospace, sans-serif;">
@@ -210,7 +118,6 @@
       </div>
     </div>
   </div>
-  @endif
 
 
 
