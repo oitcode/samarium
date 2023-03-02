65c4243bcf5b88b0a8daa0db14acd1f1915c44ad
odev01 <odev01@oit.com.np>
2023-03-03 02:24:46 +0545

    Misc update

--- resources/views/livewire/website-product-display.blade.php
+++ resources/views/livewire/website-product-display.blade.php
@@ -12,7 +12,7 @@
   </div>
 
   <div class="row mb-5 border p-2 bg-white shadow">
-    <div class="col-md-6">
+    <div class="col-md-3">
       <div class="d-flex justify-content-center h-100">
         <div class="d-flex flex-column justify-content-center h-100">
           <img class="img-fluid" src="{{ asset('storage/' . $product->image_path) }}" alt="{{ $product->name }}" style="max-height: 250px;">
@@ -32,17 +32,22 @@
         {{ $product->description }}
       </p>
       <hr />
+    </div>
+    <div class="col-md-3">
       <div class="d-flex">
+        @if (false)
         <div class="mt-3-rm mb-3-rm pb-3 mr-4">
           <div class="text-secondary mb-2">
             Quantity
           </div>
           <input type="text" class="py-1" style="font-size: 1.1rem;">
         </div>
+        @endif
         <div class="flex-grow-1 bg-warning-rm">
           <div class="h-100 d-flex flex-column justify-content-center">
-            <button class="btn btn-primary btn-block py-3" style="background-color: #004; font-family: Arial;">
+            <button class="btn btn-primary btn-block py-3 h5" style="background-color: #5a0; font-family: Arial;">
               <span class="h5">
+                <i class="fas fa-shopping-cart mr-1"></i>
                 ADD TO CART
               </span>
             </button>
