28e7757cabeb085aa0e7474f1bc634e3e413f094
odev01 <odev01@oit.com.np>
2023-03-02 20:26:08 +0545

    Misc update\

--- resources/views/livewire/cafe-menu-list.blade.php
+++ resources/views/livewire/cafe-menu-list.blade.php
@@ -1,99 +1,43 @@
 <div>
-  {{-- Categories Bar --}}
-  @if (! $modes['productCategoryProductList'])
-  <div class="">
-
-    @if (false)
-    <span class="mr-3" role="button" wire:click="enterMode('showFullMenuList')" wire:key="{{ rand() }}" style="text-decoration: underline;">
-      Show all
-    </span>
-    @endif
-
-    @if ($products == null || count($products) == 0)
-    @foreach ($productCategories as $productCategory)
-      <span class="mr-3" role="button" wire:click="selectCategory('{{ $productCategory->product_category_id }}')" wire:key="{{ rand() }}" style="text-decoration: underline;">
-        {{ $productCategory->name }}
-      </span>
-    @endforeach
-    @endif
-  </div>
-  @endif
-
   @if ($modes['productCategoryProductList'])
     @livewire ('cafe-menu-product-category-product-list', ['productCategory' => $selectedProductCategory,])
   @else
-    <div class="row">
-        @if ($products != null && count($products) > 0)
-          <div class="table-responsive border bg-white">
-            <table class="table table-hover">
+    <div class="">
 
-              <thead>
-                <tr>
-                  <th colspan="2">
-                    Item
-                  </th>
-                  <th>
-                    Stock
-                  </th>
-                  <th>
-                    Price
-                  </th>
-                  <th>
-                    Action
-                  </th>
-                </tr>
-              </thead>
+      @if (false)
+      <span class="mr-3" role="button" wire:click="enterMode('showFullMenuList')" wire:key="{{ rand() }}" style="text-decoration: underline;">
+        Show all
+      </span>
+      @endif
 
-              <tbody>
-                @foreach ($products as $product)
-                  <tr>
-              <td style="width: 50px;">
-                @if ($product->image_path)
-                  <img src="{{ asset('storage/' . $product->image_path) }}" class="mr-3" style="width: 35px; height: 35px;">
-                @else
-                  <i class="fas fa-dice-d6" class="mr-3" style="width: 35px; height: 35px;"></i>
-                @endif
-              </td>
-                    <td>
-                      {{ $product->name }}
-                    </td>
-                    <td>
-                      @if ($product->stock_applicable == 'yes')
-                        {{ $product->stock_count }}
-                        {{ $product->inventory_unit }}
-                      @else
-                        <div class="text-muted" style="font-size: 0.7rem;">
-                          <i class="fas fa-exclamation-circle mr-1"></i>
-                        </div>
-                      @endif
-                    </td>
-                    <td>
-                      @php echo number_format( $product->selling_price ); @endphp
-                    </td>
-                    <td>
-                      <div class="dropdown">
-                        <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          <i class="fas fa-cog text-secondary"></i>
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                          <button class="dropdown-item" wire:click="$emit('displayProduct', {{ $product->product_id }})">
-                            <i class="fas fa-file text-primary mr-2"></i>
-                            View
-                          </button>
-                          <button class="dropdown-item" wire:click="$emit('updateProduct', {{ $product->product_id }})">
-                            <i class="fas fa-pencil-alt text-primary mr-2"></i>
-                            Update
-                          </button>
-
-                        </div>
-                      </div>
-                    </td>
-                  </tr>
-                @endforeach
-              </tbody>
-            </table>
-          </div>
+      @if ($products == null || count($products) == 0)
+        @if (false)
+        <span class="mr-3" role="button" wire:click="selectCategory('{{ $productCategory->product_category_id }}')" wire:key="{{ rand() }}" style="text-decoration: underline;">
+          {{ $productCategory->name }}
+        </span>
         @endif
+
+        <div class="row">
+          @foreach ($productCategories as $productCategory)
+            <div class="col-md-3 border p-5 mr-3 mb-4 shadow bg-success text-white"
+                wire:click="selectCategory('{{ $productCategory->product_category_id }}')"
+                wire:key="{{ rand() }}"
+                role="button"
+                style="{{--background-color: #cfe;--}}">
+              <div>
+                <h3>
+                  {{ $productCategory->name }}
+                </h3>
+              </div>
+              <div class="text-dark-rm">
+                <i class="fas fa-shopping-cart mr-1"></i>
+                Products: {{ count($productCategory->products) }}
+              </div>
+            </div>
+          @endforeach
+        </div>
+
+      @endif
     </div>
   @endif
 </div>
