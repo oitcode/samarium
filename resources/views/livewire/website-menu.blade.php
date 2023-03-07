d956659b8e3888d3d8b74c1a45e9f52cf089a84a
odev01 <odev01@oit.com.np>
2023-03-08 02:46:09 +0545

    Refactored; Deleting the old ecomm components which were outside ecomm website directory

--- resources/views/livewire/website-menu.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    @foreach ($webpages as $webpage)
-      <a href="{{ route('website-webpage-' . $webpage->permalink) }}">
-        {{ $webpage->permalink }}
-      </a>
-    @endforeach
-</div>
