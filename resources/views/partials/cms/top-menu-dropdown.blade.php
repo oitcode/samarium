9af55703725e7ac4cab0ef6b1c207df7dd768fd6
odev01 <odev01@oit.com.np>
2023-03-16 20:44:16 +0545

    Bugfix; Issue #15; CMS website nav menu dropdown appearance issue

--- resources/views/partials/cms/top-menu-dropdown.blade.php
+++ resources/views/partials/cms/top-menu-dropdown.blade.php
@@ -1,6 +1,9 @@
-<div class="dropdown float-left bg-light">
-  <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton-{{ $cmsNavMenuItem->cms_nav_menu_item_id }}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    {{ $cmsNavMenuItem->name  }}
+<div class="dropdown float-left bg-danger-rm h-100 p-3">
+  <button class="btn-rm btn-light-rm dropdown-toggle p-2-rm bg-white border-0 font-weight-bold"
+      style=""
+      type="button" id="dropdownMenuButton-{{ $cmsNavMenuItem->cms_nav_menu_item_id }}"
+      data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    {{ strtoupper($cmsNavMenuItem->name) }}
   </button>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton-{{ $cmsNavMenuItem->cms_nav_menu_item_id }}">
     @foreach ($cmsNavMenuItem->cmsNavMenuDropdownItems as $cmsNavMenuDropdownItem)
