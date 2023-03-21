564c9e4fc92b7100d8acf586fb26b631ed32157f
odev01 <odev01@oit.com.np>
2023-03-21 09:31:10 +0545

    Misc update; Testing routes; Started

--- routes/web.php
+++ routes/web.php
@@ -242,3 +242,33 @@ Route::get('/dashboard/vat', 'VatController@index')->name('dashboard-vat');
 Route::get('/dashboard/quick-contacts', 'BgcController@quickContacts')->name('dashboard-quick-contacts');
 Route::get('/dashboard/organizing-committee', 'BgcController@organizingCommittee')->name('dashboard-organizing-committee');
 Route::get('/dashboard/sponsors', 'BgcController@sponsors')->name('dashboard-sponsors');
+
+
+/*
+ *-----------------------------------------------------------------------------
+ * Temporary
+ *-----------------------------------------------------------------------------
+ *
+ *
+ *
+ */
+Route::get('/routes', function () {
+    $routeCollection = Route::getRoutes();
+
+    echo "<table style='width:100%'>";
+    echo "<tr>";
+    echo "<td width='10%'><h4>HTTP Method</h4></td>";
+    echo "<td width='10%'><h4>Route</h4></td>";
+    echo "<td width='10%'><h4>Name</h4></td>";
+    echo "<td width='70%'><h4>Corresponding Action</h4></td>";
+    echo "</tr>";
+    foreach ($routeCollection as $value) {
+        echo "<tr>";
+        echo "<td>" . $value->methods()[0] . "</td>";
+        echo "<td>" . $value->uri() . "</td>";
+        echo "<td>" . $value->getName() . "</td>";
+        echo "<td>" . $value->getActionName() . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+});
---
+++ tests/Feature/RouteTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use Illuminate\Support\Facades\Route;
+
+class RouteTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function testAllRoutesAreAvailable()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
