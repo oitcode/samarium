b2bda8484a133bf456502e5c568643a4ed092cd4
odev01 <odev01@oit.com.np>
2023-03-22 17:59:10 +0545

    v0.3.4

--- resources/views/partials/dashboard/mobile-top-menu.blade.php
+++ resources/views/partials/dashboard/mobile-top-menu.blade.php
@@ -144,7 +144,7 @@
             <div class="dropdown-divider"></div>
             <a class="dropdown-item" href="">
               <i class="fas fa-history text-secondary mr-2"></i>
-              v0.3.3
+              v0.3.4
             </a>
             <div class="dropdown-divider"></div>
             <a class="dropdown-item" href="{{ route('logout') }}"
