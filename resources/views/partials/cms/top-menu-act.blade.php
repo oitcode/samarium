5210fe0c7b8a6828af32709e443a5a5e23917006
odev01 <odev01@oit.com.np>
2023-03-25 21:07:48 +0545

    Bugfix; Issue #50

--- resources/views/partials/cms/top-menu-act.blade.php
+++ resources/views/partials/cms/top-menu-act.blade.php
@@ -4,6 +4,12 @@
 
   @foreach ($cmsNavMenu->cmsNavMenuItems()->orderBy('order', 'asc')->get() as $cmsNavMenuItem)
     @if ($cmsNavMenuItem->type == 'item')
+
+      {{-- Do not show if linked webpage's visibility is not public --}}
+      @if ($cmsNavMenuItem->webpage->visibility != 'public')
+        @continue
+      @endif
+
       @include ('partials.cms.top-menu-button', [
         'btnRoute' => 'website-webpage-' . $cmsNavMenuItem->webpage->permalink,
         'iconFaClass' => 'fas fa-building',
