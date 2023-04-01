c76e09743fda90bfa3494203a943efab32bb69d4
odev01 <odev01@oit.com.np>
2023-04-01 12:56:59 +0545

    Bugfix; Issue #74

--- resources/views/auth/passwords/email-bak.blade.php
+++ resources/views/auth/passwords/email-bak.blade.php
@@ -1,37 +1,6 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" style="height: 100% !important;">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('vendor/fontawesome-free/css/all.css') }}" rel="stylesheet">
-
-    <style>
-      html, body {
-        overflow-x: hidden;
-      }
-    </style>
-
-
-    <!-- Livewire -->
-    @livewireStyles
-</head>
-
-<body style="height: 100% !important; background-color: #eee;">
+@section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -75,8 +44,4 @@
         </div>
     </div>
 </div>
-
-  <!-- Livewire scripts -->
-  @livewireScripts
-</body>
-</html>
+@endsection
