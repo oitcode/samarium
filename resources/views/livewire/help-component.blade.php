6054a35cb41aae564616520606c83b7fa65749fe
odev01 <odev01@oit.com.np>
2023-04-03 12:23:24 +0545

    Misc update; Dashboard help page FE

--- resources/views/livewire/help-component.blade.php
+++ resources/views/livewire/help-component.blade.php
@@ -1,79 +1,150 @@
 <div>
 
-  <div class="mb-5 py-5 px-3 bg-primary text-white">
+  @if (true)
+  <div class="mb-3 py-3 px-3 bg-light text-secondary">
     <h1 class="h2">
       <i class="fas fa-question-circle mr-2"></i>
       Help
     </h1>
   </div>
+  @endif
 
-  <div class="my-5">
-    <div class="row">
+  <div class="row">
+    <div class="col-md-8">
+      @if (true)
+      <div class="bg-light text-secondary p-4 mb-0">
+        <h2 class="h3 mb-3 text-secondary-rm">
+          <i class="fas fa-video mr-2"></i>
+          Video tutorials
+        </h2>
+      </div>
+      @endif
 
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-body p-5 bg-white o-text-bigger">
-            <i class="fab fa-github mr-2"></i>
-            Github
+      @if (false)
+      <div class="mb-0">
+
+        <div class="row" style="margin: auto;">
+
+          <div class="col-md-4 p-0">
+            <div class="card o-pulled-up">
+              <div class="card-body p-4 bg-white-rm o-text-bigger">
+                <i class="fas fa-dice-d6 mr-2"></i>
+                Sales entry
+              </div>
+            </div>
           </div>
+
+          <div class="col-md-4 p-0">
+            <div class="card">
+              <div class="card-body p-4 bg-white o-text-bigger">
+                <i class="fas fa-shopping-cart mr-2"></i>
+                <span class="o-text-bigger">
+                </span>
+                Purchase entry
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 p-0">
+            <div class="card">
+              <div class="card-body p-4 bg-white o-text-bigger">
+                <i class="fas fa-tools mr-2"></i>
+                Expense entry
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
+      @endif
+      <div class="mb-4">
 
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-body p-5 bg-white o-text-bigger">
-            <i class="fab fa-youtube mr-2"></i>
-            <span class="o-text-bigger">
-            </span>
-            View tutorials in youtube
+        <div class="row" style="margin: auto;">
+
+          <div class="col-md-4 p-0 bg-warning">
+            <div class="card">
+              <div class="card-body p-4 bg-white o-text-bigger">
+                <i class="fas fa-clone mr-2"></i>
+                <span class="o-text-bigger">
+                </span>
+                Create webpages
+              </div>
+            </div>
           </div>
+
+          <div class="col-md-4 p-0">
+            <div class="card">
+              <div class="card-body p-4 bg-white o-text-bigger">
+                <i class="fas fa-edit mr-2"></i>
+                Create posts
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 p-0">
+            <div class="card o-pulled-up">
+              <div class="card-body p-4 bg-white-rm o-text-bigger">
+                <i class="fas fa-list mr-2"></i>
+                Create post category
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
-
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-body p-5 bg-white o-text-bigger">
-            <i class="fas fa-sms mr-2"></i>
-            Contact support
-          </div>
-        </div>
-      </div>
-
     </div>
-  </div>
+    <div class="col-md-4">
+      @if (true)
+      <div class="bg-light text-secondary p-4 mb-0">
+        <h2 class="h3 mb-3 text-secondary-rm">
+          <i class="fas fa-folder-open mr-2"></i>
+          Other resources
+        </h2>
+      </div>
+      @endif
 
-  <div class="my-5">
-    <div class="row">
+      @if (false)
+      <h2 class="h4 mb-3 text-secondary">
+        Other
+      </h2>
+      @if (false)
+      <p class="text-secondary">
+        Select the topic for which you need help.
+      </p>
+      @endif
+      @endif
 
-      <div class="col-md-4">
-        <div class="card o-pulled-up">
-          <div class="card-body p-5 bg-white-rm o-text-bigger">
-            <i class="fas fa-bars mr-2"></i>
-            Getting Started
+      <div class="mb-4">
+        @if (false)
+        <h2 class="h4 mb-2 text-secondary">
+          Other resources
+        </h2>
+        <p class="text-secondary">
+          View excellent documentation.
+        </p>
+        @endif
+        <div class="row">
+
+          <div class="col-md-12 m-0">
+            <div class="card">
+              <div class="card-body p-4 bg-white o-text-bigger">
+                <i class="fab fa-github mr-2"></i>
+                Github
+              </div>
+            </div>
           </div>
+
+          <div class="col-md-12 m-0">
+            <div class="card">
+              <div class="card-body p-4 bg-white o-text-bigger">
+                <i class="fas fa-user mr-2"></i>
+                Contact support
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
-
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-body p-5 bg-white o-text-bigger">
-            <i class="fas fa-clone mr-2"></i>
-            <span class="o-text-bigger">
-            </span>
-            Create webpages
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-body p-5 bg-white o-text-bigger">
-            <i class="fas fa-edit mr-2"></i>
-            Create posts
-          </div>
-        </div>
-      </div>
-
     </div>
   </div>
 
