e46b7a49520bf6573f3cda2c79881af116a78835
odev01 <odev01@oit.com.np>
2023-04-21 12:50:23 +0545

    Misc update; Glance componenets

--- resources/views/partials/misc/glance-card.blade.php
+++ resources/views/partials/misc/glance-card.blade.php
@@ -1,6 +1,6 @@
 <a href="{{ route($btnRoute) }}">
-  <div class="d-flex flex-column-rm justify-content-between border" style="background-color: #eee;" >
-    <div class="p-3 bg-primary-rm text-white-rm flex-grow-1 d-flex-rm" style="color: #779;">
+  <div class="d-flex flex-column-rm justify-content-between border bg-success" style="{{-- background-color: #254; --}}">
+    <div class="p-3 bg-primary-rm text-white flex-grow-1 d-flex-rm" style="{{-- color: #ccc; --}}">
       <i class="{{ $iconFaClass }} fa-2x mr-2 mt-1"></i>
 
       <div class="mt-3-rm h5">
@@ -9,8 +9,8 @@
     </div>
 
     @if ($btnTextSecondary)
-      <div class="d-flex flex-column justify-content-center p-2 px-3" style="background-color: #ccd;">
-        <div class="h3" style="color: #556;">
+      <div class="d-flex flex-column justify-content-center p-2 px-3 o-darker" style="{{-- background-color: #456; --}}">
+        <div class="h3 text-white" style="{{-- color: #ddd; --}}">
           {{ $btnTextSecondary }}
         </div>
       </div>
