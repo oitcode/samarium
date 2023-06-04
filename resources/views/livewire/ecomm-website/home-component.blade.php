68b7cde0a1373943e616d808a21b8116d50b72ca
odev01 <odev01@oit.com.np>
2023-06-04 16:11:52 +0545

    Ecomm website FE update; Progress

--- resources/views/livewire/ecomm-website/home-component.blade.php
+++ resources/views/livewire/ecomm-website/home-component.blade.php
@@ -76,135 +76,41 @@
 
         <div class="row">
 
-          <div class="col-md-3">
-            <div class="card h-100 shadow">
+          @for ($ii=1; $ii <= 4; $ii++)
+            <div class="col-md-3">
+              <div class="card h-100 shadow">
       
-              <div class="d-flex flex-column justify-content-between h-100 bg-success-rm">
-                <a href="{{ route('website-product-view', [\App\Product::first()->product_id, \App\Product::first()->name]) }}">
-                  <div class="d-flex justify-content-center bg-warning-rm">
-                      @if ($productCategory->image_path)
-                        <img class="img-fluid h-25-rm w-100-rm" src="{{ asset('storage/' . \App\Product::first()->image_path) }}" alt="{{ $productCategory->name }}" style="max-height: 150px; {{--max-width: 100px;--}}">
-                      @else
-                        <i class="fas fa-dice-d6 fa-8x text-muted m-5"></i>
-                      @endif
-                  </div>
-                </a>
+                <div class="d-flex flex-column justify-content-between h-100 bg-success-rm">
+                  <a href="{{ route('website-product-view', [\App\Product::find($ii)->product_id, \App\Product::find($ii)->name]) }}">
+                    <div class="d-flex justify-content-center bg-warning-rm">
+                        @if ($productCategory->image_path)
+                          <img class="img-fluid h-25-rm w-100-rm" src="{{ asset('storage/' . \App\Product::find($ii)->image_path) }}" alt="{{ $productCategory->name }}" style="max-height: 150px; {{--max-width: 100px;--}}">
+                        @else
+                          <i class="fas fa-dice-d6 fa-8x text-muted m-5"></i>
+                        @endif
+                    </div>
+                  </a>
       
-                <div class="d-flex flex-column justify-content-between flex-grow-1 overflow-auto" style="background-color: #f5f5f5;">
-                  <div class="p-2">
-                    <a href="{{ route('website-product-view', [\App\Product::first()->product_id, \App\Product::first()->name]) }}">
-                      <h2 class="h4 font-weight-bold mt-2 mb-2 text-dark" style="font-family: Arial;">
-                        {{ strtoupper(\App\Product::first()->name) }}
-                        <br />
-                        <span class="text-danger ml-1-rm">
-                          Rs {{ \App\Product::find(1)->selling_price }}
-                        </span>
-                      </h2>
-                    </a>
+                  <div class="d-flex flex-column justify-content-between flex-grow-1 overflow-auto" style="background-color: #f5f5f5;">
+                    <div class="p-2">
+                      <a href="{{ route('website-product-view', [\App\Product::find($ii)->product_id, \App\Product::find($ii)->name]) }}">
+                        <h2 class="h4 font-weight-bold mt-2 mb-2 text-dark" style="font-family: Arial;">
+                          {{ strtoupper(\App\Product::find($ii)->name) }}
+                          <br />
+                          <span class="text-danger ml-1-rm">
+                            Rs {{ \App\Product::find($ii)->selling_price }}
+                          </span>
+                        </h2>
+                      </a>
       
-                  </div>
-                </div>
-              </div>
-
-            </div>
-          </div>
-
-          <div class="col-md-3">
-            <div class="card h-100 shadow border-0">
-      
-              <div class="d-flex-rm flex-column-rm justify-content-between-rm h-100">
-                <a href="{{ route('website-product-view', [\App\Product::find(2)->product_id, \App\Product::find(2)->name]) }}">
-                  <div class="d-flex justify-content-center bg-warning-rm">
-                      @if ($productCategory->image_path)
-                        <img class="img-fluid h-25-rm w-100-rm" src="{{ asset('storage/' . \App\Product::find(2)->image_path) }}" alt="{{ $productCategory->name }}" style="max-height: 150px; {{--max-width: 100px;--}}">
-                      @else
-                        <i class="fas fa-dice-d6 fa-8x text-muted m-5"></i>
-                      @endif
-                  </div>
-                </a>
-      
-                <div class="d-flex flex-column justify-content-between flex-grow-1 overflow-auto" style="background-color: #f5f5f5;">
-                  <div class="p-2">
-                    <a href="{{ route('website-product-view', [\App\Product::find(2)->product_id, \App\Product::find(2)->name]) }}">
-                      <h2 class="h4 font-weight-bold mt-2 mb-2 text-dark" style="font-family: Arial;">
-                        {{ strtoupper(\App\Product::find(2)->name) }}
-                        <br />
-                        <span class="text-danger ml-1-rm">
-                          Rs {{ \App\Product::find(2)->selling_price }}
-                        </span>
-                      </h2>
-                    </a>
-      
+                    </div>
                   </div>
                 </div>
-              </div>
 
-            </div>
-          </div>
-          <div class="col-md-3">
-            <div class="card h-100 shadow border-0">
-      
-              <div class="d-flex flex-column justify-content-between h-100 bg-success-rm">
-                <a href="{{ route('website-product-view', [\App\Product::find(3)->product_id, \App\Product::find(3)->name]) }}">
-                  <div class="d-flex justify-content-center bg-warning-rm">
-                      @if ($productCategory->image_path)
-                        <img class="img-fluid h-25-rm w-100-rm" src="{{ asset('storage/' . \App\Product::find(3)->image_path) }}" alt="{{ $productCategory->name }}" style="max-height: 150px; {{--max-width: 100px;--}}">
-                      @else
-                        <i class="fas fa-dice-d6 fa-8x text-muted m-5"></i>
-                      @endif
-                  </div>
-                </a>
-      
-                <div class="d-flex flex-column justify-content-between flex-grow-1 overflow-auto" style="background-color: #f5f5f5;">
-                  <div class="p-2">
-                    <a href="{{ route('website-product-view', [\App\Product::find(3)->product_id, \App\Product::find(3)->name]) }}">
-                      <h2 class="h4 font-weight-bold mt-2 mb-2 text-dark" style="font-family: Arial;">
-                        {{ strtoupper(\App\Product::find(3)->name) }}
-                        <br />
-                        <span class="text-danger ml-1-rm">
-                          Rs {{ \App\Product::find(3)->selling_price }}
-                        </span>
-                      </h2>
-                    </a>
-      
-                  </div>
-                </div>
               </div>
-
             </div>
-          </div>
-          <div class="col-md-3">
-            <div class="card h-100 shadow border-0">
-      
-              <div class="d-flex flex-column justify-content-between h-100 bg-success-rm">
-                <a href="{{ route('website-product-view', [\App\Product::find(4)->product_id, \App\Product::find(4)->name]) }}">
-                  <div class="d-flex justify-content-center bg-warning-rm">
-                      @if ($productCategory->image_path)
-                        <img class="img-fluid h-25-rm w-100-rm" src="{{ asset('storage/' . \App\Product::find(4)->image_path) }}" alt="{{ $productCategory->name }}" style="max-height: 150px; {{--max-width: 100px;--}}">
-                      @else
-                        <i class="fas fa-dice-d6 fa-8x text-muted m-5"></i>
-                      @endif
-                  </div>
-                </a>
-      
-                <div class="d-flex flex-column justify-content-between flex-grow-1 overflow-auto" style="background-color: #f5f5f5;">
-                  <div class="p-2">
-                    <a href="{{ route('website-product-view', [\App\Product::find(4)->product_id, \App\Product::find(4)->name]) }}">
-                      <h2 class="h4 font-weight-bold mt-2 mb-2 text-dark" style="font-family: Arial;">
-                        {{ strtoupper(\App\Product::find(4)->name) }}
-                        <br />
-                        <span class="text-danger ml-1-rm">
-                          Rs {{ \App\Product::find(4)->selling_price }}
-                        </span>
-                      </h2>
-                    </a>
-      
-                  </div>
-                </div>
-              </div>
+          @endfor
 
-            </div>
-          </div>
         </div>
 
         @if (false)
