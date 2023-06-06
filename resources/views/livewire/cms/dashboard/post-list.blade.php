0d1b2116c55bd3a4aeaf179b1b195309e57b4969
odev01 <odev01@oit.com.np>
2023-06-06 22:20:47 +0545

    Misc update; CMS dashboard update; Post component upate; Mobile friendly; Started

--- resources/views/livewire/cms/dashboard/post-list.blade.php
+++ resources/views/livewire/cms/dashboard/post-list.blade.php
@@ -130,125 +130,80 @@
 
     {{-- Show in smaller screens --}}
     <div class="d-md-none">
-      <div class="table-responsive border">
-        <table class="table table-hover table-bordered-rm mb-0">
-          @if (false)
-          <thead>
-            <tr class="{{ env('OC_ASCENT_BG_COLOR') }} {{ env('OC_ASCENT_TEXT_COLOR') }}">
-              <th>
-                @if (false)
-                  <input type="checkbox" class="mr-3">
-                @endif
-                Name
-              </th>
-              <th>
-                Author
-              </th>
+
+      @foreach ($posts as $post)
+        <div class="bg-white border px-3">
+          <div class="h4-rm py-4">
+            <span  wire:click="$emit('displayPost', {{ $post }})" class="h5 text-dark font-weight-bold" role="button">
+              {{ $post->name }}
+            </span>
+
+            <br/ >
+            <br/ >
+            Author:
+            @if ($post->creator)
+              {{ $post->creator->name }}
+            @else
+              <span class="">
+                <i class="fas fa-exclamation-circle text-secondary mr-1"></i>
+                Not set
+              </span>
+            @endif
+
+            <br />
+            Category:
+            @if (count($post->webpageCategories) > 0)
+              @foreach ($post->webpageCategories as $postCategory)
+                <span class="badge badge-primary mr-3">
+                  {{ $postCategory->name }}
+                </span>
+              @endforeach
+            @else
+              <i class="fas fa-exclamation-circle text-secondary mr-2"></i>
+              None
+            @endif
+
+            <br/>
+            Published:
+            {{ $post->created_at->toDateString() }}
+
+            <br/>
+            Visibility:
+            @if ($post->visibility == 'private')
+              <i class="fas fa-user text-secondary mr-2"></i>
+              Private
+            @elseif ($post->visibility == 'public')
+              <span class="text-success">
+              <i class="fas fa-check-circle mr-1"></i>
+                Public
+              </span>
+            @elseif ($post->visibility == null)
+              <i class="fas fa-exclamation-circle text-secondary"></i>
               @if (false)
-              <th>
-                Permalink
-              </th>
+              Not set
               @endif
-              <th>
-                Categories
-              </th>
-              <th>
-                Date
-              </th>
-              <th>
-                Visibility
-              </th>
-              <th>
-                Action
-              </th>
-            </tr>
-          </thead>
-          @endif
-
-          <tbody class="bg-white">
-            @foreach ($posts as $post)
-              <tr>
-                <td class="h4-rm py-4">
-                  @if (false)
-                    <input type="checkbox" class="mr-3">
-                  @endif
-                  <span  wire:click="$emit('displayPost', {{ $post }})" class="h5 text-dark font-weight-bold" role="button">
-                    {{ $post->name }}
-                  </span>
-
-                  <br/ >
-                  <br/ >
-                  Author:
-                  @if ($post->creator)
-                    {{ $post->creator->name }}
-                  @else
-                    <span class="">
-                      <i class="fas fa-exclamation-circle text-secondary mr-1"></i>
-                      Not set
-                    </span>
-                  @endif
-
-                  <br />
-                  Category:
-                  @if (count($post->webpageCategories) > 0)
-                    @foreach ($post->webpageCategories as $postCategory)
-                      <span class="badge badge-primary mr-3">
-                        {{ $postCategory->name }}
-                      </span>
-                    @endforeach
-                  @else
-                    <i class="fas fa-exclamation-circle text-secondary mr-2"></i>
-                    None
-                  @endif
-
-                  <br/>
-                  Published:
-                  {{ $post->created_at->toDateString() }}
-
-                  <br/>
-                  Visibility:
-                  @if ($post->visibility == 'private')
-                    <i class="fas fa-user text-secondary mr-2"></i>
-                    Private
-                  @elseif ($post->visibility == 'public')
-                    <span class="text-success">
-                    <i class="fas fa-check-circle mr-1"></i>
-                      Public
-                    </span>
-                  @elseif ($post->visibility == null)
-                    <i class="fas fa-exclamation-circle text-secondary"></i>
-                    @if (false)
-                    Not set
-                    @endif
-                  @else
-                    Whoops!
-                  @endif
-                </td>
-                <td>
-                  @if (false)
-                  <span class="btn btn-light mr-3">
-                    <i class="fas fa-pencil-alt"></i>
-                  </span>
-                  @endif
-                  <span class="btn btn-light mr-3" wire:click="deletePost({{ $post }})">
-                    <i class="fas fa-trash"></i>
-                  </span>
-                  @if ($modes['delete'])
-                    @if ($deletingPost->webpage_id == $post->webpage_id)
-                      <span class="btn btn-danger mr-3" wire:click="confirmDeletePost">
-                        Confirm delete
-                      </span>
-                      <span class="btn btn-light mr-3" wire:click="deletePostCancel">
-                        Cancel
-                      </span>
-                    @endif
-                  @endif
-                </td>
-              </tr>
-            @endforeach
-          </tbody>
-        </table>
-      </div>
+            @else
+              Whoops!
+            @endif
+          </div>
+          <div>
+            <span class="btn btn-light mr-3 mb-3" wire:click="deletePost({{ $post }})">
+              <i class="fas fa-trash mr-1"></i>
+              Delete post
+            </span>
+            @if ($modes['delete'])
+              @if ($deletingPost->webpage_id == $post->webpage_id)
+                <span class="btn btn-danger mr-3 mb-3" wire:click="confirmDeletePost">
+                  Confirm delete
+                </span>
+                <span class="btn btn-light mr-3 mb-3" wire:click="deletePostCancel">
+                  Cancel
+                </span>
+              @endif
+            @endif
+          </div>
+        </div>
+      @endforeach
     </div>
 
     {{-- Pagination links --}}
