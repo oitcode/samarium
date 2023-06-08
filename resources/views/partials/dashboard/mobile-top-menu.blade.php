c9c8a4c432b2566c9eff0d9341fc8c410962cbfd
odev01 <odev01@oit.com.np>
2023-06-08 10:38:51 +0545

    Misc update; Dashboard mob top menu FE update; Progress

--- resources/views/partials/dashboard/mobile-top-menu.blade.php
+++ resources/views/partials/dashboard/mobile-top-menu.blade.php
@@ -26,98 +26,77 @@
       @endif
 
       @if (preg_match("/shop/i", env('MODULES')))
-        <li class="nav-item border bg-success text-white p-3">
-          <a class="nav-link text-white" href="{{ route('sale') }}">
-            @if (env('CMP_TYPE') == 'cafe')
-              <i class="fas fa-skating mr-3"></i>
-              Takeaway
-            @else
-              <i class="fas fa-dice-d6 mr-3"></i>
-              Sales
-            @endif
-          </a>
-        </li>
-
-        @if (env('CMP_TYPE') == 'cafe')
-          <li class="nav-item border bg-success text-white p-3">
-            <a class="nav-link text-white" href="{{ route('cafesale') }}">
-              <i class="fas fa-table mr-3"></i>
-              Tables
-            </a>
-          </li>
-        @endif
-
-        @can ('is-admin')
-        <li class="nav-item border bg-success text-white p-3">
-          <a class="nav-link text-white" href="{{ route('menu') }}">
-            <i class="fas fa-list mr-3"></i>
-            @if (env('CMP_TYPE') == 'cafe')
-              Menu
-            @else
-              Products
-            @endif
-          </a>
-        </li>
-        @endcan
-
-        @can ('is-admin')
-        <li class="nav-item border bg-success text-white p-3">
-          <a class="nav-link text-white" href="{{ route('daybook') }}">
-            <i class="fas fa-book mr-3"></i>
-            Daybook
-          </a>
-        </li>
-        @endcan
-
-        @can ('is-admin')
-        <li class="nav-item border bg-success text-white p-3">
-          <a class="nav-link text-white" href="{{ route('weekbook') }}">
-            <i class="fas fa-book mr-3"></i>
-            Weekbook
-          </a>
-        </li>
-        @endcan
-
-        @can ('is-admin')
-        <li class="nav-item border bg-success text-white p-3">
-          <a class="nav-link text-white" href="{{ route('online-order') }}">
-            <i class="fas fa-cloud-download-alt mr-3"></i>
-            Weborder
-          </a>
-        </li>
-        @endcan
-
-
-        @can ('is-admin')
         <li class="nav-item dropdown bg-success text-white p-3 border">
-          <a class="nav-link dropdown-toggle text-white" href="#" id="mobTopMenuDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <a class="nav-link dropdown-toggle text-white" href="#" id="mobTopMenuShopDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <i class="fas fa-list text-secondry mr-2"></i>
-            More
+            Shop
           </a>
-          <div class="dropdown-menu" aria-labelledby="mobTopMenuDropdown1">
+          <div class="dropdown-menu" aria-labelledby="mobTopMenuShopDropdown">
+            <a class="dropdown-item py-2" href="{{ route('sale') }}">
+              @if (env('CMP_TYPE') == 'cafe')
+                <i class="fas fa-skating mr-3"></i>
+                Takeaway
+              @else
+                <i class="fas fa-dice-d6 mr-3"></i>
+                Sales
+              @endif
+            </a>
+
+            @if (env('CMP_TYPE') == 'cafe')
+              <a class="dropdown-item py-2" href="{{ route('cafesale') }}">
+                <i class="fas fa-table-d6 mr-3"></i>
+                Tables
+              </a>
+            @endif
+
+            <a class="dropdown-item py-2" href="{{ route('cafesale') }}">
+              <i class="fas fa-list mr-3"></i>
+              @if (env('CMP_TYPE') == 'cafe')
+                Menu
+              @else
+                Products
+              @endif
+            </a>
+            <a class="dropdown-item py-2" href="{{ route('daybook') }}">
+              <i class="fas fa-book mr-3"></i>
+              Daybook
+            </a>
+            <a class="dropdown-item py-2" href="{{ route('weekbook') }}">
+              <i class="fas fa-book mr-3"></i>
+              Weekbook
+            </a>
+            <a class="dropdown-item py-2" href="{{ route('online-order') }}">
+              <i class="fas fa-cloud-download-alt mr-3"></i>
+              Weborder
+            </a>
+
+            @if (true)
+            <div class="dropdown-divider"></div>
+            @endif
+
             <a class="dropdown-item" href="{{ route('dashboard-purchase') }}">
-              <i class="fas fa-shopping-cart text-secondary mr-2"></i>
+              <i class="fas fa-shopping-cart mr-3"></i>
               Purchase
             </a>
             <a class="dropdown-item" href="{{ route('dashboard-expense') }}">
-              <i class="fas fa-wrench text-secondary mr-2"></i>
+              <i class="fas fa-wrench mr-3"></i>
               Expense
             </a>
             <a class="dropdown-item" href="{{ route('dashboard-vendor') }}">
-              <i class="fas fa-users text-secondary mr-2"></i>
+              <i class="fas fa-users mr-3"></i>
               Vendors
             </a>
             <a class="dropdown-item" href="{{ route('dashboard-report') }}">
-              <i class="fas fa-chart-line text-secondary mr-2"></i>
+              <i class="fas fa-chart-line text-secondary mr-3"></i>
               Report
             </a>
             <a class="dropdown-item" href="{{ route('dashboard-inventory') }}">
-              <i class="fas fa-dolly text-secondary mr-2"></i>
+              <i class="fas fa-dolly mr-3"></i>
               Inventory
             </a>
+
           </div>
         </li>
-        @endcan
       @endif
 
       {{-- CMS options --}}
