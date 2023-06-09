704f0c3d5c0fe195b720be850e7811ea5edcf92e
odev01 <odev01@oit.com.np>
2023-06-09 14:49:27 +0545

    Misc update; Dashboard user component FE update;

--- resources/views/livewire/user/user-component.blade.php
+++ resources/views/livewire/user/user-component.blade.php
@@ -1,20 +1,21 @@
 <div class="mb-4">
 
   {{-- Toolbar --}}
+  {{-- Show in bigger screens --}}
   <div class="mb-3 p-2 d-none d-md-block bg-dark-rm">
 
     @include ('partials.dashboard.tool-bar-button-pill', [
         'btnClickMethod' => "enterMode('createUserMode')",
         'btnIconFaClass' => 'fas fa-plus-circle',
         'btnText' => 'Create',
-        'btnCheckMode' => 'createPostMode',
+        'btnCheckMode' => 'createUserMode',
     ])
 
     @include ('partials.dashboard.tool-bar-button-pill', [
         'btnClickMethod' => "enterMode('listUserMode')",
         'btnIconFaClass' => 'fas fa-list',
         'btnText' => 'List',
-        'btnCheckMode' => 'listPostMode',
+        'btnCheckMode' => 'listUserMode',
     ])
 
     @if ($modes['displayUserMode'])
@@ -32,6 +33,43 @@
         'btnText' => 'Clear modes',
         'btnCheckMode' => '',
     ])
+
+    @include ('partials.dashboard.spinner-button')
+
+    <div class="clearfix">
+    </div>
+  </div>
+
+  {{-- Show in smaller screens --}}
+  <div class="mb-3 p-2 d-md-none bg-dark-rm">
+
+    @include ('partials.dashboard.tool-bar-button-pill', [
+        'btnClickMethod' => "enterMode('createUserMode')",
+        'btnIconFaClass' => 'fas fa-plus-circle',
+        'btnText' => 'Create',
+        'btnCheckMode' => 'createUserMode',
+    ])
+
+    @include ('partials.dashboard.tool-bar-button-pill', [
+        'btnClickMethod' => "enterMode('listUserMode')",
+        'btnIconFaClass' => 'fas fa-list',
+        'btnText' => 'List',
+        'btnCheckMode' => 'listUserMode',
+    ])
+
+    <div class="d-inline-block float-right">
+      <div class="dropdown">
+        <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-cog text-secondary"></i>
+        </button>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton" style="font-size: 1rem;">
+          <button class="dropdown-item py-2" wire:click="clearModes">
+            <i class="fas fa-eraser text-primary mr-2"></i>
+            Clear modes
+          </button>
+        </div>
+      </div>
+    </div>
 
     @include ('partials.dashboard.spinner-button')
 
--- resources/views/livewire/user/user-list.blade.php
+++ resources/views/livewire/user/user-list.blade.php
@@ -1,59 +1,109 @@
 <div class="border bg-white">
 
   @if (!is_null($users) && count($users) > 0)
-    <div class="table-responsive">
-      <table class="table table-hover table-valign-middle">
-        <thead>
-          <tr class="{{ env('OC_ASCENT_BG_COLOR', 'bg-success') }}
-              {{ env('OC_ASCENT_TEXT_COLOR', 'text-white') }}
-              p-4" style="font-size: 1rem;">
-            <th>User ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Role</th>
-            <th>Action</th>
-          </tr>
-        </thead>
+    {{-- Show in bigger screens --}}
+    <div class="d-none d-md-block">
+      <div class="table-responsive">
+        <table class="table table-hover table-valign-middle">
+          <thead>
+            <tr class="{{ env('OC_ASCENT_BG_COLOR', 'bg-success') }}
+                {{ env('OC_ASCENT_TEXT_COLOR', 'text-white') }}
+                p-4" style="font-size: 1rem;">
+              <th>User ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
   
-        <tbody>
-          @foreach($users as $user)
-          <tr>
-            <td>
-              {{ $user->id }}
-            </td>
+          <tbody>
+            @foreach($users as $user)
+            <tr>
+              <td>
+                {{ $user->id }}
+              </td>
 
-            <td class="font-weight-bold" wire:click="$emit('displayUser', {{ $user }})" role="button">
-              {{ $user->name }}
-            </td>
+              <td class="font-weight-bold" wire:click="$emit('displayUser', {{ $user }})" role="button">
+                {{ $user->name }}
+              </td>
 
-            <td>
-              {{ $user->email }}
-            </td>
+              <td>
+                {{ $user->email }}
+              </td>
 
-            <td>
-              {{ $user->role }}
-            </td>
+              <td>
+                {{ $user->role }}
+              </td>
 
-            <td>
-              <span class="btn btn-tool text-danger btn-sm" wire:click="deleteUser({{ $user }})">
-                <i class="fas fa-trash mr-1"></i>
-                Delete
-              </span>
-              @if ($modes['delete'])
-                @if ($deletingUser->id == $user->id)
-                  <span class="btn btn-danger mr-3" wire:click="confirmDeleteUser">
-                    Confirm delete
-                  </span>
-                  <span class="btn btn-light mr-3" wire:click="deleteUserCancel">
-                    Cancel
-                  </span>
+              <td>
+                <span class="btn btn-tool text-danger btn-sm" wire:click="deleteUser({{ $user }})">
+                  <i class="fas fa-trash mr-1"></i>
+                  Delete
+                </span>
+                @if ($modes['delete'])
+                  @if ($deletingUser->id == $user->id)
+                    <span class="btn btn-danger mr-3" wire:click="confirmDeleteUser">
+                      Confirm delete
+                    </span>
+                    <span class="btn btn-light mr-3" wire:click="deleteUserCancel">
+                      Cancel
+                    </span>
+                  @endif
                 @endif
+              </td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    {{-- Show in smaller screens --}}
+    <div class="d-md-none">
+
+      @foreach ($users as $user)
+        <div class="bg-white border px-3">
+          <div class="h4-rm py-4">
+            <span  wire:click="$emit('displayUser', {{ $user }})" class="h5 text-dark font-weight-bold" role="button">
+              {{ \Illuminate\Support\Str::limit($user->name, 60, $end=' ...') }}
+            </span>
+
+            <br/ >
+            <br/ >
+            Email: {{ $user->email }} 
+            <br/>
+            Role:
+            @if ($user->role == 'admin')
+              <span class="badge badge-primary mx-2">
+                Admin
+              </span>
+            @else
+              <span class="badge badge-secondary mx-2">
+                Standard
+              </span>
+            @endif
+
+            <br />
+          </div>
+          <div>
+            <span class="btn btn-light mr-3 mb-3" wire:click="deleteUser({{ $user }})">
+              <i class="fas fa-trash mr-1"></i>
+              Delete user
+            </span>
+            @if ($modes['delete'])
+              @if ($deletingUser->id == $user->id)
+                <span class="btn btn-danger mr-3 mb-3" wire:click="confirmDeleteUser">
+                  Confirm delete
+                </span>
+                <span class="btn btn-light mr-3 mb-3" wire:click="deleteUserCancel">
+                  Cancel
+                </span>
               @endif
-            </td>
-          </tr>
-          @endforeach
-        </tbody>
-      </table>
+            @endif
+          </div>
+        </div>
+      @endforeach
     </div>
   @else
     <div class="p-2 text-muted">
