a92ae045ffd7f9d6ce0e788dc1ebb521c1c80f49
odev01 <odev01@oit.com.np>
2023-06-09 14:33:26 +0545

    Misc update; Dashboard Todo Mob FE update;

--- resources/views/livewire/todo-component.blade.php
+++ resources/views/livewire/todo-component.blade.php
@@ -1,6 +1,7 @@
 <div class="p-3 p-md-0">
 
   {{-- Top tool bar --}}
+  {{-- Show in bigger screens --}}
   <div class="mb-3 p-2 d-none d-md-block bg-dark-rm">
     @include ('partials.dashboard.tool-bar-button-pill', [
         'btnClickMethod' => "enterMode('createMode')",
@@ -15,6 +16,43 @@
         'btnText' => 'List',
         'btnCheckMode' => 'listMode',
     ])
+
+    @include ('partials.dashboard.spinner-button')
+
+    <div class="clearfix">
+    </div>
+  </div>
+
+  {{-- Show in smaller screens --}}
+  <div class="mb-3 p-2 d-md-none bg-dark-rm">
+
+    @include ('partials.dashboard.tool-bar-button-pill', [
+        'btnClickMethod' => "enterMode('createMode')",
+        'btnIconFaClass' => 'fas fa-plus-circle',
+        'btnText' => 'Create',
+        'btnCheckMode' => 'createMode',
+    ])
+
+    @include ('partials.dashboard.tool-bar-button-pill', [
+        'btnClickMethod' => "enterMode('listMode')",
+        'btnIconFaClass' => 'fas fa-list',
+        'btnText' => 'List',
+        'btnCheckMode' => 'listMode',
+    ])
+
+    <div class="d-inline-block float-right">
+      <div class="dropdown">
+        <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-cog text-secondary"></i>
+        </button>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton" style="font-size: 1rem;">
+          <button class="dropdown-item py-2" wire:click="clearModes">
+            <i class="fas fa-eraser text-primary mr-2"></i>
+            Clear modes
+          </button>
+        </div>
+      </div>
+    </div>
 
     @include ('partials.dashboard.spinner-button')
 
