7efd04d710bf841a73cebafb73dc947b4310ab9b
odev01 <odev01@oit.com.np>
2023-06-09 10:41:28 +0545

    Misc update; CMS website FE update for welcome screen; Update

--- resources/views/cms/website/base.blade.php
+++ resources/views/cms/website/base.blade.php
@@ -66,17 +66,66 @@
 </head>
 <body style="">
   @if (! $company)
-    <div class="p-5 bg-danger text-white text-center">
-      <h3 class="h5 font-weight-bold">
-        Create Company in dashboard ...
-      </h3>
+    <div class="d-flex justify-content-center h-100">
+      <div class="d-flex flex-column justify-content-center h-100">
+        <div class="mt-3 d-flex justify-content-center">
+          <div>
+            <h1>
+              <strong>OIT CMS</strong>
+            </h1>
+            <p class="text-muted">
+              CMS | Ecommerce | Web
+            </p>
+          </div>
+        </div>
+        <div class="my-3">
+          <div class="p-5-rm text-center border shadow" style="background-color: #eaeaea;">
+            <div class="p-3">
+              <h2>
+                Welcome
+              </h2>
+              <div class="row">
+                <div class="col-md-4">
+                </div>
+                <div class="col-md-4 text-secondary">
+                  Welcome to OIT CMS software. You can create websites quickly
+                  and easily using our software. We hope you like our software.
+                </div>
+                <div class="col-md-4">
+                </div>
+              </div>
+            </div>
 
-      <br />
-      <br />
+            <hr />
 
-      <h3 class="h5 font-weight-bold">
-        &copy; OIT | www.oit.com.np
-      </h3>
+            <h2 class="text-danger">
+              Company not set
+            </h2>
+            <p class="text-secondary">
+              Please start by creating your company in dashboard.
+            </p>
+            <h3 class="h5 font-weight-bold">
+              <a href="./dashboard" class="btn btn-success">
+                Visit dashboard
+              </a>
+            </h3>
+
+            <br />
+            <br />
+
+            <div class="bg-dark text-white p-3">
+              CMS | Ecommerce | Web
+            </div>
+          </div>
+
+          <div class="my-4 text-center text-secondary">
+            <h3 class="h6 font-weight-bold">
+              &copy; 2023 OIT | oit.com.np
+            </h3>
+          </div>
+
+        </div>
+      </div>
     </div>
   @else
   <div class="p-0">
