a1591ed4b28b5686f605dd0499aee8671cd80d9c
odev01 <odev01@oit.com.np>
2023-06-14 12:21:32 +0545

    Misc update; Dashboard purchase and expense list FE update; Mob friendly; Update

--- resources/views/livewire/expense-list.blade.php
+++ resources/views/livewire/expense-list.blade.php
@@ -1,7 +1,43 @@
 <div>
 
-  <div class="mt-2-rm mb-3 text-secondary py-3-rm d-flex bg-warning-rm" style="font-size: 1rem;">
+  {{-- Show in bigger screens --}}
+  <div class="mt-2-rm mb-3 text-secondary py-3-rm d-flex-rm bg-warning-rm d-none d-md-block" style="font-size: 1rem;">
+    <div class="d-flex">
+      <div class="mt-0 text-secondary py-3-rm mr-3" style="font-size: 1.3rem;">
+        <button class="btn {{ env('OC_ASCENT_BTN_COLOR') }}" wire:click="setPreviousDay">
+          <i class="fas fa-arrow-left"></i>
+        </button>
+        <button class="btn {{ env('OC_ASCENT_BTN_COLOR') }}" wire:click="setNextDay">
+          <i class="fas fa-arrow-right"></i>
+        </button>
+      </div>
+      <div>
+        <input type="date" wire:model.defer="startDate" class="mr-3" />
+        <input type="date" wire:model.defer="endDate" class="mr-3" />
 
+        <button class="btn {{ env('OC_ASCENT_BTN_COLOR') }} mr-3" wire:click="getExpensesForDateRange">
+          Go
+        </button>
+      </div>
+
+      <button wire:loading class="btn">
+        <div class="spinner-border text-info mr-3" role="status">
+          <span class="sr-only">Loading...</span>
+        </div>
+      </button>
+      <div class="d-flex justify-content-end flex-grow-1">
+        <div class="pl-2 font-weight-bold pr-3 border py-2 bg-white" style="font-size: 1rem;">
+          <span class="text-dark" style="font-size: 1.5rem;">
+          Rs
+          @php echo number_format( $total, 2 ); @endphp
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  {{-- Show in smaller screens --}}
+  <div class="mt-2-rm mb-3 text-secondary py-3-rm  bg-warning-rm d-md-none" style="font-size: 1rem;">
     <div class="mt-0 text-secondary py-3-rm mr-3" style="font-size: 1.3rem;">
       <button class="btn {{ env('OC_ASCENT_BTN_COLOR') }}" wire:click="setPreviousDay">
         <i class="fas fa-arrow-left"></i>
@@ -24,7 +60,7 @@
         <span class="sr-only">Loading...</span>
       </div>
     </button>
-    <div class="d-flex justify-content-end flex-grow-1">
+    <div class="d-flex flex-grow-1">
       <div class="pl-2 font-weight-bold pr-3 border py-2 bg-white" style="font-size: 1rem;">
         <span class="text-dark" style="font-size: 1.5rem;">
         Rs
--- resources/views/livewire/purchase-list.blade.php
+++ resources/views/livewire/purchase-list.blade.php
@@ -1,6 +1,46 @@
 <div>
 
-  <div class="mt-2 mb-3 text-secondary py-3-rm d-flex bg-warning-rm" style="font-size: 1rem;">
+  <div class="mt-2 mb-3 text-secondary py-3-rm bg-warning-rm d-none d-md-block" style="font-size: 1rem;">
+
+    <div class="d-flex">
+      <div class="mt-0 text-secondary py-3-rm mr-3" style="font-size: 1.3rem;">
+        <button class="btn {{ env('OC_ASCENT_BTN_COLOR') }}" wire:click="setPreviousDay">
+          <i class="fas fa-arrow-left"></i>
+        </button>
+        <button class="btn {{ env('OC_ASCENT_BTN_COLOR') }}" wire:click="setNextDay">
+          <i class="fas fa-arrow-right"></i>
+        </button>
+      </div>
+      <div>
+        <input type="date" wire:model.defer="startDate" class="mr-3" />
+        <input type="date" wire:model.defer="endDate" class="mr-3" />
+
+        <button class="btn {{ env('OC_ASCENT_BTN_COLOR') }} mr-3" wire:click="getPurchasesForDateRange">
+          Go
+        </button>
+      </div>
+
+      <button wire:loading class="btn">
+        <div class="spinner-border text-info mr-3" role="status">
+          <span class="sr-only">Loading...</span>
+        </div>
+      </button>
+      <div class="d-flex justify-content-end flex-grow-1">
+        <div class="pl-2 font-weight-bold pr-3 border py-2 bg-white" style="font-size: 1rem;">
+          <span class="text-dark" style="font-size: 1.5rem;">
+          Rs
+          @if (is_numeric($total) && ctype_digit((string) $total))
+            @php echo number_format( $total ); @endphp
+          @else
+            @php echo number_format( $total, 2 ); @endphp
+          @endif
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+  {{-- Show in smaller screens --}}
+  <div class="mt-2 mb-3 text-secondary py-3-rm bg-warning-rm d-md-none" style="font-size: 1rem;">
 
     <div class="mt-0 text-secondary py-3-rm mr-3" style="font-size: 1.3rem;">
       <button class="btn {{ env('OC_ASCENT_BTN_COLOR') }}" wire:click="setPreviousDay">
@@ -24,7 +64,7 @@
         <span class="sr-only">Loading...</span>
       </div>
     </button>
-    <div class="d-flex justify-content-end flex-grow-1">
+    <div class="d-flex justify-content-start flex-grow-1">
       <div class="pl-2 font-weight-bold pr-3 border py-2 bg-white" style="font-size: 1rem;">
         <span class="text-dark" style="font-size: 1.5rem;">
         Rs
