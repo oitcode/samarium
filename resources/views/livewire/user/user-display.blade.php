3057b90db7a3ff978d6c1f5df1ac9103d0140e51
odev01 <odev01@oit.com.np>
2023-06-15 09:26:59 +0545

    Misc update; Dashboard user display FE update

--- resources/views/livewire/user/user-display.blade.php
+++ resources/views/livewire/user/user-display.blade.php
@@ -1,5 +1,6 @@
 <div>
-  <div class="row">
+
+  <div class="row mb-4">
     <div class="col-md-8">
       <div class="bg-white p-3 border">
 
@@ -34,29 +35,29 @@
           <div class="col-md-12 d-flex flex-column justify-content-center">
 
             <div class="table-responsive border-rm">
-              <table class="table border-bottom">
+              <table class="table border-bottom-rm">
                 <tbody>
                   <tr>
-                    <th>
+                    <th class="border-0">
                       Name
                     </th>
-                    <td>
+                    <td class="border-0">
                       {{ $user->name }}
                     </td>
                   </tr>
                   <tr>
-                    <th>
+                    <th class="border-0">
                       Created date
                     </th>
-                    <td>
+                    <td class="border-0">
                       {{ $user->created_at->toDateString() }}
                     </td>
                   </tr>
                   <tr>
-                    <th>
+                    <th class="border-0">
                       Role
                     </th>
-                    <td>
+                    <td class="border-0">
                       @if ($user->role == 'standard')
                         <span class="badge badge-pill badge-success">
                           Standard
@@ -68,6 +69,81 @@
                       @else
                         {{ $user->role }}
                       @endif
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            @if (false)
+            <div class="my-5">
+              <h2 class="h4 font-weight-bold mb-3">
+                @if (false)
+                <i class="fas fa-comment mr-1"></i>
+                @endif
+                Description
+              </h2>
+              <p>
+                {{ $user->name }}
+              </p>
+            </div>
+            @endif
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 px-3">
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-8">
+      <div class="bg-white p-3 border">
+
+        <div class="px-3-rm mb-5">
+          <div class="d-flex justify-content-between">
+            <div>
+              <span class="h5 font-weight-bold">
+                Entries
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-4">
+            <div class="d-flex justify-content-center">
+              <div class="d-flex flex-column justify-content-center h-100">
+              </div>
+            </div>
+          </div>
+          <div class="col-md-12 d-flex flex-column justify-content-center">
+
+            <div class="table-responsive border-rm">
+              <table class="table border-bottom-rm">
+                <tbody>
+                  <tr>
+                    <th class="border-0">
+                      Sales
+                    </th>
+                    <td class="border-0">
+                      23
+                    </td>
+                  </tr>
+                  <tr>
+                    <th class="border-0">
+                      Purchase
+                    </th>
+                    <td class="border-0">
+                      34
+                    </td>
+                  </tr>
+                  <tr>
+                    <th class="border-0">
+                      Expense
+                    </th>
+                    <td class="border-0">
+                      12
                     </td>
                   </tr>
                 </tbody>
