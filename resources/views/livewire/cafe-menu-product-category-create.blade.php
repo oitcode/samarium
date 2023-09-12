2bec99c7500416d645057f513291304985a145dd
odev01 <odev01@oit.com.np>
2023-09-12 13:28:26 +0545

    Misc updates; Misc FE updates; Updates

--- resources/views/livewire/cafe-menu-product-category-create.blade.php
+++ resources/views/livewire/cafe-menu-product-category-create.blade.php
@@ -1,5 +1,5 @@
 <div class="card shadow-sm">
-  <div class="card-body p-3">
+  <div class="card-body p-3-rm">
 
     <h1 class="text-white-rm" style="font-size: 1.3rem;">
       Add product category
@@ -38,14 +38,10 @@
       @error('image') <span class="text-danger">{{ $message }}</span> @enderror
     </div>
 
-    <div class="p-3 m-0" {{--style="background-image: linear-gradient(to right, white, #abc);"--}}>
-      <button class="btn btn-lg badge-pill btn-success mr-3" wire:click="store">
-        Confirm
-      </button>
+    <div class="py-3 m-0" {{--style="background-image: linear-gradient(to right, white, #abc);"--}}>
 
-      <button class="btn btn-lg btn-danger badge-pill mr-3" wire:click="$emit('exitCreateProductCategoryMode')">
-        Cancel
-      </button>
+      @include ('partials.button-store')
+      @include ('partials.button-cancel', ['clickEmitEventName' => 'exitCreateProductCategoryMode',])
 
       <button wire:loading class="btn">
         <span class="spinner-border text-info mr-3" role="status">
