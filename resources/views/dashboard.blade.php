362850130ef54a4b4ab25eb99bf2c53f2d0541ef
odev01 <odev01@oit.com.np>
2023-10-02 21:58:50 +0545

    Misc updates; Misc app top menu mobile  Updates;

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,43 +8,45 @@
     Dashboard
   </x-component-header>
 
-  <div class="row">
-    <div class="col-md-6">
-      @if (preg_match("/cms/i", env('MODULES')))
-        {{-- CMS glance --}}
-        <div class="mb-4">
-          @livewire ('cms.dashboard.cms-glance')
-        </div>
-      @endif
+  <div class="d-none d-md-block">
+    <div class="row">
+      <div class="col-md-6">
+        @if (preg_match("/cms/i", env('MODULES')))
+          {{-- CMS glance --}}
+          <div class="mb-4">
+            @livewire ('cms.dashboard.cms-glance')
+          </div>
+        @endif
 
-    </div>
-
-    <div class="col-md-6">
-      @if (preg_match("/shop/i", env('MODULES')))
-        {{-- Shop glance --}}
-        <div class="mb-4">
-          @livewire ('shop.dashboard.shop-glance')
-        </div>
-      @endif
-    </div>
-  </div>
-
-  <div class="row">
-    <div class="col-md-6">
-      @livewire ('todo.dashboard.todo-glance')
-    </div>
-    <div class="col-md-6">
-      {{-- Product glance --}}
-      <div class="mb-4">
-        @livewire ('shop.dashboard.product-glance')
       </div>
 
-      @if (true)
-      <div class="my-4">
-        @livewire ('transactions-navigator')
+      <div class="col-md-6">
+        @if (preg_match("/shop/i", env('MODULES')))
+          {{-- Shop glance --}}
+          <div class="mb-4">
+            @livewire ('shop.dashboard.shop-glance')
+          </div>
+        @endif
       </div>
-      @endif
-    
+    </div>
+
+    <div class="row">
+      <div class="col-md-6">
+        @livewire ('todo.dashboard.todo-glance')
+      </div>
+      <div class="col-md-6">
+        {{-- Product glance --}}
+        <div class="mb-4">
+          @livewire ('shop.dashboard.product-glance')
+        </div>
+
+        @if (true)
+        <div class="my-4">
+          @livewire ('transactions-navigator')
+        </div>
+        @endif
+      
+      </div>
     </div>
   </div>
 
--- resources/views/partials/dashboard/mobile-dashboard-main-links.blade.php
+++ resources/views/partials/dashboard/mobile-dashboard-main-links.blade.php
@@ -2,7 +2,7 @@
 <div class="row p-3 justify-content-between-rm bg-light-rm mb-4 d-md-none" style="margin: auto;">
 
   <div class="col-6 mr-3-rm mb-3 p-0 pr-3">
-    <a href="{{ route('sale') }}" class="btn btn-success p-3 w-100" style="font-size: 1.3rem;">
+    <a href="{{ route('sale') }}" class="btn btn-light p-3 w-100" style="font-size: 1.3rem;">
       @if (env('CMP_TYPE') == 'cafe')
         <i class="fas fa-skating mr-3"></i>
         <br/>
@@ -17,7 +17,7 @@
 
   @if (env('CMP_TYPE') == 'cafe')
   <div class="col-6 mr-3-rm mb-3 p-0 pr-3">
-    <a href="{{ route('cafesale') }}" class="btn btn-success p-3 w-100" style="font-size: 1.3rem;">
+    <a href="{{ route('cafesale') }}" class="btn btn-light p-3 w-100" style="font-size: 1.3rem;">
       <i class="fas fa-table mr-3"></i>
       <br/>
       Tables
@@ -27,7 +27,7 @@
 
   @can ('is-admin')
   <div class="col-6 mr-3-rm mb-3 p-0 pr-3">
-    <a href="{{ route('daybook') }}" class="btn btn-success p-3 w-100" style="font-size: 1.3rem;">
+    <a href="{{ route('daybook') }}" class="btn btn-light p-3 w-100" style="font-size: 1.3rem;">
       <i class="fas fa-book mr-3"></i>
       <br/>
       Daybook
@@ -37,7 +37,7 @@
 
   @can ('is-admin')
   <div class="col-6 mr-3-rm mb-3 p-0 pr-3">
-    <a href="{{ route('weekbook') }}" class="btn btn-success p-3 w-100" style="font-size: 1.3rem;">
+    <a href="{{ route('weekbook') }}" class="btn btn-light p-3 w-100" style="font-size: 1.3rem;">
       <i class="fas fa-book mr-3"></i>
       <br/>
       Weekbook
@@ -47,7 +47,7 @@
 
   @can ('is-admin')
   <div class="col-6 mr-3-rm mb-3 p-0 pr-3">
-    <a href="{{ route('menu') }}" class="btn btn-success p-3 w-100" style="font-size: 1.3rem;">
+    <a href="{{ route('menu') }}" class="btn btn-light p-3 w-100" style="font-size: 1.3rem;">
       @if (env('CMP_TYPE') == 'cafe')
         <i class="fas fa-list mr-3"></i>
         <br/>
@@ -63,7 +63,7 @@
 
   @can ('is-admin')
   <div class="col-6 mr-3-rm mb-3 p-0 pr-3">
-    <a href="{{ route('customer') }}" class="btn btn-success p-3 w-100" style="font-size: 1.3rem;">
+    <a href="{{ route('customer') }}" class="btn btn-light p-3 w-100" style="font-size: 1.3rem;">
       <i class="fas fa-users mr-3"></i>
       <br/>
       Customer
@@ -73,7 +73,7 @@
 
   @can ('is-admin')
   <div class="col-6 mr-3-rm mb-3 p-0 pr-3">
-    <a href="{{ route('online-order') }}" class="btn btn-success p-3 w-100" style="font-size: 1.3rem;">
+    <a href="{{ route('online-order') }}" class="btn btn-light p-3 w-100" style="font-size: 1.3rem;">
       <i class="fas fa-cloud-download-alt mr-3"></i>
       <br/>
       Weborders
