e2a14ed5230b640288d522720ccf85bddd32d39a
odev01 <odev01@oit.com.np>
2023-10-03 12:32:29 +0545

    Misc updates; Misc FE Updates mobile;

--- resources/views/components/toolbar-classic.blade.php
+++ resources/views/components/toolbar-classic.blade.php
@@ -23,7 +23,7 @@
   </div>
 
   {{-- Show in smaller screens --}}
-  <div class="d-flex justify-content-between mb-4">
+  <div class="d-md-none d-flex justify-content-between mb-4">
     <div>
       <div class="dropdown">
         <button class="btn btn-light p-3 border shadow-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
