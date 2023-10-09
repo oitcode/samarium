3842cc8781ca99b13ef77ff222caed953cc7b317
odev01 <odev01@oit.com.np>
2023-10-09 19:57:50 +0545

    Misc updates; Misc FE Updates;

--- resources/views/partials/dashboard/tool-bar-button-pill.blade.php
+++ resources/views/partials/dashboard/tool-bar-button-pill.blade.php
@@ -1,57 +1,125 @@
-<div
-    class="btn-rm
-    @if (false)
-    @isset($btnCheckMode)
-      @isset ($modes)
-        @isset ($modes[$btnCheckMode])
-          @if ($modes[$btnCheckMode])
-            border-bottom-rm border-danger-rm
+<div>
+  {{-- For bigger screens --}}
+  <div class="d-none d-md-block">
+  <div
+      class="btn-rm
+      @if (false)
+      @isset($btnCheckMode)
+        @isset ($modes)
+          @isset ($modes[$btnCheckMode])
+            @if ($modes[$btnCheckMode])
+              border-bottom-rm border-danger-rm
+            @else
+              bg-white
+            @endif
           @else
             bg-white
-          @endif
+          @endisset
         @else
           bg-white
         @endisset
       @else
         bg-white
       @endisset
-    @else
-      bg-white
-    @endisset
-    @endif
-    border-rm shadow-rm m-0 badge-pill-rm mr-4-rm px-4 h-100 border-none shadow-none o-flipper py-3 d-flex flex-column justify-content-center rounded"
-
-
-    style="font-size: 1.1rem;
-    @if (true)
-    @isset($btnCheckMode)
-      @isset ($modes)
-        @isset ($modes[$btnCheckMode])
-          @if ($modes[$btnCheckMode])
-            {{--
-            border-bottom: 5px solid #55a;
-            padding-bottom: 10px;
-            background-color: {{ env('OC_SELECT_COLOR', '#000050') }};
-            color: white;
-            --}}
-            color: {{ env('OC_SELECT_COLOR', '#000050') }};
-            border-bottom: 5px solid {{ env('OC_SELECT_COLOR', '#000050') }};
-          @endif
+      @endif
+      border-rm shadow-rm m-0 badge-pill-rm mr-4-rm px-4 h-100 border-none shadow-none o-flipper py-3 d-flex flex-column justify-content-center rounded"
+  
+  
+      style="font-size: 1.1rem;
+      @if (true)
+      @isset($btnCheckMode)
+        @isset ($modes)
+          @isset ($modes[$btnCheckMode])
+            @if ($modes[$btnCheckMode])
+              {{--
+              border-bottom: 5px solid #55a;
+              padding-bottom: 10px;
+              background-color: {{ env('OC_SELECT_COLOR', '#000050') }};
+              color: white;
+              --}}
+              color: {{ env('OC_SELECT_COLOR', '#000050') }};
+              border-bottom: 5px solid {{ env('OC_SELECT_COLOR', '#000050') }};
+            @endif
+          @endisset
         @endisset
       @endisset
-    @endisset
-    @endif
-    "
-
-    wire:click="{{ $btnClickMethod }}" role="button">
-  <div class="">
-    <div class="d-flex justify-content-center">
-      <i class="{{ $btnIconFaClass }} mr-2"></i>
-    </div>
-    <div class="d-flex justify-content-center">
-      <strong>
-        {{ $btnText }}
-      </strong>
+      @endif
+      "
+  
+      wire:click="{{ $btnClickMethod }}" role="button">
+    <div class="">
+      <div class="d-flex justify-content-center">
+        <i class="{{ $btnIconFaClass }} mr-2"></i>
+      </div>
+      <div class="d-flex justify-content-center">
+        <strong>
+          {{ $btnText }}
+        </strong>
+      </div>
     </div>
   </div>
+  </div>
+
+  {{-- For smaller screens --}}
+  <div class="d-md-none">
+  <div
+      class="btn-rm d-md-none
+      @if (false)
+      @isset($btnCheckMode)
+        @isset ($modes)
+          @isset ($modes[$btnCheckMode])
+            @if ($modes[$btnCheckMode])
+              border-bottom-rm border-danger-rm
+            @else
+              bg-white
+            @endif
+          @else
+            bg-white
+          @endisset
+        @else
+          bg-white
+        @endisset
+      @else
+        bg-white
+      @endisset
+      @endif
+      border-rm shadow-rm m-0 badge-pill-rm mr-4-rm px-4 h-100 border-none shadow-none o-flipper py-3 d-flex flex-column justify-content-center rounded"
+  
+  
+      style="font-size: 1.1rem;
+      @if (true)
+      @isset($btnCheckMode)
+        @isset ($modes)
+          @isset ($modes[$btnCheckMode])
+            @if ($modes[$btnCheckMode])
+              {{--
+              border-bottom: 5px solid #55a;
+              padding-bottom: 10px;
+              background-color: {{ env('OC_SELECT_COLOR', '#000050') }};
+              color: white;
+              --}}
+              color: {{ env('OC_SELECT_COLOR', '#000050') }};
+              border-left: 10px solid {{ env('OC_SELECT_COLOR', '#000050') }};
+            @endif
+          @endisset
+        @endisset
+      @endisset
+      @endif
+      "
+  
+      wire:click="{{ $btnClickMethod }}" role="button">
+    <div class="">
+      <div class="d-flex justify-content-center">
+        <i class="{{ $btnIconFaClass }} mr-2"></i>
+      </div>
+      <div class="d-flex justify-content-center">
+        <strong>
+          {{ $btnText }}
+        </strong>
+      </div>
+    </div>
+  </div>
+  </div>
+
+
 </div>
