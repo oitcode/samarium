405d0b671f77a27dbf75e8cd3e45038777a3945c
odev01 <odev01@oit.com.np>
2023-10-09 15:06:12 +0545

    Misc updates; Misc FE Updates;

--- resources/views/livewire/seat-table-list-display.blade.php
+++ resources/views/livewire/seat-table-list-display.blade.php
@@ -1,22 +1,34 @@
-<div>
+<div class="h-100">
   {{-- Show in bigger screens --}}
   <div class="card mb-3 shadow
-      @if ($seatTable->isBooked()) bg-danger text-white  @else bg-success text-white @endif
+      @if ($seatTable->isBooked()) bg-danger-rm text-white-rm  @else bg-success-rm text-white-rm @endif
       h-100
       d-none d-md-block"
       wire:click="displayWorkingSeatTable({{ $seatTable->seat_table_id }})"
       role="button"
   >
-    <div class="card-header @if ($seatTable->isBooked()) bg-danger-rm @else bg-success text-white @endif">
+    <div class="card-header @if ($seatTable->isBooked()) bg-danger-rm text-white-rm @else bg-success-rm text-white-rm @endif">
       <div class="float-left">
         <h2 class="badge" style="font-size: 1.3rem;">
           {{ $seatTable->name }}
+          @if ($seatTable->isBooked())
+            <span class="badge badge-danger badge-pill ml-2">
+              Booked
+            </span>
+          @else
+            <span class="badge badge-success badge-pill ml-2">
+              Open
+            </span>
+          @endif
         </h2>
+
       </div>
       <div class="float-right" style="font-size: 1rem;">
           @if ($seatTable->isBooked())
+            <span class="h4 pt-4">
             Rs
             @php echo number_format( $seatTable->getCurrentBookingTotalAmount() ); @endphp
+            </span>
           @else
             @if (false)
             <button class="btn btn-light" wire:click="deleteSeatTable({{ $seatTable }})">
@@ -40,7 +52,7 @@
       <div class="row" style="margin: auto;">
         <div class="col-md-12">
           <div class="table-responsive">
-            <table class="table @if ($seatTable->isBooked()) bg-danger @else bg-success @endif text-white">
+            <table class="table {{-- @if ($seatTable->isBooked()) bg-danger @else bg-success @endif --}} text-white-rm">
               <tr class="border-0" style="font-size: 1rem;">
                 <td class="border-0">
                   <i class="fas fa-clock mr-3"></i>
@@ -77,11 +89,13 @@
           @if (true)
           <div class="d-flex justify-content-between">
             <div wire:click="displayWorkingSeatTable({{ $seatTable->seat_table_id }})">
+              @if (false)
               <h2 class="h3 ml-4-rm">
                 OPEN
               </h2>
+              @endif
               <div class="mr-4">
-                <i class="fas fa-plus-circle fa-3x"></i>
+                <i class="fas fa-dice-d6 text-success fa-3x"></i>
               </div>
             </div>
 
