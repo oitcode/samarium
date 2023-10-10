ca2b0d2ae5237b36ecfa7b8ede28311d9a599b18
odev01 <odev01@oit.com.np>
2023-10-10 16:05:19 +0545

    Misc updates; Misc FE Updates;

--- resources/views/livewire/ecomm-website/home-component.blade.php
+++ resources/views/livewire/ecomm-website/home-component.blade.php
@@ -84,118 +84,41 @@
 
 
               <div class="col-md-3">
-                <div class="card h-100 shadow">
+                <a href="{{ route('website-product-view', [\App\Product::find($ii)->product_id, \App\Product::find($ii)->name]) }}"
+                    class="text-decoration-none">
+                  <div class="card h-100 shadow">
       
-                  @if (true)
-                  <div class="d-flex flex-column justify-content-between h-100 bg-success-rm">
-                    <a href="{{ route('website-product-view', [\App\Product::find($ii)->product_id, \App\Product::find($ii)->name]) }}">
-                      <div class="d-flex justify-content-center bg-warning-rm">
-                          @if (\App\Product::find($ii)->image_path)
-                            <img class="img-fluid h-25-rm w-100-rm" src="{{ asset('storage/' . \App\Product::find($ii)->image_path) }}" alt="{{
-                            \App\Product::find($ii)->name }}" style="max-height: 150px; {{--max-width: 100px;--}}">
-                          @else
-                            <i class="fas fa-dice-d6 fa-8x text-muted m-5"></i>
-                          @endif
-                      </div>
-                    </a>
-      
-                    <div class="d-flex flex-column justify-content-between flex-grow-1 overflow-auto" style="background-color: #f5f5f5;">
-                      <div class="p-2">
-                        <a href="{{ route('website-product-view', [\App\Product::find($ii)->product_id, \App\Product::find($ii)->name]) }}">
-                          <h2 class="h5 font-weight-bold mt-2 mb-2 text-dark" style="font-family: Arial;">
-                            {{ strtoupper(\App\Product::find($ii)->name) }}
-                            <br />
-                            @if (\App\Product::find($ii)->selling_price != 0)
-                            <span class="text-muted ml-1-rm">
-                              Rs {{ \App\Product::find($ii)->selling_price }}
-                            </span>
-                            @endif
-                          </h2>
-                        </a>
-      
-                      </div>
-                    </div>
-                  </div>
-                  @else
+                    @if (true)
                     <div class="d-flex flex-column justify-content-between h-100 bg-success-rm">
                         <div class="d-flex justify-content-center bg-warning-rm">
-                          @if (\App\Product::find($ii)->image_path)
-                            <img class="img-fluid h-25-rm w-100-rm" src="{{ asset('storage/' . \App\Product::find($ii)->image_path) }}" alt="{{
-                            \App\Product::find($ii)->name }}" style="max-height: 150px; {{--max-width: 100px;--}}">
-                          @else
-                            <i class="fas fa-dice-d6 fa-8x text-muted m-5"></i>
-                          @endif
+                            @if (\App\Product::find($ii)->image_path)
+                              <img class="img-fluid h-25-rm w-100-rm" src="{{ asset('storage/' . \App\Product::find($ii)->image_path) }}" alt="{{
+                              \App\Product::find($ii)->name }}" style="max-height: 150px; {{--max-width: 100px;--}}">
+                            @else
+                              <i class="fas fa-dice-d6 fa-8x text-muted m-5"></i>
+                            @endif
                         </div>
-
+      
                       <div class="d-flex flex-column justify-content-between flex-grow-1 overflow-auto" style="background-color: #f5f5f5;">
                         <div class="p-2">
-                          <a href="{{ route('website-product-view', [\App\Product::find($ii)->product_id, \App\Product::find($ii)->name,]) }}">
-                            <h2 class="h5 font-weight-bold mt-2 mb-2 text-dark" style="font-family: Arial;">
+                            <h2 class="h5 font-weight-bold mt-2 mb-2-rm text-dark" style="font-family: Arial;">
                               {{ strtoupper(\App\Product::find($ii)->name) }}
                             </h2>
-                          </a>
-                          @if (false)
-                          <div class="mb-1" style="font-size: 0.7rem;">
-                            <i class="far fa-star" style="color: orange;"></i>
-                            <i class="far fa-star" style="color: orange;"></i>
-                            <i class="far fa-star" style="color: orange;"></i>
-                            <i class="far fa-star" style="color: orange;"></i>
-                            <i class="far fa-star" style="color: orange;"></i>
-                            <span class="mx-1 text-muted">
-                              (0) reviews
-                            </span>
-                          </div>
-                          @endif
-                          @if ($product->selling_price != 0)
-                            <div class="h5 text-danger text-left mt-3" style="font-weight: bold;">
-                              Rs.
-                              @php echo number_format( \App\Product::find($ii)->selling_price ); @endphp
+                            <div class="mt-0 text-muted h6 font-weight-bold">
+                              @if (\App\Product::find($ii)->selling_price != 0)
+                                Rs
+                                @php echo number_format( \App\Product::find($ii)->selling_price ); @endphp
+                              @endif
                             </div>
-                          @endif
-                          @if (false)
-                            @if (rand()%2 == 0)
-                            <div class="h6 text-dark text-left">
-                              Free shipping
-                            </div>
-                            @endif
-                            @if (rand()%2 == 0)
-                            <div class="h6 text-dark text-left">
-                              Discount
-                              <span class="text-success">
-                                50%
-                              </span>
-                            </div>
-                            @endif
-                          @endif
-                          <div class="text-danger mt-3">
-                            <span class="btn btn-outline-success-rm btn-light badge-pill-rm p-1 px-3" wire:click.prevent="addItemToCart({{
-                            \App\Product::find($ii)->product_id }})">
-                              <i class="fas fa-plus-circle mr-1"></i>
-                              Cart
-                            </span>
-                            {{-- Loading spinner --}}
-                            <span wire:loading class="spinner-border text-info mr-3"
-                                role="status"
-                                style="font-size: 1rem;">
-                            </span>
-                          </div>
-
+      
                         </div>
-                        @if (false)
-                        <div class="mb-4 px-2">
-                          <button class="btn btn-success mt-3 p-3 px-3" wire:click.prevent="addItemToCart({{ $product->product_id }})">
-                            <span class="h5">
-                            ADD TO CART
-                            </span>
-                          </button>
-                        </div>
-                        @endif
                       </div>
                     </div>
-                  @endif
+                    @endif
 
 
-                </div>
+                  </div>
+                </a>
               </div>
 
 
