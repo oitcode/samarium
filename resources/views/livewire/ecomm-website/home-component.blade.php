22f6ed61712cde71cbcf66195c210f37805665be
odev01 <odev01@oit.com.np>
2023-10-14 21:50:28 +0545

    Misc updates; Misc Ecomm website FE updates; Updates;

--- resources/views/livewire/ecomm-website/home-component.blade.php
+++ resources/views/livewire/ecomm-website/home-component.blade.php
@@ -79,7 +79,7 @@
 
         <div class="row bg-white py-3" style="margin: auto;">
 
-          @for ($ii=1; $ii <= \App\Product::max('product_id') && $ii <= 4; $ii++)
+          @for ($ii=1; $ii <= \App\Product::max('product_id') && $ii <= 3; $ii++)
             @if (\App\Product::find($ii))
 
 
@@ -92,7 +92,7 @@
                     <div class="d-flex flex-column justify-content-between h-100 bg-success-rm">
                         <div class="d-flex justify-content-center bg-warning-rm">
                             @if (\App\Product::find($ii)->image_path)
-                              <img class="img-fluid h-25-rm w-100" src="{{ asset('storage/' . \App\Product::find($ii)->image_path) }}" alt="{{
+                              <img class="img-fluid h-25-rm w-100-rm" src="{{ asset('storage/' . \App\Product::find($ii)->image_path) }}" alt="{{
                               \App\Product::find($ii)->name }}" style="max-height: 150px; {{--max-width: 100px;--}}">
                             @else
                               <i class="fas fa-ellipsis-h fa-8x text-muted m-5"></i>
