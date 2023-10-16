69f34ed7c624ee72d80a8cc208208cd92fbc2a92
odev01 <odev01@oit.com.np>
2023-10-16 08:09:15 +0545

    Misc updates; Ecomm website subscribe us component started; Started;

---
+++ app/Http/Livewire/EcommWebsite/SubscribeUs.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\EcommWebsite;
+
+use Livewire\Component;
+
+class SubscribeUs extends Component
+{
+    public function render()
+    {
+        return view('livewire.ecomm-website.subscribe-us');
+    }
+}
---
+++ resources/views/livewire/ecomm-website/subscribe-us.blade.php
@@ -0,0 +1,31 @@
+<div>
+
+
+  <div class="px-3 text-white">
+    <div class="row">
+      <div class="col-md-6 py-4">
+        <h2 class="h5 font-weight-bold text-center-rm">
+          Subscribe us
+        </h2>
+        <p class="text-muted-rm text-center-rm">
+          Subscribe to our newsletter to get the latest updates.
+        </p>
+
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Enter a valid email address" />
+        </div>
+        <div class="mt-4 d-flex justify-content-center-rm">
+          <button class="btn btn-danger shadow">
+            Subscribe
+          </button>
+        </div>
+      </div>
+      @if (true)
+      <div class="col-md-6" style="{{--background-image: linear-gradient(to bottom right, #FF512F, #DD2476);--}}">
+      </div>
+      @endif
+    </div>
+  </div>
+
+
+</div>
