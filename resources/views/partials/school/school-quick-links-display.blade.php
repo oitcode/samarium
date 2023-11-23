897534763cc18360f95f5580214518997e999e6f
odev01 <odev01@oit.com.np>
2023-11-23 11:43:19 +0545

    Misc updates; Misc CMS website FE update; Updates

---
+++ resources/views/partials/school/school-quick-links-display.blade.php
@@ -0,0 +1,58 @@
+{{--
+
+Quick links to pages that are most relevant to school.
+
+--}}
+
+<div class="bg-white">
+  <div class="row p-3">
+    <div class="col-6 col-md-2 d-flex justify-content-center bg-danger text-white border p-0">
+      <a href="/calendar" class="p-3 py-5 text-reset">
+        <div class="text-center">
+          <div class="mb-2">
+            <i class="fas fa-calendar fa-2x"></i>
+          </div>
+          <div class="h5">
+            Calendar
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="col-6 col-md-2 d-flex justify-content-center bg-primary text-white border p-0">
+      <a href="/noticeboard" class="p-3 py-5 text-reset">
+        <div class="text-center">
+          <div class="mb-2">
+            <i class="fas fa-edit fa-2x"></i>
+          </div>
+          <div class="h5">
+            Noticeboard
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="col-6 col-md-2 d-flex justify-content-center bg-info text-white border p-0">
+      <a href="/gallery" class="p-3 py-5 text-reset">
+        <div class="text-center">
+          <div class="mb-2">
+            <i class="fas fa-images fa-2x"></i>
+          </div>
+          <div class="h5">
+            Gallery
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="col-6 col-md-2 d-flex justify-content-center bg-success text-white border p-0">
+      <a href="/contact-us" class="p-3 py-5 text-reset">
+        <div class="text-center">
+          <div class="mb-2">
+            <i class="fas fa-comment fa-2x"></i>
+          </div>
+          <div class="h5">
+            Contact us
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
