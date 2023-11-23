8c709dfb7edc9f5c95701a25eb26ed671f06a97d
odev01 <odev01@oit.com.np>
2023-11-23 20:31:22 +0545

    Misc updates; Calendar event edit name started; Started

--- app/Http/Livewire/School/CalendarEventDisplay.php
+++ app/Http/Livewire/School/CalendarEventDisplay.php
@@ -4,12 +4,37 @@ namespace App\Http\Livewire\School;
 
 use Livewire\Component;
 
+use App\Traits\ModesTrait;
+
 class CalendarEventDisplay extends Component
 {
+    use ModesTrait;
+
     public $calendarEvent;
+
+    public $modes = [
+        'editNameMode' => false,
+        'editDateMode' => false,
+        'editIsHolidayMode' => false,
+    ];
+
+    protected $listeners = [
+        'calendarEventUpdateNameCancelled',
+        'calendarEventUpdateNameCompleted',
+    ];
 
     public function render()
     {
         return view('livewire.school.calendar-event-display');
+    }
+
+    public function calendarEventUpdateNameCancelled()
+    {
+        $this->exitMode('editNameMode');
+    }
+
+    public function calendarEventUpdateNameCompleted()
+    {
+        $this->exitMode('editNameMode');
     }
 }
--- resources/views/livewire/school/calendar-event-display.blade.php
+++ resources/views/livewire/school/calendar-event-display.blade.php
@@ -1,6 +1,63 @@
 <div>
-    <div class="bg-white p-3 border">
-      Calendar event display
-      {{ $calendarEvent->title }}
+
+
+  <div class="bg-white p-3 border">
+
+    {{-- Temp refresh --}}
+    <div class="mb-3">
+      <i class="fas fa-refresh" wire:click="$refresh"></i>
     </div>
+
+    {{-- Event name --}}
+    <div class="my-3">
+      <div>
+        Event name
+
+        <span class="text-primary ml-3" wire:click="enterMode('editNameMode')">
+          Edit
+        </span>
+
+      </div>
+      <div>
+        @if ($modes['editNameMode'])
+          @livewire ('calendar.dashboard.calendar-event-edit-name', ['calendarEvent' => $calendarEvent,])
+        @else
+          {{ $calendarEvent->title }}
+        @endif
+      </div>
+    </div>
+
+    {{-- Event date --}}
+    <div class="my-3">
+      <div>
+        Event date
+
+        <span class="text-primary ml-3">
+          Edit
+        </span>
+
+      </div>
+      <div>
+        {{ $calendarEvent->start_date }}
+      </div>
+    </div>
+
+    {{-- Is holidar --}}
+    <div class="my-3">
+      <div>
+        Is holiday
+
+        <span class="text-primary ml-3">
+          Edit
+        </span>
+
+      </div>
+      <div>
+        {{ $calendarEvent->is_holiday }}
+      </div>
+    </div>
+
+  </div>
+
+
 </div>
