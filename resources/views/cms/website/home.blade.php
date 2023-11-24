6bd5dde29d99c69a802b9a2d286ea5bf63d0aea4
odev01 <odev01@oit.com.np>
2023-11-24 16:46:03 +0545

    Misc updates; Misc CMS home page notice displayer FE update; Misc updates

--- resources/views/cms/website/home.blade.php
+++ resources/views/cms/website/home.blade.php
@@ -26,21 +26,21 @@
 {{-- Notifications/post displayer  --}}
 @if (\App\WebpageCategory::where('name', 'notice')->first())
   @if (count(\App\WebpageCategory::where('name', 'notice')->first()->webpages()->where('is_post', 'yes')->get()) > 0)
-  <div class="container-fluid bg-dark-rm text-white p-0" style="background-color: brown;">
-    <div class="container" style="font-size: 1.3rem; white-space: nowrap; overflow: hidden;">
-      <div class="o-ltr py-3 ">
-        <div class="d-inline mr-5">
-          <a href="{{ route('website-webpage-' . \App\WebpageCategory::where('name', 'notice')->first()->webpages()->where('is_post', 'yes')->orderBy('webpage_id', 'desc')->first()->permalink) }}"
-              class="text-reset" style="">
-            <span class="badge badge-pill badge-light">
-              Notice
-            </span>
-            {{ \App\WebpageCategory::where('name', 'notice')->first()->webpages()->where('is_post', 'yes')->orderBy('webpage_id', 'desc')->first()->name }}
-          </a>
+  <a href="{{ route('website-webpage-' . \App\WebpageCategory::where('name', 'notice')->first()->webpages()->where('is_post', 'yes')->orderBy('webpage_id', 'desc')->first()->permalink) }}"
+      class="text-reset text-decoration-none" style="">
+    <div class="container-fluid bg-dark-rm text-white p-0" style="background-color: brown;">
+      <div class="container" style="font-size: 1.3rem; white-space: nowrap; overflow: hidden;">
+        <div class="o-ltr py-3 ">
+          <div class="d-inline mr-5">
+              <span class="badge badge-pill badge-light">
+                Notice
+              </span>
+              {{ \App\WebpageCategory::where('name', 'notice')->first()->webpages()->where('is_post', 'yes')->orderBy('webpage_id', 'desc')->first()->name }}
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </a>
   @endif
 @endif
 
