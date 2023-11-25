209901c8f346b0d168799f7235fe2a2c026d9028
odev01 <odev01@oit.com.np>
2023-11-25 14:06:03 +0545

    Misc updates; CMS home page FE update; Misc updates

--- resources/views/cms/website/home.blade.php
+++ resources/views/cms/website/home.blade.php
@@ -47,6 +47,7 @@
 @if (true)
 {{-- Hero/Featured image --}}
 @if (\App\CmsTheme::first())
+{{-- Show in bigger screens --}}
 <div class="container-fluid bg-light p-0 d-none d-md-block" 
   style="background-image: @if (\App\CmsTheme::first())
                              url({{ asset('storage/' . \App\CmsTheme::first()->hero_image_path) }})
@@ -58,6 +59,22 @@
                            background-repeat: no-repeat;
                            background-position: center;
                            height: 700px;">
+  <div class="o-overlay py-5 h-100">
+  </div>
+</div>
+
+{{-- Show in smaller screens --}}
+<div class="container-fluid bg-light p-0 d-md-none" 
+  style="background-image: @if (\App\CmsTheme::first())
+                             url({{ asset('storage/' . \App\CmsTheme::first()->hero_image_path) }})
+                           @else
+                             url({{ asset('img/school-5.jpg') }})
+                           @endif
+                           ;
+                           background-size: cover;
+                           background-repeat: no-repeat;
+                           background-position: center;
+                           height: 300px;">
   <div class="o-overlay py-5 h-100">
   </div>
 </div>
