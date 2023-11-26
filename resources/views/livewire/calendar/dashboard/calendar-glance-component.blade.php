50176dbd3c1bb10736b68ba2bdffc15faf090651
odev01 <odev01@oit.com.np>
2023-11-26 22:42:36 +0545

    Misc updates; Calendar glance component updates; Updates

--- resources/views/livewire/calendar/dashboard/calendar-glance-component.blade.php
+++ resources/views/livewire/calendar/dashboard/calendar-glance-component.blade.php
@@ -13,7 +13,7 @@
             <i class="far fa-arrow-alt-circle-left fa-2x text-secondary"></i>
           </button>
 
-          <button class="btn btn-dark mr-3 px-2 font-weight-bold rounded-lg">
+          <button class="btn btn-dark mr-3 px-2 font-weight-bold rounded-lg" style="min-width: 300px;">
             @if (\Carbon\Carbon::today() == \Carbon\Carbon::create($calendarDate))
               <span class="">
                 TODAY
@@ -39,7 +39,7 @@
             <i class="far fa-arrow-alt-circle-right fa-2x text-secondary"></i>
           </button>
 
-          <div class="d-none d-md-block my-3-rm text-secondary-rm ml-5" style="font-size: 1rem;">
+          <div class="d-none d-md-block my-3-rm text-secondary-rm ml-3" style="font-size: 1rem;">
 
             <input type="date" wire:model.defer="calendarDate" class="ml-5">
             <button class="btn btn-light" wire:click="setCalendarDate">
@@ -48,6 +48,18 @@
           </div>
         </div>
 
+        <div class="float-left">
+          @if ($today['is_holiday'])
+            <button class="btn btn-light">
+              <i class="fas fa-flag text-danger"></i>
+              HOLIDAY
+            </button>
+          @else
+            <button class="btn btn-light">
+              NOT A HOLIDAY
+            </button>
+          @endif
+        </div>
         <div wire:loading>
           <div class="spinner-border text-info mr-3" role="status">
             <span class="sr-only">Loading...</span>
@@ -69,34 +81,40 @@
         Holiday
       </h2>
       @endif
-      @if ($today['is_holiday'])
-        <div class="font-weight-bold border border-danger shadow p-2 py-3 rounded-lg">
-          <i class="fas fa-flag text-danger"></i>
-          HOLIDAY
-        </div>
-      @else
-        <div class="bg-light text-dark font-weight-bold border shadow p-2 py-3 rounded-lg">
-          NOT A HOLIDAY
-        </div>
-      @endif
     </div>
 
-    {{-- Show calendar events for the selected date --}}
-    <div class="col-md-6 p-0 bg-danger-rm text-white-rm py-3-rm border-rm bg-warning-rm border-rm">
-      <h2 class="h5 font-weight-bold my-3">
-        Events
-      </h2>
-      @if (count($today['events']) > 0)
-        @foreach ($today['events'] as $event)
-          <div class="mb-3 p-2 border rounded-lg shadow bg-dark-rm text-white-rm py-3">
-            <i class="fas fa-calendar mr-1 text-primary"></i>
-            <span class="h6">
-              {{ $event->title }}
-            </span>
+    <div class="row" style="margin: auto;">
+      <div class="col-md-6 p-0 bg-danger-rm text-white-rm border-rm bg-warning-rm border-rm">
+        {{-- Show calendar events for the selected date --}}
+        <h2 class="h5 font-weight-bold mb-3">
+          Events
+        </h2>
+        @if (count($today['events']) > 0)
+          @foreach ($today['events'] as $event)
+            <div class="mb-3 p-2 border rounded-lg shadow-rm bg-dark-rm text-white-rm py-3">
+              <i class="fas fa-calendar mr-1 @if ($event->is_holiday == 'yes') text-danger @else text-primary @endif"></i>
+              <span class="h6">
+                {{ $event->title }}
+              </span>
+            </div>
+          @endforeach
+        @else
+          No events
+        @endif
+      </div>
+      @if (false)
+      <div class="col-md-6">
+        @if ($today['is_holiday'])
+          <div class="font-weight-bold border shadow-rm p-2 py-3 rounded-lg">
+            <i class="fas fa-flag text-danger"></i>
+            HOLIDAY
           </div>
-        @endforeach
-      @else
-        No events
+        @else
+          <div class="bg-light text-dark font-weight-bold border shadow-rm p-2 py-3 rounded-lg">
+            NOT A HOLIDAY
+          </div>
+        @endif
+      </div>
       @endif
     </div>
 
