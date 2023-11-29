e417b6995ff59313b279255b2bc60b90de9f3d62
odev01 <odev01@oit.com.np>
2023-11-29 12:07:29 +0545

    Misc updates; Calendar group list started; Started

--- app/Http/Livewire/Calendar/Dashboard/CalendarGroupList.php
+++ app/Http/Livewire/Calendar/Dashboard/CalendarGroupList.php
@@ -4,8 +4,12 @@ namespace App\Http\Livewire\Calendar\Dashboard;
 
 use Livewire\Component;
 
+use App\CalendarGroup;
+
 class CalendarGroupList extends Component
 {
+    public $calendarGroups;
+
     public function render()
     {
         return view('livewire.calendar.dashboard.calendar-group-list');
--- resources/views/livewire/calendar/dashboard/calendar-group-list.blade.php
+++ resources/views/livewire/calendar/dashboard/calendar-group-list.blade.php
@@ -1,3 +1,60 @@
 <div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+
+
+  @if (!is_null($calendarGroups) && count($calendarGroups) > 0)
+
+    {{-- Show in bigger screens --}}
+    <div class="d-none d-md-block bg-white border">
+      <div class="table-responsive">
+        <table class="table table-hover table-valign-middle">
+          <thead>
+            <tr class="{{ env('OC_ASCENT_BG_COLOR', 'bg-success') }}
+                {{ env('OC_ASCENT_TEXT_COLOR', 'text-white') }}
+                p-4" style="font-size: 1rem;">
+              <th>Calendar group ID</th>
+              <th>Name</th>
+            </tr>
+          </thead>
+  
+          <tbody>
+            @foreach ($calendarGroups as $calendarGroup)
+            <tr>
+
+              <td>
+                {{ $calendarGroup->calendar_group_id }}
+              </td>
+
+              <td class="font-weight-bold" wire:click="$emit('displayCalendarGroup', {{ $calendarGroup }})" role="button">
+                {{ $calendarGroup->name }}
+              </td>
+
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+
+    {{-- Show in smaller screens --}}
+    <div class="d-md-none">
+
+      @foreach ($calendarGroups as $calendarGroup)
+        <div class="bg-white border px-3">
+          <div class="h4-rm py-4">
+            <span  wire:click="$emit('displayCalendarGroup', {{ $calendarGroup }})" class="h5 text-dark font-weight-bold" role="button">
+              {{ $calendarGroup->name }}
+            </span>
+          </div>
+        </div>
+      @endforeach
+    </div>
+
+  @else
+    <div class="p-2 text-muted">
+      No calendar groups.
+    </div>
+  @endif
+
+
 </div>
