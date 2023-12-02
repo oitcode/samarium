4e4757d570eb875436e97627b7a69b5ddc7df7c5
odev01 <odev01@oit.com.np>
2023-12-02 07:56:49 +0545

    Misc updates; Company welcome fe update; Progress

--- resources/views/livewire/lv-company-info.blade.php
+++ resources/views/livewire/lv-company-info.blade.php
@@ -1,7 +1,7 @@
 <div class="d-flex-rm justify-content-center-rm border bg-white text-white-rm p-3 shadow">
     <div>
       @if (true)
-      <h2 class="h4 text-success-rm font-weight-bold mb-3">
+      <h2 class="h5 text-success-rm font-weight-bold mb-3">
         <div class="mb-3">
           <img src="{{ asset('storage/' . $company->logo_image_path) }}" class="img-fluid" style="height: 75px;">
         </div>
@@ -9,17 +9,23 @@
       </h2>
       @endif
 
-      <div class="mb-3">
-        <h3 class="h5 font-weight-bold">
+      @if (true)
+      <div class="">
+        <h3 class="h6 mb-0 text-secondary">
           {{ $company->tagline }}
         </h3>
       </div>
+      @endif
 
+      @if (false)
       <div class="text-secondary-rm">
         <div class="mr-3">
           <i class="fas fa-copyright mr-1"></i>
-          Powered by OIT
+          Powered by
+          OIT
         </div>
       </div>
+      @endif
+
     </div>
 </div>
