431dcd2ae97affe432fc8645477a0886a27fe611
odev01 <odev01@oit.com.np>
2023-12-17 10:46:46 +0545

    Misc updates; Misc updates ; Misc updates

--- resources/views/partials/cms/site-top-menu-mob.blade.php
+++ resources/views/partials/cms/site-top-menu-mob.blade.php
@@ -18,7 +18,7 @@
   </div>
   @endif
 
-  <nav class="navbar navbar-expand-lg navbar-light-rm bg-light-rm border bg-warning-rm p-0"
+  <nav class="navbar navbar-expand-lg navbar-light-rm bg-light-rm border-bottom bg-warning-rm p-0"
       style="
       background-color: orange;
           background-color:
@@ -72,13 +72,13 @@
       "></i>
     </button>
   
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent" style="{{--background-image: linear-gradient(-135deg, transparent 0%, rgba(0,0,0,0.5) 100%);--}}">
       <ul class="navbar-nav mr-auto border-top">
         @if ($cmsNavMenu)
           @foreach ($cmsNavMenu->cmsNavMenuItems()->orderBy('order', 'asc')->get() as $cmsNavMenuItem)
             @if ($cmsNavMenuItem->type == 'item')
-              <li class="nav-item p-2 bg-light text-dark border-bottom">
-                <a class="nav-link h5 text-dark font-weight-bold"
+              <li class="nav-item p-2 bg-transparent text-white border-bottom text-center-rm">
+                <a class="nav-link h4 text-reset font-weight-bold"
                     href="{{ route('website-webpage-' . $cmsNavMenuItem->webpage->permalink) }}">
                   {{ $cmsNavMenuItem->name }}
                 </a>
@@ -103,6 +103,49 @@
             @endif
           @endforeach
         @endif
+        <li class="nav-item p-2 bg-transparent text-white border-bottom-rm text-center-rm">
+          <div class="nav-link h4 text-reset font-weight-bold">
+            <div class="p-3" style="background-color: rgba(0, 0, 0, 0.5);">
+              <div class="d-flex">
+                @if ($company->fb_link)
+                  <div class="mr-3">
+                    <a href="{{ $company->fb_link }}" class="text-reset pt-0" target="_blank">
+                      <i class="fab fa-facebook text-reset fa-2x mr-2 pt-0"></i>
+                    </a>
+                  </div>
+                @endif
+                @if ($company->twitter_link)
+                  <div class="mr-3">
+                    <a href="{{ $company->twitter_link }}" class="text-reset" target="_blank">
+                      <i class="fab fa-twitter text-white-rm fa-2x mr-2 "></i>
+                    </a>
+                  </div>
+                @endif
+                @if ($company->youtube_link)
+                  <div class="mr-3">
+                    <a href="{{ $company->youtube_link }}" class="text-reset" target="_blank">
+                      <i class="fab fa-youtube text-white-rm fa-2x mr-2 "></i>
+                    </a>
+                  </div>
+                @endif
+                @if ($company->insta_link)
+                  <div class="mr-3">
+                    <a href="{{ $company->insta_link }}" class="text-reset" target="_blank">
+                      <i class="fab fa-instagram text-white-rm fa-2x mr-2 "></i>
+                    </a>
+                  </div>
+                @endif
+                @if ($company->tiktok_link)
+                  <div class="mr-3">
+                    <a href="{{ $company->tiktok_link }}" class="text-reset" target="_blank">
+                      <i class="fab fa-tiktok text-white-rm fa-2x mr-2 "></i>
+                    </a>
+                  </div>
+                @endif
+              </div>
+            </div>
+          </div>
+        </li>
       </ul>
     </div>
   </nav>
