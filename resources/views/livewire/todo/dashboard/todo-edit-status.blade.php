61876a6949d06b2628a300f7ce9588e783a2e9df
odev01 <odev01@oit.com.np>
2023-12-27 16:50:20 +0545

    Misc updates; Dashboard todo updates ; Updated

---
+++ resources/views/livewire/todo/dashboard/todo-edit-status.blade.php
@@ -0,0 +1,21 @@
+<div>
+
+
+  <select class="custom-control w-75" wire:model.defer="status">
+    <option value="pending">Pending</option>
+    <option value="progress">Progress</option>
+    <option value="deferred">Deferred</option>
+    <option value="cancelled">Cancelled</option>
+    <option value="done">Done</option>
+  </select>
+  <div class="my-3">
+    <button class="btn btn-sm btn-success ml-2" wire:click="update">
+      Save
+    </button>
+    <button class="btn btn-sm btn-danger ml-2" wire:click="$emit('todoUpdateStatusCancelled')">
+      Cancel
+    </button>
+  </div>
+
+
+</div>
