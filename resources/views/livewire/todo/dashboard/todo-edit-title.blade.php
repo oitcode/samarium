f05b822dd22a5a029cb66e265e5ff908db865ac2
odev01 <odev01@oit.com.np>
2023-12-27 15:33:48 +0545

    Misc updates; Dashboard todo updates ; Updated

---
+++ resources/views/livewire/todo/dashboard/todo-edit-title.blade.php
@@ -0,0 +1,18 @@
+<div>
+
+
+  <div class="form-group">
+    <input type="text" class="form-control" wire:model.defer="title">
+  </div>
+
+  <div>
+    <button class="btn btn-success mr-2" wire:click="update">
+      Save
+    </button>
+    <button class="btn btn-danger mr-2" wire:click="$emit('todoUpdateTitleCancelled')">
+      Cancel
+    </button>
+  </div>
+
+
+</div>
