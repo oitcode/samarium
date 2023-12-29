a0d999bc34bc85c42e65702b76e33e47378cec55
odev01 <odev01@oit.com.np>
2023-12-29 14:03:21 +0545

    v0.8.2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,7 +82,7 @@
                 </strong></i>
               </h2>
               <h2 class="h5 text-secondary-rm d-flex justify-content-center">
-                  v0.8.1
+                  v0.8.2
               </h2>
             </div>
           </div>
