49d40792a82c4044c83333fa94afc603954934b4
odev01 <odev01@oit.com.np>
2024-01-22 19:36:50 +0545

    Misc updates;

--- resources/views/partials/dashboard/sale-invoice-work-options.blade.php
+++ resources/views/partials/dashboard/sale-invoice-work-options.blade.php
@@ -9,18 +9,10 @@
   </div>
 
   <div class="col-6-rm col-md-3-rm mr-3-rm mb-3 p-0 pr-3">
-    <a href="{{ route('cafesale') }}" class="btn btn-light p-3 w-100">
+    <a href="" class="btn btn-light p-3 w-100">
       <i class="fas fa-print mr-3"></i>
       <br/>
       Print
-    </a>
-  </div>
-
-  <div class="col-6-rm col-md-3-rm mr-3-rm mb-3 p-0 pr-3">
-    <a href="{{ route('cafesale') }}" class="btn btn-light p-3 w-100">
-      <i class="fas fa-trash-alt mr-3"></i>
-      <br/>
-      Delete
     </a>
   </div>
 
