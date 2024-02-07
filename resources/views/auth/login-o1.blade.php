f62fa6b758f94ace8b1f72c1d7ef25033c604c31
odev01 <odev01@oit.com.np>
2024-02-07 06:04:12 +0545

    Misc updates; Login page blade file update; Updates

--- resources/views/auth/login-o1.blade.php
+++ resources/views/auth/login-o1.blade.php
@@ -28,9 +28,6 @@
       }
     </style>
 
-
-    <!-- Livewire -->
-    @livewireStyles
 </head>
 
 <body style="height: 100% !important; background-color: #ded;">
@@ -42,16 +39,6 @@
             <div class="d-flex justify-content-center bg-danger-rm p-3 px-5">
               <div class="mr-4 p-0">
                 <i class="fas fa-check-circle fa-2x text-primary"></i>
-              </div>
-              <div class="p-0 text-center">
-                @if (false)
-                <h1 class="h2 text-primary p-0 m-0">
-                  WebCMS
-                </h1>
-                <div class="h4 text-primary">
-                  Content management
-                </div>
-                @endif
               </div>
             </div>
           </div>
@@ -81,9 +68,6 @@
                 </div>
 
                 <div class="form-group">
-                    @if (false)
-                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-                    @endif
 
                     <input id="password" type="password"
                         class="form-control @error('password') is-invalid @enderror"
@@ -126,95 +110,5 @@
     </div>
   </div>
 
-
-
-  @if (false)
-  <div class="row bg-warning-rm" style="height: 100% !important;">
-    <div class="col-md-6">
-      <div class="h-100 bg-info text-white-rm" style="color: #050; font-family: monospace, sans-serif;">
-        <div class="h-100 d-flex flex-column justify-content-center">
-          <div class="d-flex justify-content-center">
-            @include ('partials.login-left')
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="h-100 col-md-6">
-      <div class="h-100 d-flex flex-column justify-content-center">
-        <div class="d-flex justify-content-center">
-          <div class="col-md-12">
-        <div class="card border-0">
-
-            <div class="card-body border">
-                <form method="POST" action="{{ route('login') }}">
-                    @csrf
-
-                    <div class="form-group row">
-                        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                        <div class="col-md-6">
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                            @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                        <div class="col-md-6">
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                            @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <div class="col-md-6 offset-md-4">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                <label class="form-check-label" for="remember">
-                                    {{ __('Remember Me') }}
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="form-group row mb-0">
-                        <div class="col-md-8 offset-md-4">
-                            <button type="submit" class="btn btn-danger btn-block">
-                                {{ __('Loginasf') }}
-                            </button>
-
-                            @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            @endif
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  @endif
-
-
-
-  <!-- Livewire scripts -->
-  @livewireScripts
 </body>
 </html>
