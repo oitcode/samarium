f86c1b0a6a73455c9c83f316a67b9e2430d7bf8a
odev01 <odev01@oit.com.np>
2024-02-26 22:39:05 +0545

    Misc updates; Dashboard sale invoice work updates; Updates

--- resources/views/partials/dashboard/sale-invoice-work-options.blade.php
+++ resources/views/partials/dashboard/sale-invoice-work-options.blade.php
@@ -9,11 +9,11 @@
   </div>
 
   <div class="col-6-rm col-md-3-rm mr-3-rm mb-3 p-0 pr-3">
-    <button class="btn btn-success p-3 w-100" wire:click="">
+    <a href="{{ route('dashboard-print-sale-invoice', $saleInvoice->sale_invoice_id) }}" class="btn btn-success p-3 w-100" target="_blank">
       <i class="fas fa-print mr-3"></i>
       <br/>
       Print
-    </button>
+    </a>
   </div>
 
 </div>
