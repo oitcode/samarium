7aaa92f0246e13d3bb478911bcc1663435870965
odev01 <odev01@oit.com.np>
2024-05-03 11:16:01 +0545

    Misc updates;

--- routes/web.php
+++ routes/web.php
@@ -330,33 +330,3 @@ Route::get('/vacancy/{id}/{name}', 'WebsiteController@vacancyView')->name('websi
  */
 Route::get('/user/signup', 'WebsiteController@userSignup')->name('website-user-signup');
 Route::get('/user/profile', 'WebsiteController@userProfile')->name('website-user-profile');
-
-
-/*
- *-----------------------------------------------------------------------------
- * Temporary
- *-----------------------------------------------------------------------------
- *
- *
- *
- */
-Route::get('/routes', function () {
-    $routeCollection = Route::getRoutes();
-
-    echo "<table style='width:100%'>";
-    echo "<tr>";
-    echo "<td width='10%'><h4>HTTP Method</h4></td>";
-    echo "<td width='10%'><h4>Route</h4></td>";
-    echo "<td width='10%'><h4>Name</h4></td>";
-    echo "<td width='70%'><h4>Corresponding Action</h4></td>";
-    echo "</tr>";
-    foreach ($routeCollection as $value) {
-        echo "<tr>";
-        echo "<td>" . $value->methods()[0] . "</td>";
-        echo "<td>" . $value->uri() . "</td>";
-        echo "<td>" . $value->getName() . "</td>";
-        echo "<td>" . $value->getActionName() . "</td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-});
