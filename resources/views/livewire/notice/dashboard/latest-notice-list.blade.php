63afbbacff2b27f17f8a820578c16306f8f3152e
odev01 <odev01@oit.com.np>
2024-05-12 13:11:44 +0545

    Misc updates;

--- resources/views/livewire/notice/dashboard/latest-notice-list.blade.php
+++ resources/views/livewire/notice/dashboard/latest-notice-list.blade.php
@@ -1,21 +1,29 @@
 <div class="border bg-white mx-3">
 
-    <div class="table-responsive">
-      <table class="table">
-        <tr>
-          <th class="bg-danger text-white">
-            Notice
-          </th>
-        </tr>
-        @foreach ($notices as $notice) 
-        <tr>
-          <td class="text-primary font-weight-bold">
-            <a href="{{ route('website-webpage-' . $notice->permalink) }}">
-              {{ $notice->name }}
-            </a>
-          </td>
-        </tr>
-        @endforeach
-      </table>
-    </div>
+      <div class="table-responsive">
+        <table class="table">
+          <tr>
+            <th class="bg-danger text-white">
+              Notice
+            </th>
+          </tr>
+          @if (count($notices) > 0)
+            @foreach ($notices as $notice) 
+            <tr>
+              <td class="text-primary font-weight-bold">
+                <a href="{{ route('website-webpage-' . $notice->permalink) }}">
+                  {{ $notice->name }}
+                </a>
+              </td>
+            </tr>
+            @endforeach
+          @else
+            <tr>
+              <td class="text-muted">
+                No notice to show.
+              </td>
+            </tr>
+          @endif
+        </table>
+      </div>
 </div>
