cb2f0bc2ba4260d5b61e7d35c231558ac75057f2
odev01 <odev01@oit.com.np>
2024-05-14 16:22:17 +0545

    Misc updates;

--- resources/views/partials/package/package-intro.blade.php
+++ resources/views/partials/package/package-intro.blade.php
@@ -1,3 +1,12 @@
+{{--
+|
+| Show this when company is not set.
+|
+| This is shown when no company is created in database.
+|
+--}}
+
+
 <div class="d-flex justify-content-center bg-light text-dark p-3 pt-5 pt-md-3">
   <div>
     <h1 class="bg-danger-rm">
@@ -13,8 +22,14 @@
   <div class="d-flex flex-column justify-content-center h-100">
 
     <div class="my-3-rm p-3">
+
       <div class="p-3 text-center border shadow" style="background-color: #eaeaea;">
 
+        {{--
+        |
+        | Company not set message
+        |
+        --}}
         <h2 class="h4 text-danger">
           <i class="fas fa-exclamation-circle mr-1"></i>
           Company not set
@@ -22,6 +37,12 @@
         <p class="text-secondary">
           Please start by creating your company in dashboard.
         </p>
+
+        {{--
+        |
+        | Link to dashboard
+        |
+        --}}
         <h3 class="h5 font-weight-bold">
           <a href="./dashboard" class="btn btn-success badge-pill-rm px-3">
             Visit dashboard
@@ -30,6 +51,11 @@
 
       </div>
 
+      {{--
+      |
+      | Display date
+      |
+      --}}
       <div class="my-4 text-center text-secondary p-3">
         <h3 class="h6">
           {{ date('Y M d') }}
