7e2950ab70030560ed1661cf93c1840b966f8e29
odev01 <odev01@oit.com.np>
2024-05-20 07:27:01 +0545

    Misc updates;

--- resources/views/livewire/calendar/website/today-display.blade.php
+++ resources/views/livewire/calendar/website/today-display.blade.php
@@ -1,7 +1,9 @@
-<div class="my-4-rm mb-5-rm border p-3-rm rounded shadow bg-white">
+<div class="rounded">
 
 
+  @if (true)
   <div style="
+    {{--
     background-color:
         @if (\App\CmsTheme::first())
           {{ \App\CmsTheme::first()->footer_bg_color }}
@@ -16,66 +18,56 @@
           white
         @endif
         ;
+        --}}
   ">
-    <div class="h5 p-3 mb-0 font-weight-bold" style="background-color: rgba(0, 0, 0, 0.2);">
-      TODAY
-    </div>
   </div>
+  @endif
 
-  <div class="row" style="margin: auto;
-    {{--
-    background-color:
-        @if (\App\CmsTheme::first())
-          {{ \App\CmsTheme::first()->ascent_bg_color }}
-        @else
-          orange
-        @endif
-        ;
-    color:
-        @if (\App\CmsTheme::first())
-          {{ \App\CmsTheme::first()->ascent_text_color }}
-        @else
-          white
-        @endif
-    ;
-    --}}
-  ">
-    @if (false)
-    <div class="col-md-3 p-3 bg-primary-rm text-white-rm">
-      <span class="h5 font-weight-bold mb-3">
-      TODAY
-      </span>
-      @if ($today['is_holiday'] == 'yes')
-        <br/>
-        <span class="badge badge-pill badge-danger h5 font-weight-bold p-2">
-          HOLIDAY
-        </span>
-      @endif
-    </div>
-    @endif
-    <div class="col-md-3 bg-info-rm text-white-rm p-3">
-      <h2 class="h5 font-weight-bold mb-3">
-        {{ strtoupper($today['day']->format('l')) }}
-      </h2>
+  <div>
+    <span class="text-success-rm p-1-rm border border-danger-rm px-2 bg-success text-white">
+    Today
+    </span>,
+  </div>
+  <div class="border bg-danger-rm" style="">
+    <div class="h6 px-2 mb-0 mt-3 text-muted mb-2">
       {{ \App\Traits\NepaliDateTrait::convertEnglishToNepaliDate($today['day']->toDateString(), 'english')  }}
-      <br />
+      2081,
+      {{ $today['day']->format('l') }}
+    </div>
+    <div class="col-md-3-rm bg-info-rm text-white-rm px-3">
+      <h2 class="h5 font-weight-bold my-0">
+      </h2>
+      @if (false)
       <span class="">
         {{ $today['day']->format('Y F d') }}
       </span>
+      @endif
     </div>
     @if ($today['is_holiday'] == 'yes')
-      <div class="col-md-3 p-3 bg-success-rm text-white-rm border">
+      <div class="col-md-3-rm p-3 bg-success-rm text-white-rm border-rm">
         <div class="badge badge-pill badge-danger px-3">
-          <span class="h5 font-weight-bold">
-            HOLIDAY
+          <span class="h6 font-weight-bold-rm">
+            Holiday
           </span>
         </div>
       </div>
+    @else
+      @if (false)
+      <div class="col-md-3-rm p-3 bg-success-rm text-white-rm border-rm">
+        <div class="badge badge-pill badge-success px-3">
+          <span class="h6 font-weight-bold-rm">
+            Open
+          </span>
+        </div>
+      </div>
+      @endif
     @endif
-    <div class="col-md-6 bg-success-rm text-white-rm p-3 flex-grow-1">
-      <h2 class="h5 font-weight-bold mb-3">
-        EVENTS
+    <div class="col-md-6-rm bg-success-rm text-white-rm px-3 pb-3 flex-grow-1">
+      @if (false)
+      <h2 class="h5 font-weight-bold mb-0">
+        Events
       </h2>
+      @endif
       @if (count($today['events']) > 0)
         @foreach ($today['events'] as $event)
           <i class="fas fa-calendar mr-1"></i>
@@ -85,7 +77,7 @@
           <br />
         @endforeach
       @else
-        No events
+        No calendar events
       @endif
     </div>
   </div>
