70d2979697f741ace9003c26eb496ead4bc00e77
odev01 <odev01@example.com>
2024-06-05 14:40:36 +0545

    Misc update; CMS updates; Updates

--- app/Http/Livewire/Cms/Website/LatestPostListGrid.php
+++ app/Http/Livewire/Cms/Website/LatestPostListGrid.php
@@ -21,7 +21,15 @@ class LatestPostListGrid extends Component
             $postCategory = WebpageCategory::where('name', $this->category)->first();
             $this->webpages = $postCategory->webpages()->where('visibility', 'public')->orderBy('webpage_id', 'DESC')->get();
         } else {
-            $this->webpages = Webpage::where('is_post', 'yes')->where('visibility', 'public')->orderBy('webpage_id', 'DESC')->limit(4)->get();
+            $this->webpages = Webpage::where('is_post', 'yes')
+                                  ->where('visibility', 'public')
+                                  ->whereDoesntHave('webpageCategories', function ($query) {
+                                      $query->where('name', 'activities');
+                                  })
+                                  ->orderBy('webpage_id', 'desc')
+                                  ->limit(4)
+                                  ->get();
+
         }
 
         return view('livewire.cms.website.latest-post-list-grid');
