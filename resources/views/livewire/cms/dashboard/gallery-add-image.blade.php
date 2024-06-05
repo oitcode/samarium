f3c555aebac8892767e0bcda70ad5e6710aa5d5a
odev01 <odev01@example.com>
2024-06-05 13:42:59 +0545

    Misc update; CMS gallery updates; Updates

--- resources/views/livewire/cms/dashboard/gallery-add-image.blade.php
+++ resources/views/livewire/cms/dashboard/gallery-add-image.blade.php
@@ -9,6 +9,10 @@
   <div>
     <button class="btn btn-sm btn-success mr-3" wire:click="store">Save</button>
     <button class="btn btn-sm btn-secondary" wire:click="$emit('addGalleryImagesCancelled')">Cancel</button>
+    <button wire:loading class="btn">
+      <span class="spinner-border text-info mr-3" role="status">
+      </span>
+    </button>
   </div>
 
 </div>
