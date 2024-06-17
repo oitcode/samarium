ee1940eeb84e2fc5d641b5fa50791e879b8c246d
odev01 <odev01@example.com>
2024-06-17 07:13:23 +0545

    Misc update; Misc FE updates; Updates

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,30 +1,77 @@
-@extends ('ecomm-website.base')
+<!doctype html>
 
-@section ('content')
+
+{{--
+|
+| Register page blade file.
+|
+| This is the blade file of our register page. Please modify this file if you need
+| any changes.
+|
+--}}
+
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" style="height: 100% !important;">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Chartjs -->
+    <script src="{{ asset('js/chart.js') }}"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/fontawesome-free/css/all.css') }}" rel="stylesheet">
+
+    <style>
+      html, body {
+        overflow-x: hidden;
+      }
+    </style>
+
+    <!-- Livewire -->
+    @livewireStyles
+</head>
+
+<body style="height: 100% !important;">
   <div class="container p-3">
   <div class="d-flex-rm justify-content-center-rm h-100">
     <div class="d-flex flex-column justify-content-center py-3">
 
 
-      <div class="card bg-secondary-rm shadow-lg mb-3" {{-- style="min-width: 700px !important;" --}}>
-          @if (true)
-          <div class="card-header bg-white text-dark d-flex justify-content-center-rm py-4">
-            <div class="d-flex flex-column">
-              <div class="d-flex justify-content-center-rm">
-                <h1 class="h5 font-weight-bold pl-3">
-                  Signup
-                </h1>
-              </div>
-            </div>
-          </div>
-          @endif
+      <div class="card-rm bg-light-rm bg-secondary-rm shadow-lg-rm mb-3" {{-- style="min-width: 700px !important;" --}}>
 
-          <div class="card-body">
-            <div class="row">
-              <div class="col-md-6">
-                <form method="POST" action="{{ route('register') }}">
+          <div class="card-body bg-light">
+            <div class="d-flex justify-content-center">
+              <div class="col-md-6-rm">
+                @if (true)
+                <div class="bg-white-rm text-dark-rm d-flex-rm justify-content-center-rm py-4 text-center">
+                  <div class="mb-5">
+                    <img src="{{ asset('storage/' . $company->logo_image_path) }}"
+                        class="img-fluid"
+                        alt="{{ $company->name }} logo"
+                        style="height: 75px !important; {{-- max-width: 75px !important; --}}">
+                  </div>
+                  <h1 class="h3 font-weight-bold pl-3-rm">
+                    Signup
+                  </h1>
+                </div>
+                @endif
+                <form method="POST" action="{{ route('register') }}" class="border bg-white p-3">
                     @csrf
       
+                    @if (false)
                     <div class="form-group row-rm">
                         <label for="name" class="col-md-4-rm col-form-label text-md-right-rm">{{ __('Name') }}</label>
                         <br/>
@@ -39,6 +86,7 @@
                             @enderror
                         </div>
                     </div>
+                    @endif
       
                     <div class="form-group row-rm">
                         <label for="email" class="col-md-4-rm col-form-label text-md-right-rm">{{ __('E-Mail Address') }}</label>
@@ -68,6 +116,7 @@
                         </div>
                     </div>
       
+                    @if (false)
                     <div class="form-group row-rm">
                         <label for="password-confirm" class="col-md-4-rm col-form-label text-md-right-rm">{{ __('Confirm Password') }}</label>
       
@@ -75,6 +124,7 @@
                             <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                         </div>
                     </div>
+                    @endif
       
                     <div class="form-group row-rm mb-0">
                         <div class="col-md-6-rm offset-md-4-rm">
@@ -84,7 +134,13 @@
                         </div>
                     </div>
                 </form>
+                <div class="py-3 px-3 text-center-rm">
+                  <small>
+                    By continuing, you aggree to our Terms of Use and Privacy policy.
+                  </small>
+                </div>
               </div>
+              @if (false)
               <div class="col-md-6">
 
                 <div class="my-4 text-secondary pr-5">
@@ -119,17 +175,17 @@
                 @endif
 
               </div>
+              @endif
             </div>
 
-            <div class="py-3 px-1 text-center-rm">
-              <small>
-                By continuing, you aggree to our <a href="">Terms of Use</a> and <a href="">Privacy policy</a>.
-              </small>
-            </div>
 
           </div>
       </div>
     </div>
   </div>
   </div>
-@endsection
+
+  <!-- Livewire scripts -->
+  @livewireScripts
+</body>
+</html>
