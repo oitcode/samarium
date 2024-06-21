0279df9340aaecc92d92723c28b5e5ef2c560b8a
odev01 <odev01@example.com>
2024-06-21 08:23:50 +0545

    Misc update; CMS FE updates; Updates

--- resources/views/partials/cms/nav-menu-mobile.blade.php
+++ resources/views/partials/cms/nav-menu-mobile.blade.php
@@ -107,15 +107,15 @@
           @if ($cmsNavMenu)
             @foreach ($cmsNavMenu->cmsNavMenuItems()->orderBy('order', 'asc')->get() as $cmsNavMenuItem)
               @if ($cmsNavMenuItem->type == 'item')
-                <li class="nav-item px-3 bg-transparent">
-                  <a class="nav-link h4 text-reset font-weight-bold"
+                <li class="nav-item px-2 py-1 bg-transparent text-white-rm border-bottom text-center-rm">
+                  <a class="nav-link h6 text-reset font-weight-bold-rm mb-0"
                       href="{{ route('website-webpage-' . $cmsNavMenuItem->webpage->permalink) }}">
                     {{ $cmsNavMenuItem->name }}
                   </a>
                 </li>
               @else
-                <li class="nav-item dropdown px-3 border">
-                  <a class="nav-link dropdown-toggle h4 text-dark-rm font-weight-bold text-reset" href="#"
+                <li class="nav-item dropdown px-2 py-1 border">
+                  <a class="nav-link dropdown-toggle h6 mb-0 text-dark-rm font-weight-bold-rm text-reset" href="#"
                       id="navbarDropdown-{{ $cmsNavMenuItem->name }}"
                       role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     {{ $cmsNavMenuItem->name }}
@@ -299,15 +299,15 @@
           @if ($cmsNavMenu)
             @foreach ($cmsNavMenu->cmsNavMenuItems()->orderBy('order', 'asc')->get() as $cmsNavMenuItem)
               @if ($cmsNavMenuItem->type == 'item')
-                <li class="nav-item p-2 bg-transparent text-white-rm border-bottom-rm text-center-rm">
-                  <a class="nav-link h4 text-reset font-weight-bold"
+                <li class="nav-item px-2 py-1 bg-transparent text-white-rm border-bottom text-center-rm">
+                  <a class="nav-link h6 text-reset font-weight-bold-rm mb-0"
                       href="{{ route('website-webpage-' . $cmsNavMenuItem->webpage->permalink) }}">
                     {{ $cmsNavMenuItem->name }}
                   </a>
                 </li>
               @else
-                <li class="nav-item dropdown p-2 border">
-                  <a class="nav-link dropdown-toggle h4 text-dark-rm font-weight-bold text-reset" href="#"
+                <li class="nav-item dropdown px-2 py-1 border">
+                  <a class="nav-link dropdown-toggle h6 mb-0 text-dark-rm font-weight-bold-rm text-reset" href="#"
                       id="navbarDropdown-{{ $cmsNavMenuItem->name }}"
                       role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     {{ $cmsNavMenuItem->name }}
@@ -362,12 +362,11 @@
               </a>
             </li>
             @endif
-            <li class="nav-item border bg-light-rm text-dark-rm p-3">
-              <a class="nav-link text-dark h4" href="{{ route('logout') }}"
+            <li class="nav-item border bg-light-rm text-dark-rm px-2">
+              <a class="nav-link text-dark h6" href="{{ route('logout') }}"
                   onclick="event.preventDefault();
                             document.getElementById('logout-form').submit();"
               >
-                <i class="fas fa-power-off mr-2 text-danger font-weight-bold"></i>
                 <span class="text-danger font-weight-bold">
                 Logout
               </a>
@@ -381,7 +380,7 @@
           --}}
           <li class="nav-item p-2 bg-transparent text-white-rm border-bottom-rm text-center-rm">
             <div class="nav-link h4 text-reset font-weight-bold">
-              <div class="p-3" style="background-color: rgba(0, 0, 0, 0.5);">
+              <div class="p-3" style="background-color: rgba(0, 0, 0, 0.0);">
                 <div class="d-flex">
                   @if ($company->fb_link)
                     <div class="mr-3">
