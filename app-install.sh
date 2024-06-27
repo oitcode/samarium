74713c5f91932627e638f4b2f00b3d8a71f33060
odev01 <odev01@example.com>
2024-06-27 11:36:35 +0545

    Misc update; app-install bash script updated; Updates

--- app-install.sh
+++ app-install.sh
@@ -9,14 +9,15 @@
 ##  this bash script, and your application will be completely setup. It will perform
 ##  below actions:
 ##
-##  1. Create required database and grant access to the database.
-##  2. Download and install composer dependencies.
-##  3. Download and install npm dependencies.
-##  4. Run migrations.
-##  5. Generate application key.
+##  1. Check if php dependencies are installed. If not installed then install them.
+##  2. Check if composer and npm are installed. If not installed then install them.
+##  2. Create required database and grant access to the database.
+##  3. Download and install composer dependencies.
+##  4. Download and install npm dependencies.
+##  5. Run migrations.
+##  6. Generate application key.
 ##
 ##  Thank you for trying out this application! 
-##
 ##
 
 echo '================================================================================'
@@ -24,6 +25,91 @@ echo 'Welcome'
 echo '================================================================================'
 
 
+##
+## First check that php dependencies are installed.
+##
+
+declare -a phpDependencies=(
+                   "php"
+                   "php-bcmath"
+                   "php-json"
+                   "php-mbstring"
+                   "php-tokenizer"
+                   "php-xml"
+                   "php-common"
+               )
+
+## Check for each php dependency
+for i in "${phpDependencies[@]}"
+do
+    REQUIRED_PKG=$i
+    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
+    echo $REQUIRED_PKG: $PKG_OK
+    if [ "" = "$PKG_OK" ]; then
+      echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+      sudo apt-get --yes install $REQUIRED_PKG
+    fi
+done
+
+##
+## Check for composer.
+##
+
+which composer >> /dev/null
+
+if [ $? != 0 ]; then
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+    php composer-setup.php
+    php -r "unlink('composer-setup.php');"
+    sudo mv composer.phar /usr/local/bin/composer
+else
+    echo 'Composer already installed'
+fi
+
+##
+## Check for npm.
+##
+
+declare -a npmDependencies=(
+                   "npm"
+               )
+
+for i in "${npmDependencies[@]}"
+do
+    REQUIRED_PKG=$i
+    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
+    echo $REQUIRED_PKG: $PKG_OK
+    if [ "" = "$PKG_OK" ]; then
+      echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+      sudo apt-get --yes install $REQUIRED_PKG
+    fi
+done
+
+##
+## Check for mysql.
+##
+
+declare -a mysqlDependencies=(
+                   "mysql-server"
+               )
+
+for i in "${mysqlDependencies[@]}"
+do
+    REQUIRED_PKG=$i
+    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
+    echo $REQUIRED_PKG: $PKG_OK
+    if [ "" = "$PKG_OK" ]; then
+      echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+      sudo apt-get --yes install $REQUIRED_PKG
+    fi
+done
+
+
+##
+## Do the required tasks for setting up a typical laravel application
+## (which our application is!).
+##
 clear
 echo '-- Setting up database'
 echo -n 'Database name: '
@@ -88,6 +174,9 @@ clear
 echo '-- Generating application key'
 php artisan key:generate
 
+clear
+echo '-- Creating storage links'
+php artisan storage:link
 
 clear
 echo '-- Press any key to continue ...'
