3ebe9a5a063eeaeabc1bd5ec7af0dea5743e351c
odev01 <odev01@example.com>
2024-07-05 17:25:55 +0545

    Misc updates;

--- resources/views/livewire/product/dashboard/product-question-list.blade.php
+++ resources/views/livewire/product/dashboard/product-question-list.blade.php
@@ -14,13 +14,15 @@
     <table class="table table-hover shadow-sm border">
       <thead>
         <tr class="p-4 bg-white text-dark" style="font-size: 1rem;">
+          @if (false)
           <th>
             ID
           </th>
+          @endif
           <th class="">
             Product
           </th>
-          <th class="d-none d-md-table-cell">
+          <th class="">
             Question
           </th>
           <th class="">
@@ -33,15 +35,18 @@
       </thead>
 
       <tbody class="bg-white">
+        @if (false && count($productQuestions) > 0)
         @foreach ($productQuestions as $productQuestion)
           <tr>
+            @if (false)
             <td>
               {{ $productQuestion->product_question_id }}
             </td>
+            @endif
             <td class="h6 font-weight-bold" wire:click="$emit('displayProductQuestion', {{ $productQuestion }})" role="button">
               {{ \Illuminate\Support\Str::limit($productQuestion->product->name, 60, $end=' ...') }}
             </td>
-            <td class="d-none d-md-table-cell font-weight-bold" style="font-size: 1rem;">
+            <td class="font-weight-bold" style="font-size: 1rem;">
               {{ $productQuestion->question_text }}
             </td>
             <td>
@@ -54,6 +59,16 @@
             </td>
           </tr>
         @endforeach
+        @else
+          <tr>
+            <td colspan="4">
+              <p class="font-weight-bold text-muted-rm h4 py-4 text-center" style="color: #fe8d01;">
+                <i class="fas fa-exclamation-circle mr-2"></i>
+                No product questions yet.
+              <p>
+            </td>
+          </tr>
+        @endif
       </tbody>
     </table>
 
