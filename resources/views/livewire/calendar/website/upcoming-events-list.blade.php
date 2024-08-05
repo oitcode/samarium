364b3386e654fc3c1f1b217d6b5cbcd0dfb80e44
odev01 <odev01@example.com>
2024-08-05 14:25:30 +0545

    Misc update

--- resources/views/livewire/calendar/website/upcoming-events-list.blade.php
+++ resources/views/livewire/calendar/website/upcoming-events-list.blade.php
@@ -1,27 +1,55 @@
 <div>
 
+
+    @php
+      $loopDate = \Carbon\Carbon::today();
+    @endphp
+    @for ($i=0; $i<30; $i++)
+
+      @if (\App\SchoolCalendarEvent::whereDate('start_date', $loopDate->format('Y-m-d'))->get()->count())
+        @if (false)
+        <div class="mt-3">
+          {{ $loopDate->format('Y-m-d') }}
+        </div>
+        @endif
+
+        <div class="d-flex mb-3">
+          <div class="border-right mr-4 pr-4">
+            <div class="h2 font-weight-bold text-success">
+            {{ $loopDate->format('F j') }}
+            </div>
+            <div>
+            {{ $loopDate->format('l') }}
+            </div>
+          </div>
+          <div class="">
+            @foreach (\App\SchoolCalendarEvent::whereDate('start_date', $loopDate->format('Y-m-d'))->get() as $calendarEvent)
+              <div>
+                {{ $calendarEvent->title }}
+
+                @if ($calendarEvent->calendarGroups != null && count($calendarEvent->calendarGroups) > 0)
+                  @foreach ($calendarEvent->calendarGroups as $calendarGroup)
+                    <span class="badge badge-primary p-2">
+                    {{ $calendarGroup->name }}
+                  @endforeach
+                @endif
+              </div>
+            @endforeach
+          </div>
+        </div>
+      @endif
+
+      @php
+        $loopDate = $loopDate->addDay();
+      @endphp
+    @endfor
+
+  {{--
   @foreach ($calendarEvents as $calendarEvent)
     @php
       $eventDate = \Carbon\Carbon::create($calendarEvent->start_date);
     @endphp
-    <div class="d-flex mb-3">
-      <div class="border-right mr-4 pr-4">
-        <div class="h2 font-weight-bold text-success">
-        {{ $eventDate->format('F j' ) }}
-        </div>
-        {{ $eventDate->format('l') }}
-      </div>
-      <div class="">
-        {{ $calendarEvent->title }}
-
-        @if ($calendarEvent->calendarGroups != null && count($calendarEvent->calendarGroups) > 0)
-          @foreach ($calendarEvent->calendarGroups as $calendarGroup)
-            <span class="badge badge-primary p-2">
-            {{ $calendarGroup->name }}
-          @endforeach
-        @endif
-      </div>
-    </div>
   @endforeach
+  --}}
 
 </div>
