7c663ce23a533e24066a5f1a05dd08e453892c7d
odev01 <odev01@example.com>
2024-09-27 06:15:22 +0545

    Upcoming events list update

--- app/Livewire/Calendar/Website/UpcomingEventsList.php
+++ app/Livewire/Calendar/Website/UpcomingEventsList.php
@@ -5,15 +5,135 @@ namespace App\Livewire\Calendar\Website;
 use Livewire\Component;
 
 use App\SchoolCalendarEvent;
+use App\CalendarGroup;
 
 class UpcomingEventsList extends Component
 {
     public $calendarEvents;
 
+    public $monthBook = array();
+
+    public $displayMonthName;
+    public $startDay;
+    public $endDay;
+
+    public $today;
+
+    public $displayingCalendarEvent;
+    public $calendarGroups;
+    public $selectedCalendarGroup = null;
+
+    public function mount()
+    {
+        $this->calendarGroups = CalendarGroup::all();
+        $this->selectedCalendarGroup = CalendarGroup::first();
+
+        $this->populateMonthBook();
+
+        if (count($calendarGroups) > 0) {
+            $this->calendarEvents = SchoolCalendarEvent::whereHas('calendarGroups', function ($query) { $query->where('name', $this->selectedCalendarGroup->name);})
+                ->whereDate('start_date', '>=', \Carbon\Carbon::today())
+                ->orderBy('start_date', 'asc')
+                ->get();
+        } else {
+            $this->calendarEvents = SchoolCalendarEvent::whereDate('start_date', '>=', \Carbon\Carbon::today())
+                ->orderBy('start_date', 'asc')
+                ->get();
+        }
+    }
+
     public function render()
     {
-        $this->calendarEvents = SchoolCalendarEvent::whereDate('start_date', '>=', \Carbon\Carbon::today())->orderBy('start_date', 'asc')->get();
-
         return view('livewire.calendar.website.upcoming-events-list');
+    }
+
+    public function selectCalendarGroup(CalendarGroup $calendarGroup)
+    {
+        $this->selectedCalendarGroup = $calendarGroup;
+
+        $this->populateMonthBook();
+
+        $this->calendarEvents = SchoolCalendarEvent::whereHas('calendarGroups', function ($query) { $query->where('name', $this->selectedCalendarGroup->name);})
+            ->whereDate('start_date', '>=', \Carbon\Carbon::today())
+            ->orderBy('start_date', 'asc')
+            ->get();
+    }
+
+    public function populateMonthBook()
+    {
+        // $monthStartDate = $this->monthInfo2081[$this->displayMonthName][0];
+        // $monthEndDate = $this->monthInfo2081[$this->displayMonthName][1];
+
+        $monthStartDay = \Carbon\Carbon::today();
+        $monthEndDay = $monthStartDay->copy()->addDays(30);
+
+        $this->monthBook = array();
+
+        $day = $monthStartDay->copy();
+
+        for ($i = 0; ; $i++) {
+            $this->monthBook[] = [
+                'day' => $day->copy(),
+                'is_holiday' => $this->checkIfDayIsHoliday($day->copy()),
+                'events' => $this->getEventsForTheDay($day->copy()),
+            ];
+
+            $day = $day->addDay();
+
+            if ($day > $monthEndDay) {
+                break;
+            }
+        }
+    }
+
+    public function checkIfDayIsHoliday($day)
+    {
+        $events = SchoolCalendarEvent::whereDate('start_date' , '<=', $day->toDateString())
+            ->whereDate('end_date', '>=', $day->toDateString())
+            ->where('is_holiday', 'yes')
+            ->get();
+
+        $calendarGroupEvent = collect([]);
+
+        foreach ($events as $event) {
+            if ($event->calendarGroups && count($event->calendarGroups) > 0) {
+                foreach ($event->calendarGroups as $calendarGroup) {
+                    if ($calendarGroup->calendar_group_id == $this->selectedCalendarGroup->calendar_group_id) {
+                        $calendarGroupEvent->push($event);
+                    }
+                }
+            } else {
+                $calendarGroupEvent->push($event);
+            }
+        }
+
+        if (count($calendarGroupEvent)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getEventsForTheDay($day)
+    {
+        $events = SchoolCalendarEvent::whereDate('start_date' , '<=', $day->toDateString())
+            ->whereDate('end_date', '>=', $day->toDateString())
+            ->get();
+
+        $calendarGroupEvent = collect([]);
+
+        foreach ($events as $event) {
+            if ($event->calendarGroups && count($event->calendarGroups) > 0) {
+                foreach ($event->calendarGroups as $calendarGroup) {
+                    if ($calendarGroup->calendar_group_id == $this->selectedCalendarGroup->calendar_group_id) {
+                        $calendarGroupEvent->push($event);
+                    }
+                }
+            } else {
+                // $calendarGroupEvent->push($event);
+            }
+        }
+
+        return $calendarGroupEvent;
     }
 }
--- resources/views/livewire/calendar/website/upcoming-events-list.blade.php
+++ resources/views/livewire/calendar/website/upcoming-events-list.blade.php
@@ -46,72 +46,139 @@
 
 
 
-   <div class="mb-3 col-md-4-rm bg-success text-white p-3">
+   <div class="d-flex justify-content-between col-md-4-rm bg-success text-white p-3">
      <h2 class="h6 mb-0">
        Upcoming events
      </h2>
+     <div class="my-3">
+       @foreach ($calendarGroups as $calendarGroup)
+         <button class="btn @If ($calendarGroup->calendar_group_id == $selectedCalendarGroup->calendar_group_id) btn-dark @else btn-light border @endif p-3"
+             type="button" wire:click="selectCalendarGroup({{ $calendarGroup }})">{{ $calendarGroup->name }}</button>
+       @endforeach
+     </div>
    </div>
 
-    @php
-      $loopDate = \Carbon\Carbon::today();
-      $noEvent = true;
-    @endphp
-    @for ($i=0; $i<30; $i++)
 
-      @if (\App\SchoolCalendarEvent::whereDate('start_date', $loopDate->format('Y-m-d'))->get()->count())
-        @php
-          $noEvent = false;
-        @endphp
+            @foreach ($monthBook as $day)
+              @if (count($day['events']) > 0)
+              <div
+                  class=" d-flex p-3 border " >
+                <div class="border-0 w-50">
 
-        @if (false)
-        <div class="mt-3">
-          {{ $loopDate->format('Y-m-d') }}
-        </div>
-        @endif
+                    <div class="h5 font-weight-bold text-success mb-1">
+                    {{ \App\Traits\NepaliDateTrait::convertEnglishToNepaliDate($day['day']->toDateString(), 'english')  }}
+                    </div>
+                    <div>
+                    {{ $day['day']->format('l') }}
+                    </div>
 
-        <div class="d-flex mb-3">
-          <div class="border-right mr-4 pr-4 pl-3">
-            <div class="h5 font-weight-bold text-success">
-            {{ \App\Traits\NepaliDateTrait::convertEnglishToNepaliDate($loopDate->toDateString(), 'english')  }}
-            </div>
-            <div>
-            {{ $loopDate->format('l') }}
-            </div>
-          </div>
-          <div class="px-3">
-            @foreach (\App\SchoolCalendarEvent::whereDate('start_date', $loopDate->format('Y-m-d'))->get() as $calendarEvent)
-              <div class="mb-2">
-                {{ $calendarEvent->title }}
+                </div>
+                <div class="d-block-rm d-md-table-cell-rm border-0">
+                  @if ($day['day']->format('l') == 'Saturday' || $day['is_holiday'])
+                    @if (false)
+                    <span class=" badge badge-pill badge-danger">
+                      Holiday
+                    </span>
+                    <br />
+                    @endif
+                  @endif
+                  @foreach ($day['events'] as $event)
 
-                @if ($calendarEvent->is_holiday == 'yes')
-                  <span class="text-danger mx-3">
-                    <i class="fas fa-exclamation-circle mr-1"></i>
-                    Holiday
-                  </span>
-                @endif
-
-                @if ($calendarEvent->calendarGroups != null && count($calendarEvent->calendarGroups) > 0)
-                  @foreach ($calendarEvent->calendarGroups as $calendarGroup)
-                    <span class="badge badge-pill badge-primary p-2">
-                    {{ $calendarGroup->name }}
+                    <div class="d-flex">
+                      <div class="mr-2">
+                        <i class="fas fa-calendar text-muted"></i>
+                      </div>
+                      <div>
+                        @if ($selectedCalendarGroup)
+                        @else
+                          @foreach ($event->calendarGroups as $calendarGroup)
+                            {{ $calendarGroup->name }}
+                            :&nbsp;&nbsp;
+                          @endforeach
+                        @endif
+                        {{ $event->title }}
+                        @if ($day['day']->format('l') == 'Saturday' || $day['is_holiday'])
+                          <span class="mx-4 text-danger">
+                            <i class="fas fa-exclamation-circle"></i>
+                            Holiday
+                          </span>
+                        @endif
+                      </div>
+                    </div>
                   @endforeach
-                @endif
+                </div>
               </div>
+              @endif
             @endforeach
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @if (false)
+    @if (count($calendarEvents) > 0)
+      @foreach ($calendarEvents as $calendarEvent)
+      <div class="d-flex mb-3">
+        <div class="border-right mr-4 pr-4 pl-3">
+          <div class="h5 font-weight-bold text-success">
+          @if (false)
+          {{ \App\Traits\NepaliDateTrait::convertEnglishToNepaliDate($calendarEvent->start_date->toDateString(), 'english')  }}
+          @else
+          Foo
+          @endif
+          </div>
+          <div>
+          @if (false)
+          {{ $calendarEvent->start_date('l') }}
+          @else
+          Bar
+          @endif
           </div>
         </div>
-      @endif
+        <div class="px-3">
+            <div class="mb-2">
+              {{ $calendarEvent->title }}
 
-      @php
-        $loopDate = $loopDate->addDay();
-      @endphp
-    @endfor
+              @if ($calendarEvent->is_holiday == 'yes')
+                <span class="text-danger mx-3">
+                  <i class="fas fa-exclamation-circle mr-1"></i>
+                  Holiday
+                </span>
+              @endif
 
-    @if ($noEvent)
+              @if ($calendarEvent->calendarGroups != null && count($calendarEvent->calendarGroups) > 0)
+                @foreach ($calendarEvent->calendarGroups as $calendarGroup)
+                  <span class="badge badge-pill badge-primary p-2">
+                  {{ $calendarGroup->name }}
+                @endforeach
+              @endif
+            </div>
+        </div>
+      </div>
+      @endforeach
+    @else
       <div class="h5 font-weight-bold px-3" style="color: orange;">
         <i class="fas fa-exclamation-circle mr-1"></i>
         No upcoming events.
       </div>
+    @endif
     @endif
 
   {{--
