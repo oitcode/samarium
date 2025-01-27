00418216fd8b36eb76b353d1cbe7707db3aaf8d6
odev01 <odev01@example.com>
2025-01-27 14:45:52 +0545

    Register page blade file updates

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,7 +10,6 @@
 |
 --}}
 
-
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" style="height: 100% !important;">
 <head>
     <meta charset="utf-8">
@@ -32,6 +31,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom-style.css') }}" rel="stylesheet">
     <link href="{{ asset('vendor/fontawesome-free/css/all.css') }}" rel="stylesheet">
 
     <style>
@@ -44,99 +44,86 @@
     @livewireStyles
 </head>
 
-<body style="height: 100% !important;">
-  <div class="container p-3">
-    <div class="d-flex-rm justify-content-center-rm h-100">
-      <div class="d-flex flex-column justify-content-center py-3">
+<body>
+  <div class="container">
+    <div class="d-flex justify-content-center py-3">
+      <div class="col-md-4">
 
-        <div class="card-rm bg-light-rm bg-secondary-rm shadow-lg-rm mb-3" {{-- style="min-width: 700px !important;" --}}>
+        {{-- Logo and signup text --}}
+        <div class="py-4 text-center">
+          <img src="{{ asset('storage/' . $company->logo_image_path) }}"
+              class="img-fluid mb-5"
+              alt="{{ $company->name }} logo"
+              style="height: 75px !important; {{-- max-width: 75px !important; --}}">
+          <h1 class="h3 o-heading">
+            Signup
+          </h1>
+        </div>
 
-          <div class="card-body bg-light">
-            <div class="d-flex justify-content-center">
-              <div class="col-md-6-rm">
-                @if (true)
-                <div class="bg-white-rm text-dark-rm d-flex-rm justify-content-center-rm py-4 text-center">
-                  <div class="mb-5">
-                    <img src="{{ asset('storage/' . $company->logo_image_path) }}"
-                        class="img-fluid"
-                        alt="{{ $company->name }} logo"
-                        style="height: 75px !important; {{-- max-width: 75px !important; --}}">
-                  </div>
-                  <h1 class="h3 font-weight-bold pl-3-rm">
-                    Signup
-                  </h1>
+        {{--
+           |
+           | Register form
+           |
+        --}}
+        <form method="POST" action="{{ route('register') }}" class="border bg-white p-3">
+            @csrf
+            <div class="form-group">
+                <label for="name" class="col-form-label o-heading">{{ __('Name') }}</label>
+                <div>
+                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                    @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
-                @endif
-                <form method="POST" action="{{ route('register') }}" class="border bg-white p-3">
-                    @csrf
-        
-                    <div class="form-group row-rm">
-                        <label for="name" class="col-md-4-rm col-form-label text-md-right-rm">{{ __('Name') }}</label>
-
-                        <div class="col-md-6-rm">
-                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                            @error('name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="form-group row-rm">
-                        <label for="email" class="col-md-4-rm col-form-label text-md-right-rm">{{ __('E-Mail Address') }}</label>
-        
-                        <div class="col-md-6-rm">
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-        
-                            @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                    </div>
-        
-                    <div class="form-group row-rm">
-                        <label for="password" class="col-md-4-rm col-form-label text-md-right-rm">{{ __('Password') }}</label>
-        
-                        <div class="col-md-6-rm">
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-        
-                            @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="form-group row-rm">
-                        <label for="password-confirm" class="col-md-4-rm col-form-label text-md-right-rm">{{ __('Confirm Password') }}</label>
-
-                        <div class="col-md-6-rm">
-                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                        </div>
-                    </div>
-
-                    <div class="form-group row-rm mb-0">
-                        <div class="col-md-6-rm offset-md-4-rm">
-                            <button type="submit" class="btn btn-block btn-danger py-3">
-                                {{ __('Register') }}
-                            </button>
-                        </div>
-                    </div>
-                </form>
-
-                <div class="py-3 px-3 text-center-rm">
-                  <small>
-                    By continuing, you agree to our Terms of Use and Privacy policy.
-                  </small>
-                </div>
-              </div>
             </div>
-          </div>
+
+            <div class="form-group">
+                <label for="email" class="col-form-label o-heading">{{ __('E-Mail Address') }}</label>
+                <div>
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+        
+            <div class="form-group">
+                <label for="password" class="col-form-label o-heading">{{ __('Password') }}</label>
+                <div>
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+        
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="password-confirm" class="col-form-label o-heading">{{ __('Confirm Password') }}</label>
+                <div>
+                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                </div>
+            </div>
+
+            <div class="form-group mb-0">
+                <div>
+                    <button type="submit" class="btn btn-block btn-danger o-heading text-white py-3">
+                        {{ __('Register') }}
+                    </button>
+                </div>
+            </div>
+        </form>
+
+        <div class="py-3 px-3">
+          <small>
+            By continuing, you agree to our Terms of Use and Privacy policy.
+          </small>
         </div>
       </div>
     </div>
