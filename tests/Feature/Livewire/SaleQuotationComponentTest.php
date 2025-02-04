88dcb6a92f85b97a82f709300f36eaad8163a95f
ismaail <contact@ismaail.com>
2025-02-04 20:53:48 +0100

    Fix failing tests
    Fix failing tests

--- tests/Feature/Livewire/SaleQuotationComponentTest.php
+++ tests/Feature/Livewire/SaleQuotationComponentTest.php
@@ -3,17 +3,21 @@
 namespace Tests\Feature\Livewire;
  
 use App\Livewire\Shop\Dashboard\SaleQuotationComponent;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
-use App\User;
- 
 class SaleQuotationComponentTest extends TestCase
 {
-    /** @test */
+    use RefreshDatabase;
+
+    #[Test]
     public function component_exists_on_the_page()
     {
-        $user = User::where('role', 'admin')->first();;
+        User::factory()->create([]);
 
+        $user = User::where('role', 'admin')->first();
         $this->actingAs($user)
             ->get('/dashboard/sale-quotation')
             ->assertSeeLivewire(SaleQuotationComponent::class);
