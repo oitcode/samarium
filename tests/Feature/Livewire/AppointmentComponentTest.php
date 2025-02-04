fe881eedc5d650b60eb1525ce4646ab6f0e54806
ismaail <contact@ismaail.com>
2025-02-04 20:53:48 +0100

    Fix failing tests
    Fix failing tests

--- tests/Feature/Livewire/AppointmentComponentTest.php
+++ tests/Feature/Livewire/AppointmentComponentTest.php
@@ -3,15 +3,21 @@
 namespace Tests\Feature\Livewire;
  
 use App\Livewire\Appointment\Dashboard\AppointmentComponent;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
 use App\User;
  
 class AppointmentComponentTest extends TestCase
 {
-    /** @test */
+    use RefreshDatabase;
+
+    #[Test]
     public function component_exists_on_the_page()
     {
+        User::factory()->create([]);
+
         $user = User::where('role', 'admin')->first();
 
         $this->actingAs($user)
