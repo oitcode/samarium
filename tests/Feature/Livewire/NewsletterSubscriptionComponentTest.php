d46f4f7bbb1dc5c60d060d068142ae80a175970e
ismaail <contact@ismaail.com>
2025-02-04 21:48:39 +0100

    Fix failing tests

--- tests/Feature/Livewire/NewsletterSubscriptionComponentTest.php
+++ tests/Feature/Livewire/NewsletterSubscriptionComponentTest.php
@@ -3,17 +3,21 @@
 namespace Tests\Feature\Livewire;
  
 use App\Livewire\NewsletterSubscription\Dashboard\NewsletterSubscriptionComponent;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
-use App\User;
- 
 class NewsletterSubscriptionComponentTest extends TestCase
 {
-    /** @test */
+    use RefreshDatabase;
+
+    #[Test]
     public function component_exists_on_the_page()
     {
-        $user = User::where('role', 'admin')->first();
+        User::factory()->create([]);
 
+        $user = User::where('role', 'admin')->first();
         $this->actingAs($user)
             ->get('/dashboard/newsletter-subscription')
             ->assertSeeLivewire(NewsletterSubscriptionComponent::class);
