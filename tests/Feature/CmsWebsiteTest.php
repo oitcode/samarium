404dee7be3664bc60596a5d2b75681fbdba3e60d
ismaail <contact@ismaail.com>
2025-02-04 22:25:35 +0100

    Disable CmsWebsiteTest

--- tests/Feature/CmsWebsiteTest.php
+++ tests/Feature/CmsWebsiteTest.php
@@ -19,6 +19,8 @@ class CmsWebsiteTest extends TestCase
      */
     public function test_all_public_webpages_are_accessible()
     {
+        $this->markTestSkipped('webpage route disabled');
+
         Webpage::factory()->create();
 
         $publicWebpages = Webpage::where('visibility', 'public')->get();
@@ -37,6 +39,8 @@ class CmsWebsiteTest extends TestCase
      */
     public function test_all_non_public_webpages_are_not_accessible()
     {
+        $this->markTestSkipped('webpage route disabled');
+
         $nonPublicWebpages = Webpage::where('visibility', '!=', 'public')->get();
 
         foreach ($nonPublicWebpages as $nonPublicWebpage) {
