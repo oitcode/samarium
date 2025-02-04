522496ddd4410bd9b6d8ac349acc5a8ba0c63dbd
ismaail <contact@ismaail.com>
2025-02-04 22:24:19 +0100

    Add Product, ProductCategory & ProductVendor Factories

---
+++ database/factories/ProductVendorFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<\App\ProductVendor>
+ */
+class ProductVendorFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->company(),
+            'phone' => $this->faker->phoneNumber(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'address' => $this->faker->address(),
+        ];
+    }
+}
