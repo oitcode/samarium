ac4f82aa79e74e862da90752545f80eb5f0c45a6
fabiorodriguesroque <fabioroque9@gmail.com>
2025-02-16 13:17:09 +0000

    chore: add hasFactory trait to model

--- app/NewsletterSubscription.php
+++ app/NewsletterSubscription.php
@@ -2,10 +2,13 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class NewsletterSubscription extends Model
 {
+    use HasFactory;
+
     /**
      * The table associated with the model.
      *
