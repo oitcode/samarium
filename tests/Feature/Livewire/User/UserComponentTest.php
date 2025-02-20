29022adbbfa1825773887f0250e677c65d1f61e3
fabiorodriguesroque <fabioroque9@gmail.com>
2025-02-20 23:34:11 +0000

    tests: test UserComponent methods

--- tests/Feature/Livewire/User/UserComponentTest.php
+++ tests/Feature/Livewire/User/UserComponentTest.php
@@ -6,6 +6,7 @@ use App\Livewire\User\UserComponent;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class UserComponentTest extends TestCase
@@ -18,5 +19,39 @@ class UserComponentTest extends TestCase
             ->get('/dashboard/users')
             ->assertSeeLivewire(UserComponent::class)
             ->assertStatus(200);
+    }
+
+    public function test_exit_create_user_mode_on_user_created()
+    {
+        Livewire::test(UserComponent::class)
+            ->set('modes.createUserMode', true)
+            ->call('userCreated')
+            ->assertSet('modes.createUserMode', false);
+    }
+
+    public function test_exit_create_user_mode_on_exit_create_user_mode()
+    {
+        Livewire::test(UserComponent::class)
+            ->set('modes.createUserMode', true)
+            ->call('exitCreateUserMode')
+            ->assertSet('modes.createUserMode', false);
+    }
+
+    public function test_display_user_on_display_user()
+    {
+        $user = User::where('role', 'admin')->first();
+
+        Livewire::test(UserComponent::class)
+            ->assertSet('modes.displayUserMode', false)
+            ->call('displayUser', $user)
+            ->assertSet('modes.displayUserMode', true);
+    }
+
+    public function test_exit_user_display_mode_on_exit_user_display_mode()
+    {
+        Livewire::test(UserComponent::class)
+            ->set('modes.displayUserMode', true)
+            ->call('exitUserDisplayMode')
+            ->assertSet('modes.displayUserMode', false);
     }
 }
