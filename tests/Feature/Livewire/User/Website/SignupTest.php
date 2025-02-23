c3bf849366ef825f57e5b4dbc15cc4e675f5250e
fabiorodriguesroque <fabioroque9@gmail.com>
2025-02-23 17:03:58 +0000

    tests: add tests to user/website/signup

---
+++ tests/Feature/Livewire/User/Website/SignupTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature\Livewire\User\Website;
+
+use App\Livewire\User\Website\Signup;
+use Tests\TestCase;
+
+class SignupTest extends TestCase
+{
+    public function test_component_exists_on_page()
+    {
+        $this->get('/user/signup')
+            ->assertStatus(200)
+            ->assertSeeLivewire(Signup::class);
+    }
+
+    public function test_user_can_sign_up()
+    {
+        // todo: this feature is not working.
+    }
+}
