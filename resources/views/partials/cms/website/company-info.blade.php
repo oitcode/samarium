160824848340951bea63bc47a4b13a4d58aa0754
oitcode <operating.it.np@gmail.com>
2025-03-18 12:37:02 +0545

    Implemented company info partial

---
+++ resources/views/partials/cms/website/company-info.blade.php
@@ -0,0 +1,12 @@
+<div class="container-fluid bg-primary text-white py-4">
+  <div class="container py-4">
+
+    <h1 class="h4 o-heading text-white text-center">
+      {{ $company->short_name }}
+    </h1>
+
+    <div>
+      {{ $company->brief_description }}
+    </div>
+  </div>
+</div>
