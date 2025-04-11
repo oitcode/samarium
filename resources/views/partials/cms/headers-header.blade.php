9f80a831d09d0d4c00c3744209f21f97679c4c06
oitcode <operating.it.np@gmail.com>
2025-04-11 12:20:51 +0545

    Website UI updates

--- resources/views/partials/cms/headers-header.blade.php
+++ resources/views/partials/cms/headers-header.blade.php
@@ -16,87 +16,52 @@
     ;"
 >
   {{-- Show in bigger screens --}}
+  <div class="container-fluid d-none d-md-block bg-primary text-white">
+    <div class="container text-right py-2">
+      <i class="fas fa-envelope mr-1"></i>
+      {{ $company->email }}
+      <i class="fas fa-phone ml-4 mr-1"></i>
+      {{ $company->phone }}
+    </div>
+  </div>
   <div class="container d-none d-md-block">
-    <div class="d-flex justify-content-center h-100 pl-2">
-      <a href="{{ route('website-home') }}" class="text-decoration-none">
-      <div class="d-flex py-3">
-        <div class="mr-4 d-flex flex-column justify-content-center">
-            <img src="{{ asset('storage/' . $company->logo_image_path) }}" style="max-width: 100px; max-height: 100px;">
-        </div>
-      </div>
-      </a>
-      <div class="d-flex flex-grow-1">
-        <div class="d-flex flex-column justify-content-center flex-grow-1 px-4">
-          <h1 class="h5 font-weight-bold my-1" style="font-family: Mono;">
-            @if ($company->short_name)
-              {{ \Illuminate\Support\Str::limit($company->short_name, 100, $end=' ...') }}
-            @else
-              {{ \Illuminate\Support\Str::limit($company->name, 100, $end=' ...') }}
-            @endif
-          </h1>
-          <div class="w-100">
-            <div>
-              {{ $company->address }}
+    <div class="d-flex justify-content-between">
+      <div class="">
+        <div class="d-flex justify-content-center h-100 pl-2">
+          <a href="{{ route('website-home') }}" class="text-decoration-none">
+          <div class="d-flex py-3">
+            <div class="mr-4 d-flex flex-column justify-content-center">
+                <img src="{{ asset('storage/' . $company->logo_image_path) }}" style="max-width: 100px; max-height: 100px;">
             </div>
           </div>
+          </a>
+          <div class="d-flex flex-grow-1">
+            <div class="d-flex flex-column justify-content-center flex-grow-1 px-4">
+              <h1 class="h5 font-weight-bold my-1" style="font-family: Mono;">
+                @if ($company->short_name)
+                  {{ \Illuminate\Support\Str::limit($company->short_name, 100, $end=' ...') }}
+                @else
+                  {{ \Illuminate\Support\Str::limit($company->name, 100, $end=' ...') }}
+                @endif
+              </h1>
+              <div class="w-100">
+                <div>
+                  {{ $company->address }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="">
+        <div class="h-100 d-flex flex-column justify-content-center">
+          <button class="btn btn-primary btn-lg">
+            <i class="fas fa-plus-circle mr-1"></i>
+            Post product
+          </button>
         </div>
       </div>
     </div>
   </div>
 
-  <div class="d-none d-md-block">
-    <nav class="navbar navbar-expand-lg m-0 p-0 d-md-none">
-      <button class="navbar-toggler p-3 m-3" style="border: 1px solid gray;" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <i class="fas fa-list"></i>
-      </button>
-      <a class="navbar-brand p-3 text-reset" href="/">
-        <img src="{{ asset('storage/' . $company->logo_image_path) }}" class="img-fluid" style="height: 50px;">
-      </a>
-      <div class="px-3">
-        @livewire ('ecomm-website.shopping-cart-badge')
-      </div>
-      <div class="collapse navbar-collapse m-0 p-0 mt-4" id="navbarSupportedContent" style="margin-left: 0;">
-        <ul class="navbar-nav m-0 p-0" style="margin: auto;">
-          {{-- Common things --}}
-          @guest
-            <li class="nav-item border bg-light text-dark p-3">
-              <a class="nav-link text-dark" href="{{ route('login') }}">
-                <i class="fas fa-user mr-3"></i>
-                <span class="font-weight-bold">
-                Sign in 
-                </span>
-              </a>
-            </li>
-            <li class="nav-item border bg-light text-dark p-3">
-              <a class="nav-link text-dark" href="{{ route('register') }}">
-                <i class="fas fa-lock mr-3"></i>
-                <span class="font-weight-bold">
-                Sign up
-                </span>
-              </a>
-            </li>
-          @else
-            <li class="nav-item border bg-light text-dark p-3">
-              <a class="nav-link text-dark" href="{{ route('website-user-profile') }}">
-                <i class="fas fa-user mr-3"></i>
-                <span class="font-weight-bold">
-                My profile
-                </span>
-              </a>
-            </li>
-            <li class="nav-item border bg-light text-dark p-3">
-              <a class="nav-link text-dark" href="{{ route('logout') }}"
-                  onclick="event.preventDefault();
-                            document.getElementById('logout-form').submit();"
-              >
-                <i class="fas fa-power-off mr-2 text-danger font-weight-bold"></i>
-                <span class="text-danger font-weight-bold">
-                Logout
-              </a>
-            </li>
-          @endguest
-        </ul>
-      </div>
-    </nav>
-  </div>
 </div>
