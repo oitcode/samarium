19f0a914c50ea0563fbb65fe5bb5f5e6902a4f37
oitcode <operating.it.np@gmail.com>
2025-04-24 12:18:34 +0545

    Implemented selling price unit for product - database migration

---
+++ database/migrations/2025_04_24_112544_update_product_table_add_selling_price_unit.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('product', function (Blueprint $table) {
+            $table->string('selling_price_unit')->nullable()->after('selling_price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('product', function (Blueprint $table) {
+            $table->dropColumn('selling_price_unit');
+        });
+    }
+};
