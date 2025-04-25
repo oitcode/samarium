e4d8a3ee1c9b6d249cc78f82d78d12328ddbfca9
oitcode <operating.it.np@gmail.com>
2025-04-25 12:42:50 +0545

    Website featured product list update

--- resources/views/livewire/product/website/featured-product-list.blade.php
+++ resources/views/livewire/product/website/featured-product-list.blade.php
@@ -7,9 +7,11 @@
 
     <div class="row my-4" style="margin: auto;">
       @foreach ($featuredProducts as $product)
-        <div class="col-md-4 p-3">
-          @livewire ('ecomm-website.product-list-display', ['product' => $product,])
-        </div>
+        @if ($product->show_in_front_end == 'yes')
+          <div class="col-md-4 p-3">
+            @livewire ('ecomm-website.product-list-display', ['product' => $product,])
+          </div>
+        @endif
       @endforeach
     </div>
   </div>
