fe20b7372ff088088bf24d0fc07ee2dacc16e2ea
oitcode <operating.it.np@gmail.com>
2025-06-17 18:07:41 +0545

    Misc update

--- resources/views/dashboard/bgc/quick-contacts.blade.php
+++ resources/views/dashboard/bgc/quick-contacts.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section ('content')
-  @if (\App\Team::where('name', 'Quick Contacts')->first())
-    @livewire ('team.dashboard.team-display', ['team' => \App\Team::where('name', 'Quick Contacts')->first(),])
+  @if (\App\Models\Team\Team::where('name', 'Quick Contacts')->first())
+    @livewire ('team.dashboard.team-display', ['team' => \App\Models\Team\Team::where('name', 'Quick Contacts')->first(),])
   @else
     <div class="text-secondary">
       <i class="fas fa-exclamation-circle mr-1"></i>
