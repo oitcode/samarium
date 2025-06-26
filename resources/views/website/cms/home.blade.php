9da849c5f17d0e33f431b4dca146baa9dc22f5cc
oitcode <operating.it.np@gmail.com>
2025-06-26 16:59:47 +0545

    Misc updates

--- resources/views/website/cms/home.blade.php
+++ resources/views/website/cms/home.blade.php
@@ -60,7 +60,7 @@
   | HFN.
   |
   --}}
-  @if (has_module('hfn'))
+  @if (false && has_module('hfn'))
     <div class="container-fluid p-0 o-fade-in" style="
     @if($cmsTheme)
       background-color: {{ $cmsTheme->ascent_bg_color }};
@@ -100,9 +100,9 @@
                         @endif
                         ;
               ">
-                @if (\App\Webpage::where('name', 'Contact us')->orWhere('permalink', '/contact-us')->first())
+                @if (\App\Models\Cms\Webpage\Webpage::where('name', 'Contact us')->orWhere('permalink', '/contact-us')->first())
                   <div class="border" style="background-color: rgba(0, 0, 0, 0.5)">
-                    <a href="{{ \App\Webpage::where('name', 'Contact us')->orWhere('permalink', '/contact-us')->first()->permalink }}"
+                    <a href="{{ \App\Models\Cms\Webpage\Webpage::where('name', 'Contact us')->orWhere('permalink', '/contact-us')->first()->permalink }}"
                         class="btn btn-block py-3"
                         style="
                         color:
@@ -139,12 +139,13 @@
   |
   --}}
   <div class="container">
-    @foreach (\App\Product::limit(5)->get() as $product)
+    @foreach (\App\Models\Product\Product::limit(5)->get() as $product)
       @livewire ('product.website.product-listing-display', ['product' => $product,], key(rand()),)
     @endforeach                                                                                                                                      
   </div>
   @endif
   
+  @if (true)
   {{--
   |
   | Calendar and latest notice.
@@ -228,11 +229,69 @@
   |
   --}}
   @if (has_module('bgc'))
-    @if (\App\Team::where('team_type', 'playing_team')->first())
+    @if (\App\Models\Team\Team::where('team_type', 'playing_team')->first())
       <div class="container my-4">
         @include ('partials.team.team-block-display')
       </div>
     @endif
   @endif
+  @endif
+
+ {{--
+  |
+  | Product filter.
+  |
+  --}}
+  @livewire ('product.website.product-filter')
+
+ {{--
+  |
+  | Company brief info.
+  |
+  --}}
+  @include ('partials.cms.website.company-info')
+
+  {{--
+  |
+  | Featured products.
+  |
+  --}}
+  @livewire ('product.website.featured-product-list')
+
+  @if (false)
+  {{--
+  |
+  | Product listing.
+  |
+  --}}
+  <div class="container">
+    @foreach (\App\Models\Product\Product::limit(5)->get() as $product)
+      @livewire ('product.website.product-listing-display', ['product' => $product,], key(rand()),)
+    @endforeach                                                                                                                                        
+  </div>
+  @endif
+
+  {{--
+  |
+  | Show products for all categories.
+  |
+  --}}
+  @foreach (\App\Models\Product\ProductCategory::all() as $productCategory)
+    @if (count($productCategory->products) > 0)
+      <div class="container my-5">
+        <h2 class="h4 o-heading text-center">
+          {{ $productCategory->name }}
+        </h2>
+
+        <div class="row" style="margin: auto;">
+          @foreach ($productCategory->products as $product)
+            <div class="col-md-4 p-3">
+              @livewire ('ecomm-website.product-list-display', ['product' => $product,])
+            </div>
+          @endforeach
+        </div>
+      </div>
+    @endif
+  @endforeach
 
 @endsection
