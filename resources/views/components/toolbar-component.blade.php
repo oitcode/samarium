8388610f8aa6d0aced60a930b1cbe04b74d70416
oitcode <operating.it.np@gmail.com>
2025-07-30 13:37:18 +0545

    Misc updates

--- resources/views/components/toolbar-component.blade.php
+++ resources/views/components/toolbar-component.blade.php
@@ -1,14 +1,14 @@
-<div class="row bg-white-rm py-0 mb-1 border-rm" style="margin: auto;">
+<div class="row bg-white py-0 mb-1 border o-border-radius p-2" style="margin: auto;">
   {{-- Toolbar info --}}
-  <div class="col-md-6 my-2 p-2 d-flex flex-column justify-content-center">
-    <div>
+  <div class="col-md-6 mt-2 mb-0 mb-md-2 p-2 d-flex flex-column justify-content-center">
+    <div class="text-center text-md-left">
       {{ $toolbarInfo }}
     </div>
   </div>
 
   {{-- Toolbar buttons --}}
-  <div class="col-md-6 mt-0 p-2 d-flex justify-content-end">
-    <div>
+  <div class="col-md-6 mt-0 p-2 d-flex justify-content-center justify-content-md-end">
+    <div class="text-center text-md-left">
       {{ $toolbarButtons }}
     </div>
   </div>
