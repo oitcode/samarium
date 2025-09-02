037ccfad10bc4bd0fa77f154221bafe153a3484a
oitcode <operating.it.np@gmail.com>
2025-09-02 16:05:08 +0545

    Product review create bugfix

--- app/Livewire/Product/Website/ProductReviewCreate.php
+++ app/Livewire/Product/Website/ProductReviewCreate.php
@@ -4,7 +4,7 @@ namespace App\Livewire\Product\Website;
 
 use Livewire\Component;
 use Illuminate\View\View;
-use App\Models\ProductReview;
+use App\Models\Product\ProductReview;
 
 class ProductReviewCreate extends Component
 {
