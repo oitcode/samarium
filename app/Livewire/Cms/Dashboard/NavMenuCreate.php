246eaacb80a77960cc80457f1d741d2049cb3267
oitcode <operating.it.np@gmail.com>
2025-09-05 13:33:17 +0545

    Dashboard cms nav menu component fix

--- app/Livewire/Cms/Dashboard/NavMenuCreate.php
+++ app/Livewire/Cms/Dashboard/NavMenuCreate.php
@@ -4,7 +4,7 @@ namespace App\Livewire\Cms\Dashboard;
 
 use Livewire\Component;
 use Illuminate\View\View;
-use App\CmsNavMenu;
+use App\Models\Cms\CmsNavMenu\CmsNavMenu;
 
 class NavMenuCreate extends Component
 {
